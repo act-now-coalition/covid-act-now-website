340c3e528a3d4e0b033d048171a62e44654c58ab
Virsaviya <virsaviya@Stardust-21.local>
2020-03-18 18:30:52 -0700

    Add prettier and MUI

--- src/App.js
+++ src/App.js
@@ -1,44 +1,49 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import React from "react";
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import ModelPage from './ModelPage';
 import HomePage from './HomePage';
+import theme from 'assets/theme';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div className="App" style={{ maxWidth: 900, margin: "auto" }}>
-        <h1>Coronavirus: Why You Must Act Now</h1>
-        <h3>
-          Political Leaders, Public Health Officials: The only thing that
-          matters right now is the speed of your response
-        </h3>
-        <h3 style={{ color: "red" }}>
-          This model is intended to help make fast decisions, not predict the
-          future
-        </h3>
-        <Switch>
-          <Route path="/ca">
-            <ModelPage location="ca" locationName="California" />
-          </Route>
-          <Route path="/ak">
-            <ModelPage location="ak" locationName="Alaska" />
-          </Route>
-          <Route path="/fl">
-            <ModelPage location="fl" locationName="Florida" />
-          </Route>
-          <Route path="/co">
-            <ModelPage location="co" locationName="Colorado" />
-          </Route>
-          <Route path="/wa">
-            <ModelPage location="wa" locationName="Washington" />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <div className="App" style={{ maxWidth: 900, margin: 'auto' }}>
+          <h1>Coronavirus: Why You Must Act Now</h1>
+          <h3>
+            Political Leaders, Public Health Officials: The only thing that
+            matters right now is the speed of your response
+          </h3>
+          <h3 style={{ color: 'red' }}>
+            This model is intended to help make fast decisions, not predict the
+            future
+          </h3>
+          <Switch>
+            <Route path="/ca">
+              <ModelPage location="ca" locationName="California" />
+            </Route>
+            <Route path="/ak">
+              <ModelPage location="ak" locationName="Alaska" />
+            </Route>
+            <Route path="/fl">
+              <ModelPage location="fl" locationName="Florida" />
+            </Route>
+            <Route path="/co">
+              <ModelPage location="co" locationName="Colorado" />
+            </Route>
+            <Route path="/wa">
+              <ModelPage location="wa" locationName="Washington" />
+            </Route>
+            <Route path="/">
+              <HomePage />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
-
---
+++ src/assets/theme/overrides/index.js
@@ -0,0 +1,13 @@
+import MuiButton from './MuiButton';
+import MuiIconButton from './MuiIconButton';
+import MuiPaper from './MuiPaper';
+import MuiTableCell from './MuiTableCell';
+import MuiTableHead from './MuiTableHead';
+
+export default {
+  MuiButton,
+  MuiIconButton,
+  MuiPaper,
+  MuiTableCell,
+  MuiTableHead
+};
