2879fde142ef23078f440492947f2f02eb2625f5
Igor Kofman <kofman@gmail.com>
2020-03-20 16:50:23 -0700

    Fix endorsements

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -34,43 +34,58 @@ const Footer = ({ children }) => {
         </Typography>
 
         <Typography variant="body2" component="p" style={{ color: '#222' }}>
-          While no projection is perfect, we endorse this tool and model as a
-          valid and important way to frame the decisions political leaders must
-          make NOW.
-          <br />
-          <br />
-          <ul>
-            <li>
-              Nirav Shah, MD, MPH, Senior Scholar, Stanford University Clinical
-              Excellence Research Center; Former Commissioner, New York State
-              Department of Health
-            </li>
-            <li>
-              “This model offers a great tool for state policy staff who's job it
-              is to advise the Governor and the Director of Health regarding what kinds of
-              policies should be instituted to encourage social distancing to minimize contagion
-              with corona virus. Staff need to be prepared to defend with comparative data their
-              recommendations regarding whether to close schools, close bars, restrict gatherings
-              to a select number or encourage non-essential workers to telecommute.  This web site
-              offers state specific estimates that demonstrate the impact of such policies on the
-              curve of infections, hospitalizations and deaths in a very sophisticated manner. “ <br />
-              Vincent Mor <br />
-              Florence Pirce Grant University Professor, Professor of Health Services, Policy and Practice  <br />
-              Brown University
-            </li>
-            <li>
-              Valerie Nurr&#39;araaluk Davidson <br />
-              former Alaska Commissioner of Health and Social Services, Founder's Council member, United States of Care
-            </li>
-            <li>
-              Tomas Pueyo, author of
-              <a href="https://medium.com/@tomaspueyo/coronavirus-act-today-or-people-will-die-f4d3d9cd99ca">Coronavirus: Why You Must Act Now</a>
-            </li>
-          </ul>
-          More are forthcoming. Please email us if you wish to add your name to
-          this list as a credentialed scientist in public health or infectious
-          disease.
+          <h2 style={{ border: '1px solid #ccc', padding: 20 }}>
+            "While no projection is perfect, we endorse this tool and model as a
+            valid and important way to frame the decisions political leaders
+            must make now."
+          </h2>
 
+          <p style={{ color: '#222' }}>
+            Nirav Shah, MD, MPH, Senior Scholar, Stanford University Clinical
+            Excellence Research Center; Former Commissioner, New York State
+            Department of Health
+          </p>
+
+          <p style={{ color: '#222' }}>
+            Vincent Mor, Florence Pirce Grant University Professor, Professor of
+            Health Services, Policy and Practice, Brown University{' '}
+          </p>
+
+          <blockquote style={{ fontStyle: 'italic' }}>
+            This model offers a great tool for state policy staff whose job it
+            is to advise the Governor and the Director of Health regarding what
+            kinds of policies should be instituted to encourage social
+            distancing to minimize contagion with corona virus. Staff need to be
+            prepared to defend with comparative data their recommendations
+            regarding whether to close schools, close bars, restrict gatherings
+            to a select number or encourage non-essential workers to
+            telecommute. This web site offers state specific estimates that
+            demonstrate the impact of such policies on the curve of infections,
+            hospitalizations and deaths in a very sophisticated manner.
+          </blockquote>
+
+          <p style={{ color: '#222' }}>
+            Valerie Nurr'araaluk Davidson, former Alaska Commissioner of Health
+            and Social Services, Founder's Council member, United States of Care{' '}
+          </p>
+
+          <p style={{ color: '#222' }}>
+            Tomas Pueyo, author of{' '}
+            <a href="https://medium.com/@tomaspueyo/coronavirus-act-today-or-people-will-die-f4d3d9cd99ca">
+              Coronavirus: Why You Must Act Now
+            </a>
+          </p>
+
+          <p style={{ color: '#222' }}>
+            Ben Goldman-Israelow, Infectious Disease Fellow, Yale School of
+            Medicine
+          </p>
+
+          <p style={{ color: '#222' }}>
+            More are forthcoming. Please email us if you wish to add your name
+            to this list as a credentialed scientist in public health or
+            infectious disease.
+          </p>
         </Typography>
 
         <Typography variant="h3" component="h3" style={{ marginBottom: 20 }}>
