f23d493c0d1e50c0857e67b9229b7d53c2f68d6c
Virsaviya <virsaviya.efraim@gmail.com>
2020-03-21 18:27:09 -0700

    Pass data into highcharts and basic formatting

---
+++ src/components/Chart/Chart.style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  .highcharts-tooltip-box {
+    fill: black;
+    fill-opacity: 0.6;
+  }
+  .highcharts-tooltip text {
+    fill: white;
+    text-shadow: 0 0 3px black;
+  }
+`;
