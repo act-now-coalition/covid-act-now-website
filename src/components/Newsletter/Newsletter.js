ecead3206610f67e146fd3f8b8f66a3f9db1ec9a
Greg Soltis <gsoltis@gmail.com>
2020-03-22 13:14:32 -0700

    Add title to newsletter iframe to remove a11y warning

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -4,7 +4,7 @@ import { StyledNewsletter } from './Newsletter.style';
 const Newsletter = () => {
   return (
     <StyledNewsletter>
-      <iframe class="mj-w-res-iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://app.mailjet.com/widget/iframe/1dtU/pXf" width="100%"></iframe>
+      <iframe title="newletter signup" class="mj-w-res-iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://app.mailjet.com/widget/iframe/1dtU/pXf" width="100%"></iframe>
     </StyledNewsletter>
   );
 };
