3732397c2d7d3f507a3a94ff25c668dedb5e03a0
Zack Rosen <zack@stitchlabs.com>
2020-03-21 20:05:20 -0400

    resolves #70

---
+++ src/components/Newsletter/Newsletter.style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const StyledNewsletter = styled.div`
+  iframe {
+    min-height: 350px;
+
+    @media(min-width: 600px) {
+      min-height: 270px;
+    }
+  }
+`;
+
