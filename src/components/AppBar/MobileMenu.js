60b49066190d16368864c6fc8e292c21b86e9de6
Greg Soltis <gsoltis@gmail.com>
2020-03-22 20:32:49 -0700

    Changed goTo to return an eventhandler, which prevents the event's default action

--- src/components/AppBar/MobileMenu.js
+++ src/components/AppBar/MobileMenu.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { 
+import {
   StyledMenu,
 } from './AppBar.style';
 
 const MobileMenu = ({ open, goTo }) => {
   return (
     <StyledMenu open={open}>
-      <a onClick={() => goTo('/')}>
+      <a onClick={goTo('/')} href="/">
         Map
       </a>
-      <a onClick={() => goTo('/about')}>
+      <a onClick={goTo('/about')} href="/about">
         About
       </a>
-      <a onClick={() => goTo('/model')}>
+      <a onClick={goTo('/model')} href="/model">
         Model
       </a>
-      <a onClick={() => goTo('/endorsements')}>
+      <a onClick={goTo('/endorsements')} href="/endorsements">
         Endorsements
       </a>
     </StyledMenu>
