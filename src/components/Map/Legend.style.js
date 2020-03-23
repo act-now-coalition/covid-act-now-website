8aee7aa18f94b8bdee143a1d16ff3030da2a3d97
Adnan Pirzada <ad.pirz@gmail.com>
2020-03-23 16:33:11 -0700

    Whitespace cleanup

--- src/components/Map/Legend.style.js
+++ src/components/Map/Legend.style.js
@@ -3,10 +3,7 @@ import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
-export const LegendContainer = styled(Grid)`
-  padding: 15px;
-  margin: 0 10px;
-`;
+export const LegendContainer = styled(Grid)``;
 
 export const LegendItemHeader = styled(Box)`
   width: 100%;
@@ -21,13 +18,13 @@ export const LegendItemContainer = styled(Paper)`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: 5px;
-  padding: 8px 2px;
+  margin: 3px 5px;
+  padding: 4px 2px;
 `;
 
 export const LegendItemDescription = styled(Box)`
-  margin: 20px;
-  padding: 8px 12px;
+  margin: 8px;
+  padding: 8px;
   background-color: #f7f7f7;
   border-radius: 3px;
   width: 90%;
@@ -37,7 +34,7 @@ export const ColorBox = styled(Grid)`
   background-color: ${props => props.color};
   border: 1px solid gray;
   border-radius: 10px;
-  margin: 0 15px 0 5px;
+  margin: 2px 10px 0 0;
   height: 20px;
   width: 20px;
 `;
