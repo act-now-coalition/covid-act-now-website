c70c7276d38e04eae56e1f7cb1245b02d1ff8f05
Zack Rosen <zack@stitchlabs.com>
2020-03-24 02:50:59 -0400

    remove how to use tool
    - add updated map image

--- src/screens/HomePage/HomePage.js
+++ src/screens/HomePage/HomePage.js
@@ -27,28 +27,6 @@ export default function HomePage() {
       <Wrapper style={{ backgroundColor: '#F2F2F2' }}>
         <Content>
           <Typography variant="h3" component="h3" style={{ marginBottom: 20 }}>
-            How to use this tool
-          </Typography>
-          <Typography variant="body2" component="p" style={{ color: '#222', marginBottom: '3rem' }}>
-            This tool is built to enable political leaders to quickly make
-            decisions in their Coronavirus response informed by best available
-            data and modeling.
-            <br />
-            <br />
-            Here are the questions we built this tool to answer:
-            <ol>
-              <li>
-                What will the impact be in my region be and when can I expect it?
-              </li>
-              <li>How long until my hospital system is under severe pressure?</li>
-              <li>
-                What are my menu of interventions, and how will they address the
-                spread of Coronavirus?
-              </li>
-            </ol>
-          </Typography>
-
-          <Typography variant="h3" component="h3" style={{ marginBottom: 20 }}>
             Endorsements
           </Typography>
           <div style={{margin: '-2rem'}}>
