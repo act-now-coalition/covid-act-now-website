bc14ffb95e7d7382340b261d665cbdf26dd9d90b
Michael Lehenbauer <mikelehen@gmail.com>
2020-03-27 16:44:19 -0700

    Add a /compare page for testing / debugging model data.
    This is an experimental page for internal usage but needs to live in the
    website so we can share all of the chart loading / rendering code with the rest
    of the website.
    
    The idea is to let us easily compare all of the (state) graphs for e.g. staging
    vs. prod or a modeler can point it at their local model files and compare them
    as well.
    
    I think this will be useful, but for now it's experimental.  We can iterate and
    or abandon it depending on future needs.
    
    One specific improvement I'd like to make is adding logic to do a simple
    algorithmic comparison of each model and then sort them based on how different
    they seem to be.

---
+++ src/screens/CompareModels/CompareModels.style.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background-color: white;
+  padding: 1rem 2rem;
+  min-height: calc(100vh - 64px);
+`;
+
+export const ModelSelectorContainer = styled.div`
+  // HACK: This should maybe be sticky or something, but I don't know how to make it work.
+  position: fixed;
+  z-index: 10;
+  background-color: white;
+  border: 1px solid black;
+  border-radius: 3px;
+  padding: 1rem;
+`;
+
+export const ModelComparisonsContainer = styled.div`
+  // HACK: Leave extra space at the top to leave room for InputsContainer (which is position=fixed).
+  padding-top: 200px;
+`;
