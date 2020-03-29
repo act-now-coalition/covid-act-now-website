12994ca845c779621d0a5fc9b6b45a98c367136a
Darrel O'Pry <darrel.opry@gmail.com>
2020-03-29 03:13:18 -0400

    chore: cypress code coverage
    * instrument cypress for code coverage
    * add inital TESTING.md
    * streamline run-scripts
    * cache cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -18,4 +18,5 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  on('task', require('@cypress/code-coverage/task'))
 };
