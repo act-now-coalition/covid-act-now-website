3ae7f5f40889a127db65766fbfe6078e4be8158c
Sean Zhu <interestinglythere@gmail.com>
2020-03-30 12:46:25 -0700

    Add padding back to /endorsements

---
+++ src/screens/Endorsements/EndorsementsPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Endorsements from 'screens/Endorsements/Endorsements';
+import { Wrapper } from 'screens/Endorsements/EndorsementsPage.style.js';
+
+const EndorsementsPage = () => {
+  return (
+    <Wrapper>
+      <Endorsements />
+    </Wrapper>
+  );
+};
+
+export default EndorsementsPage;
