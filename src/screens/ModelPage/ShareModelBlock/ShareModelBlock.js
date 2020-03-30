273f4a87b268028ff23dbb0ae63f04d2bf3d7297
Fiskell <stephen.fiskell@gmail.com>
2020-03-30 16:09:37 -0700

    Move display name up

--- src/screens/ModelPage/ShareModelBlock/ShareModelBlock.js
+++ src/screens/ModelPage/ShareModelBlock/ShareModelBlock.js
@@ -11,8 +11,12 @@ import { ShareContainer, ShareSpacer } from './../ModelPage.style';
 import Callout from 'components/Callout/Callout';
 import { STATES } from 'enums';
 
-const ShareModelBlock = ({ location }) => {
+const ShareModelBlock = ({ location, county }) => {
   const locationName = STATES[location];
+  const countyName = county && county.county;
+  const displayName = countyName
+    ? `${countyName}, ${locationName}`
+    : locationName;
   const shareURL = `https://covidactnow.org/state/${location}`;
   const shareQuote = `This is the point of no return for intervention to prevent ${locationName}'s hospital system from being overloaded by Coronavirus: `;
   const hashtag = 'COVIDActNow';
@@ -22,7 +26,7 @@ const ShareModelBlock = ({ location }) => {
       <ShareContainer>
         <div
           style={{ paddingRight: 28, fontWeight: 'bold' }}
-        >{`Share ${locationName}'s COVID-19 trends:`}</div>
+        >{`Share ${displayName}'s COVID-19 trends:`}</div>
 
         <FacebookShareButton url={shareURL} quote={shareQuote}>
           <FacebookIcon size={40} round={false} borderRadius={5} />
