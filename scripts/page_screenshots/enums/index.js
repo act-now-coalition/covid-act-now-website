58e71c13645d52352ff0200ce4faf6125df41a8d
Zack Rosen <zack@stitchlabs.com>
2020-03-30 23:20:52 -0400

    add screenshot script

---
+++ scripts/page_screenshots/enums/index.js
@@ -0,0 +1,13 @@
+const { map } = require('lodash');
+const { US_STATES } = require('./../../enums');
+
+module.exports = {
+  HOME_PAGE: {
+    SELECTOR: '.us-state-map',
+    DATASET: '/',
+  },
+  STATE_PAGES: {
+    SELECTOR: '.highcharts-container',
+    DATASET: map(US_STATES, (_, stateCode) => `/state/${stateCode}`),
+  }
+}
