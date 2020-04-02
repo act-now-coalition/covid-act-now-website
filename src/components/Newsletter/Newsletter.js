93b586aa2cc1cdef946eccdb1078a33dada45b54
Mike McDonald <mike@stedi.com>
2020-04-02 02:21:39 -0600

    testing sumo, looks like they keys aren't right?

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -14,6 +14,12 @@ const Newsletter = () => {
         src="https://app.mailjet.com/widget/iframe/1dtU/pZV"
         width="100%"
       ></iframe>
+      <button class="sumome-listbuilder-id-93b0ee9a31c30132f8c3e6b586feac9f4904126edcae1426f98bced80950ea58">
+        Click me!
+      </button>
+      <button data-sumome-listbuilder-embed-id="93b0ee9a31c30132f8c3e6b586feac9f4904126edcae1426f98bced80950ea58">
+        Click me to test Sumo!
+      </button>
     </StyledNewsletter>
   );
 };
