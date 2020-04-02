9c5a728543ed8c9f5acb68f634cee0c3dabdb2f4
Mike McDonald <mike@stedi.com>
2020-04-02 10:28:03 -0600

    adding an email signup to the model page

--- src/components/Newsletter/Newsletter.js
+++ src/components/Newsletter/Newsletter.js
@@ -14,7 +14,7 @@ const Newsletter = () => {
         src="https://app.mailjet.com/widget/iframe/1dtU/pZV"
         width="100%"
       ></iframe> */}
-      <span data-sumome-listbuilder-embed-id="93b0ee9a31c30132f8c3e6b586feac9f4904126edcae1426f98bced80950ea58"></span>
+      <span data-sumome-listbuilder-embed-id="new-2"></span>
     </StyledNewsletter>
   );
 };
