ad9e5b16df27b225ad1db95a366b790accbff2ae
Adnan Pirzada <ad.pirz@gmail.com>
2020-04-03 10:53:07 -0700

    Temp share disabled

--- src/screens/Embed/EmbedFooter.js
+++ src/screens/Embed/EmbedFooter.js
@@ -23,9 +23,9 @@ export default function EmbedFooter({ onShare }) {
         >
           More&nbsp;
         </IconButton>
-        <IconButton onClick={onShare} icon={<ShareIcon />} iconPosition="left">
+        {/* <IconButton onClick={onShare} icon={<ShareIcon />} iconPosition="left">
           &nbsp;Share
-        </IconButton>
+        </IconButton> */}
       </FooterButtonContainer>
       <FooterSignature>
         <FooterLogoWrapper>
