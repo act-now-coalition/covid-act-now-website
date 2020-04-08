e012982ca40fab0d06be03bc786cd3b226f24423
AJ Palkovic <aj@discordapp.com>
2020-04-07 21:49:28 -0700

    more ci fixes?

--- cypress/integration/ModelPage_spec.js
+++ cypress/integration/ModelPage_spec.js
@@ -1,11 +1,11 @@
 describe('/state/:id', () => {
   it(`Stay At Home has the correct title & description`, () => {
     cy.visit('/state/NY');
-    cy.title().should('eq', 'Keep staying at home in New York.');
+    cy.title().should('eq', 'New York Forecast - Covid Act Now');
     cy.get('head meta[name="description"]').should(
       'have.attr',
       'content',
-      'Avoiding hospital overload depends heavily on your cooperation.',
+      'New York: Keep staying at home to protect against the COVID-19 outbreak.',
     );
   });
 
@@ -13,20 +13,20 @@ describe('/state/:id', () => {
   // (and may ultimately need to be removed).
   it(`Limited Action has the correct title & description`, () => {
     cy.visit('/state/SD');
-    cy.title().should('eq', 'You must act now in South Dakota!');
+    cy.title().should('eq', 'South Dakota Forecast - Covid Act Now');
     cy.get('head meta[name="description"]').should(
       'have.attr',
       'content',
-      'To prevent hospital overload, our projections indicate a Stay at home order must be implemented soon.',
+      'South Dakota: Urge your public officials to act now against the COVID-19 outbreak!',
     );
   });
   it(`Social Distancing has the correct title & description`, () => {
     cy.visit('/state/ND');
-    cy.title().should('eq', 'You must act now in North Dakota!');
+    cy.title().should('eq', 'North Dakota Forecast - Covid Act Now');
     cy.get('head meta[name="description"]').should(
       'have.attr',
       'content',
-      'To prevent hospital overload, our projections indicate a Stay at home order must be implemented soon.',
+      'North Dakota: Urge your public officials to act now against the COVID-19 outbreak!',
     );
   });
 });
