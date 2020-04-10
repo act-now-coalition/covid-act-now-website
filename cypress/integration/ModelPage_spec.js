2e673cd51fe36db2e8bb890687ebca57eda9bd33
Zack Rosen <zack@stitchlabs.com>
2020-04-09 22:46:05 -0400

    update cypress tests

--- cypress/integration/ModelPage_spec.js
+++ cypress/integration/ModelPage_spec.js
@@ -1,32 +1,33 @@
 describe('/state/:id', () => {
   it(`Stay At Home has the correct title & description`, () => {
-    cy.visit('/state/NY');
-    cy.title().should('eq', 'Keep staying at home in New York.');
+    cy.visit('/us/ny');
+    cy.title().should('eq', 'New York Forecast - Covid Act Now');
     cy.get('head meta[name="description"]').should(
       'have.attr',
       'content',
-      'Avoiding hospital overload depends heavily on your cooperation.',
+      'New York: Keep staying at home to protect against the COVID-19 outbreak.',
     );
   });
 
   // NOTE: These tests will need to be updated as states change their status
   // (and may ultimately need to be removed).
   it(`Limited Action has the correct title & description`, () => {
-    cy.visit('/state/SD');
-    cy.title().should('eq', 'You must act now in South Dakota!');
+    cy.visit('/us/sd');
+    cy.title().should('eq', 'South Dakota Forecast - Covid Act Now');
     cy.get('head meta[name="description"]').should(
       'have.attr',
       'content',
-      'To prevent hospital overload, our projections indicate a Stay at home order must be implemented soon.',
+      'South Dakota: Urge your public officials to act now against the COVID-19 outbreak!',
     );
   });
+
   it(`Social Distancing has the correct title & description`, () => {
-    cy.visit('/state/ND');
-    cy.title().should('eq', 'You must act now in North Dakota!');
+    cy.visit('/us/nd');
+    cy.title().should('eq', 'North Dakota Forecast - Covid Act Now');
     cy.get('head meta[name="description"]').should(
       'have.attr',
       'content',
-      'To prevent hospital overload, our projections indicate a Stay at home order must be implemented soon.',
+      'North Dakota: Urge your public officials to act now against the COVID-19 outbreak!',
     );
   });
 });
