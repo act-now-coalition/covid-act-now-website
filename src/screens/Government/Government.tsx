c36a0bf2bb9ca775ffc1e996ca076d8f3c96d88c
Dan Schlosser <dan.r.schlosser@gmail.com>
2020-04-19 00:26:36 -0400

    bold numbers

--- src/screens/Government/Government.tsx
+++ src/screens/Government/Government.tsx
@@ -25,29 +25,37 @@ const Government = ({ children }: { children: React.ReactNode }) => {
           </Typography>
           <Typography variant="body1" component="p">
             Our goal is to provide you with data, modeling, and analysis to help
-            your government manage its response to COVID. Specifically we can:
+            your government manage its response to COVID. Specifically you can:
           </Typography>
           <ol>
             <li>
               <Typography variant="body1" component="p">
-                Work together to improve the data inputs for your state- or
-                county-specific COVID model: Our model is only as good as its
-                data. If you have different or higher quality data, we can work
-                together to ingest that data into the model and create better,
-                more accurate COVID projections to help your government plan its
-                response. Connect with us at{' '}
+                <strong>
+                  Work with us to improve the data inputs for your state- or
+                  county-specific COVID model
+                </strong>{' '}
+                <br />
+                Our model is only as good as its data. If you have different or
+                higher quality data, we can work together to ingest that data
+                into the model and create better, more accurate COVID
+                projections to help your government plan its response. Connect
+                with us at{' '}
                 <a href="mailto:gov@covidactnow.org">gov@covidactnow.org</a>.
               </Typography>
             </li>
             <li>
               <Typography variant="body1" component="p">
-                Use our API: You can find our API here. It allows you to
-                download our data and specify the level of intervention for your
-                state or county. In the upcoming weeks, we intend to continue
-                adding features including but not limited to: state level
-                aggregation (return information about all states) and county
-                level aggregation (return all county level data for a given
-                state.
+                <strong>Use our API</strong> <br />
+                You can find our API{' '}
+                <a href="https://github.com/covid-projections/covid-data-model/blob/master/api/README.V1.md">
+                  here
+                </a>
+                . It allows you to download our data and specify the level of
+                intervention for your state or county. In the upcoming weeks, we
+                intend to continue adding features including but not limited to:
+                state level aggregation (return information about all states)
+                and county level aggregation (return all county level data for a
+                given state.
               </Typography>
             </li>
           </ol>
