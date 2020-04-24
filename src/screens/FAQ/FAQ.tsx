e9500cb07fda4ba636a2d7b7898ab0ea3954ca69
Adnan Pirzada <ad.pirz@gmail.com>
2020-04-23 20:25:51 -0700

    Close breaking tag + add proper spacing

--- src/screens/FAQ/FAQ.tsx
+++ src/screens/FAQ/FAQ.tsx
@@ -73,17 +73,17 @@ const FAQ = ({ children }: { children: React.ReactNode }) => {
           >
             Yes! The model is public
           </a>
-          . 
-          <br><br>
-          You can also get the output of the model via our 
+          .
+          <br />
+          You can also get the output of the model via our{' '}
           <a
             target="_blank"
             rel="noopener noreferrer"
             href="https://github.com/covid-projections/covid-data-model/blob/master/api/README.V1.md"
           >
             API
-          </a>
-           or 
+          </a>{' '}
+          or{' '}
           <a
             target="_blank"
             rel="noopener noreferrer"
