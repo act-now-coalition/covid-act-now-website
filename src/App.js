9e7ffcd77caf2a337c3fcdc21548376554c93e3a
Dan Schlosser <dan.r.schlosser@gmail.com>
2020-04-27 11:51:32 -0400

    finish products removal

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ModelPage from 'screens/ModelPage/ModelPage';
 import HomePage from 'screens/HomePage/HomePage';
 import About from 'screens/About/About';
 // import ComingSoon from 'screens/ComingSoon/ComingSoon';
-import Products from 'screens/Products/Products';
+import Resources from 'screens/Resources/Resources';
 import Contact from 'screens/Contact/Contact';
 import Terms from 'screens/Terms/Terms';
 import Privacy from 'screens/Terms/Privacy';
@@ -42,7 +42,7 @@ export default function App() {
             />
 
             <Route path="/about" component={About} />
-            <Route path="/products" component={Products} />
+            <Route path="/resources" component={Resources} />
             <Route path="/contact" component={Contact} />
             <Route path="/terms" component={Terms} />
             <Route path="/privacy" component={Privacy} />
