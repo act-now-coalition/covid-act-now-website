6854a4ad53c2182681bf558f30c1935544b46f7d
Michael Lehenbauer <mikelehen@gmail.com>
2020-05-01 10:15:29 -0700

    Add 7-day trailing average text to disclaimer under positive rate chart.

--- src/components/LocationPage/ChartsHolder.tsx
+++ src/components/LocationPage/ChartsHolder.tsx
@@ -109,6 +109,7 @@ const ChartsHolder = (props: {
                   The World Health Organization recommends a positive test rate
                   of less than 10% before reopening. The countries most
                   successful in containing COVID have rates of 3% or less.
+                  We calculate the rate as a 7-day trailing average.
                 </Disclaimer>
               </>
             )}
