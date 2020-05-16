9a8ed0069f41aabcd2f13cea794838d22e6b9c93
Michael Lehenbauer <mikelehen@gmail.com>
2020-05-15 17:10:00 -0700

    Only do 10 screenshots at a time.

--- scripts/share_images/index.ts
+++ scripts/share_images/index.ts
@@ -2,7 +2,6 @@
 import fs from 'fs-extra';
 import path from 'path';
 import Pageres from 'pageres';
-import PromisePool from 'es6-promise-pool';
 import CalculatedInterventionJSON from '../../src/assets/data/calculated_state_interventions.json';
 import CalculatedCountyInterventionJSON from '../../src/assets/data/calculated_county_interventions.json';
 import _ from 'lodash';
@@ -11,6 +10,8 @@ const BASE_URL = 'http://localhost:3000/internal/share-image';
 const CSS_SELECTOR = '.screenshot';
 const OUTPUT_DIR = path.join(__dirname, 'output');
 const OUTPUT_SIZE = '1200x630';
+// How many screenshots to send to pageres at once.
+const PAGERES_BATCH_SIZE = 10;
 
 (async () => {
   await fs.ensureDir(OUTPUT_DIR);
@@ -18,25 +19,41 @@ const OUTPUT_SIZE = '1200x630';
   await fs.emptyDir(`${OUTPUT_DIR}/states`);
   await fs.emptyDir(`${OUTPUT_DIR}/counties`);
 
-  const pageres = new Pageres();
+  let screenshots = [] as Array<{ url: string; filename: string }>;
 
-  addSrc(pageres, '/', 'home');
+  screenshots.push({ url: '/', filename: 'home' });
 
   for (const stateCode in CalculatedInterventionJSON) {
     const state = stateCode.toLowerCase();
-    addSrc(pageres, `/states/${state}`);
+    screenshots.push({ url: `/states/${state}`, filename: `/states/${state}` });
   }
 
   for (const fips in CalculatedCountyInterventionJSON) {
-    addSrc(pageres, `/counties/${fips}`);
+    screenshots.push({
+      url: `/counties/${fips}`,
+      filename: `/counties/${fips}`,
+    });
   }
 
-  await pageres.dest(OUTPUT_DIR).run();
+  // For testing, just do first 101 screenshots.
+  screenshots = screenshots.slice(0, 101);
+
+  while (screenshots.length > 0) {
+    const pageres = new Pageres().dest(OUTPUT_DIR);
+    const urls = [];
+    const batchSize = Math.min(PAGERES_BATCH_SIZE, screenshots.length);
+    for (let i = 0; i < batchSize; i++) {
+      const s = screenshots.pop()!;
+      urls.push(s.url);
+      pageres.src(BASE_URL + s.url, [OUTPUT_SIZE], {
+        selector: CSS_SELECTOR,
+        filename: s.filename,
+      });
+    }
+    console.log('Screenshotting:', urls.join(', '));
+    await pageres.run();
+  }
 
   console.log('Completed successfully.');
   process.exit(0);
 })();
-
-async function addSrc(pageres: Pageres, url: string, filename: string = url) {
-  pageres.src(BASE_URL + url, [OUTPUT_SIZE], { selector: CSS_SELECTOR, filename});
-}
