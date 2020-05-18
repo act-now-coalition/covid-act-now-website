75fd677a5b57d48b0a732568895a2e58da82f4e5
Adnan Pirzada <ad.pirz@gmail.com>
2020-05-18 12:53:05 -0700

    Return appropriate status badge icons for contact tracing

---
+++ src/components/LocationPage/ScrollToTopButton.style.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+
+const StyledScrollToTopButton = styled(Button)`
+  position: fixed;
+  top: 200;
+  left: 20vh;
+  z-index: 100;
+`;
+
+export default StyledScrollToTopButton;
