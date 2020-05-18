86e044804a5be0fbf54705eaa434dab2bdbb5961
Adnan Pirzada <ad.pirz@gmail.com>
2020-05-18 12:53:05 -0700

    Return appropriate status badge icons for contact tracing

--- src/components/SignalStatus/SignalStatus.tsx
+++ src/components/SignalStatus/SignalStatus.tsx
@@ -7,26 +7,32 @@ import CheckIcon from 'assets/images/checkIcon';
 import ExclamationIcon from 'assets/images/exclamationIcon';
 import QuestionIcon from 'assets/images/questionIcon';
 
-const getIcon = function (levelInfo: LevelInfo) {
+const getIcon = function (levelInfo: LevelInfo, flipOrder?: Boolean) {
   switch (levelInfo.level) {
     case Level.LOW:
+      if (flipOrder) return <ExclamationIcon textColor={levelInfo.color} />;
       return <CheckIcon textColor={levelInfo.color} />;
     case Level.MEDIUM:
       return <TildeIcon textColor={levelInfo.color} />;
     case Level.HIGH:
+      if (flipOrder) return <CheckIcon textColor={levelInfo.color} />;
       return <ExclamationIcon textColor={levelInfo.color} />;
     case Level.UNKNOWN:
       return <QuestionIcon textColor={levelInfo.color} />;
   }
 };
 
-const SignalStatus = (props: { levelInfo: LevelInfo; condensed?: Boolean }) => {
+const SignalStatus = (props: {
+  levelInfo: LevelInfo;
+  condensed?: Boolean;
+  flipOrder?: Boolean;
+}) => {
   return (
     <SignalStatusWrapper
       color={props.levelInfo.color}
       condensed={props.condensed}
     >
-      {getIcon(props.levelInfo)}
+      {getIcon(props.levelInfo, props.flipOrder || false)}
       <span>{props.levelInfo.name}</span>
     </SignalStatusWrapper>
   );
