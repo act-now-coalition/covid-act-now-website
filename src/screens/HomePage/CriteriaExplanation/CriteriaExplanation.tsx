ceb3d6ab14fe4f76066051e8cd484ecc5c3bd86e
jamestamplin <jamestamplin@users.noreply.github.com>
2020-05-22 12:30:09 -0700

    Criterion => Indicator

--- src/screens/HomePage/CriteriaExplanation/CriteriaExplanation.tsx
+++ src/screens/HomePage/CriteriaExplanation/CriteriaExplanation.tsx
@@ -13,21 +13,21 @@ const CriteriaExplanation = () => {
     <Wrapper>
       <CriteriaList>
         <Criterion>
-          <Kicker>Criterion 1</Kicker>
+          <Kicker>Indicator 1</Kicker>
           <CriterionHeader>Is COVID spreading?</CriterionHeader>
           <CriterionDescription>
             Are COVID cases and deaths increasing?{' '}
           </CriterionDescription>
         </Criterion>
         <Criterion>
-          <Kicker>Criterion 2</Kicker>
+          <Kicker>Indicator 2</Kicker>
           <CriterionHeader>Are we testing enough?</CriterionHeader>
           <CriterionDescription>
             Is COVID testing widespread enough to identify new cases?
           </CriterionDescription>
         </Criterion>
         <Criterion>
-          <Kicker>Criterion 3</Kicker>
+          <Kicker>Indicator 3</Kicker>
           <CriterionHeader>Are our hospitals ready?</CriterionHeader>
           <CriterionDescription>
             Do hospitals have capacity to treat a surge of COVID
@@ -35,7 +35,7 @@ const CriteriaExplanation = () => {
           </CriterionDescription>
         </Criterion>
         <Criterion>
-          <Kicker>Criterion 4</Kicker>
+          <Kicker>Indicator 4</Kicker>
           <CriterionHeader>Are we tracing fast enough?</CriterionHeader>
           <CriterionDescription>
             Are we finding and isolating new cases before COVID spreads?
