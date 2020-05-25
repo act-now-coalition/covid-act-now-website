915f0a1f9938e9bcea50e8b54c02385ba5ac7cb5
Michael Lehenbauer <mikelehen@gmail.com>
2020-05-24 21:19:21 -0700

    Create MetricChart helper component and use it in a few places.

--- src/components/Charts/index.tsx
+++ src/components/Charts/index.tsx
@@ -1,3 +1,4 @@
+export { default as MetricChart } from './MetricChart';
 export { default as ChartRt } from './ChartRt';
 export { default as ChartZones } from './ChartZones';
 export { default as ChartPositiveTestRate } from './ChartPositiveTestRate';
