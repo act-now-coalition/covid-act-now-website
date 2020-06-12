f67b200a772edb338c6e9bdccf9dad9450284b26
Max Henderson <maxwhenderson@users.noreply.github.com>
2020-06-12 16:45:03 -0700

    Final tweaks to decomp
    Final tweaks to temporary static decomp %s

--- src/common/models/Projection.ts
+++ src/common/models/Projection.ts
@@ -29,16 +29,16 @@ export const TRACERS_NEEDED_PER_CASE = 5;
  * get from the API to account for hospitals being able to decrease their utilization
  * (by cancelling elective surgeries, using surge capacity, etc.).
  */
-const ICU_DECOMP_FACTOR = 0.2;
+const ICU_DECOMP_FACTOR = 0.21;
 const ICU_DECOMP_FACTOR_STATE_OVERRIDES: { [key: string]: number } = {
   Alabama: 0.15,
   Arizona: 0,
-  Delaware: 0.25,
-  'District of Columbia': 0.05,
-  Georgia: 0.15,
-  Mississippi: 0.15,
+  Delaware: 0.3,
+  'District of Columbia': 0.025,
+  Georgia: 0.10,
+  Mississippi: 0.12,
   Nevada: 0.25,
-  'Rhode Island': 0.1,
+  'Rhode Island': 0,
 };
 
 const DEFAULT_UTILIZATION = 0.75;
