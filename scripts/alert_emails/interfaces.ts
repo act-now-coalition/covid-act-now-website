3575d5277f10bc60d56fcf10098fff18e33315fc
Susan G <suzgoldblatt@gmail.com>
2020-06-29 11:05:32 -0700

    initial email sending work (#948)
    * sending an individual email via Campaign monitor
    * Add Firestore and Github Actions stuff for alerts emails. (#977)
    
    Co-authored-by: Susan Goldblatt <sgoldblatt@Susans-MacBook-Pro.local>
    Co-authored-by: Michael Lehenbauer <michael@covidactnow.org>

---
+++ scripts/alert_emails/interfaces.ts
@@ -0,0 +1,11 @@
+import { Level } from '../../src/common/level';
+import { Metric } from '../../src/common/metric';
+
+export interface Alert {
+  fips: string;
+  locationName: string;
+  locationURL: string;
+  lastUpdated: string;
+  oldLevel: Level;
+  newLevel: Level;
+}
