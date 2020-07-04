b2f0a32baeda207e7e50091eaf11b0f073ad764a
Pablo Navarro <pnavarrc@gmail.com>
2020-07-03 18:49:19 -0700

    Update threat level names (#1016)
    * rename HIGH to CRITICAL
    * rename MEDIUM_HIGH to HIGH

--- src/common/metrics/location_summary.ts
+++ src/common/metrics/location_summary.ts
@@ -25,16 +25,16 @@ export const LOCATION_SUMMARY_LEVELS: LevelInfoMap = {
     detail: locationName =>
       `COVID in ${locationName} is spreading in a slow and controlled fashion, and ${locationName}’s COVID preparedness meets international standards. If this trend continues, ${locationName} may eventually achieve herd immunity, though this may take years.`,
   },
-  [Level.MEDIUM_HIGH]: {
-    level: Level.MEDIUM_HIGH,
+  [Level.HIGH]: {
+    level: Level.HIGH,
     upperLimit: 0,
     name: MEDIUM_HIGH_NAME,
     color: COLOR_MAP.ORANGE_DARK.BASE,
     detail: locationName =>
       `${locationName} is at risk of an outbreak. COVID cases are either increasing at a rate likely to overwhelm hospitals and/or the state’s COVID preparedness is well below international standards.`,
   },
-  [Level.HIGH]: {
-    level: Level.HIGH,
+  [Level.CRITICAL]: {
+    level: Level.CRITICAL,
     upperLimit: 0,
     name: HIGH_NAME,
     color: COLOR_MAP.RED.BASE,
