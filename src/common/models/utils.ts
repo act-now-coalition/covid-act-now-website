7421a66602502c62bca80924a44316c9d9b2b11f
Michael Lehenbauer <michael@covidactnow.org>
2020-07-06 13:58:57 -0700

    Use actuals where available in ICU Headroom metric; update AZ and DC decomp. (#1011)
    This uses ICUBeds.currentUsageCovid and/or ICUBeds.currentUsageTotal when available in the API to calculate the ICU Headroom metric. Since either one or both may be available, there are 4 possible combinations, and this code handles all of them and subtly includes the result in the copy to make it possible to decode the calculation.
    
    1. If neither are available, you get the existing site behavior where both covid and non-covid patients are estimates:
    2. If both are available, then we omit "we estimate" in both places:
    3. If only ICUBeds.currentUsageCovid is available, then non-covid patients is an estimate and we include the typical utilization + decomp percentage that we're using to estimate it.
    4. If only ICUBeds.currentUsageTotal is available, then both covid and non-covid patients are estimates but we omit the percentage for non-covid patients, since we can avoid using the typical utilization+decomp percentage.
    
    I also updated AZ decomp to 40% to better match up with actuals from their dashboard, as well as DC decomp to 15% to match up with their published numbers.

---
+++ src/common/models/utils.ts
@@ -0,0 +1,54 @@
+import _ from 'lodash';
+
+// TODO(michael): We should perhaps create a Timeseries class that wraps an
+// Array<Date> and Array<number | null> and hang these utility functions off of
+// that.
+
+export function indexOfLastValue<T>(data: Array<T | null>): number | null {
+  for (let i = data.length - 1; i >= 0; i--) {
+    if (data[i] !== null && data[i] !== undefined) {
+      return i;
+    }
+  }
+  return null;
+}
+
+export function lastValue<T>(data: Array<T | null>): T | null {
+  const i = indexOfLastValue(data);
+  return i === null ? null : data[i]!;
+}
+
+/**
+ * Replaces all values after the specified date with nulls.
+ */
+export function omitDataAfterDate<T>(
+  data: Array<T | null> | Array<T>,
+  dates: Array<Date>,
+  cutoffDate: Date,
+): Array<T | null> {
+  return dates.map((date, idx) => {
+    const value = data[idx];
+    return date <= cutoffDate ? value : null;
+  });
+}
+
+/**
+ * Returns true if the series has at least 7 non-null values in the last 14
+ * days and has ever had a non-zero value.
+ */
+export function hasRecentData(
+  data: Array<number | null>,
+  dates: Array<Date>,
+): boolean {
+  const currentDateIndex = _.findLastIndex(dates, date => date <= new Date());
+  const twoWeeksAgoIndex = Math.max(0, currentDateIndex - 13);
+  const lastTwoWeeksData = data.slice(twoWeeksAgoIndex, currentDateIndex + 1);
+  const dataPointsInLastTwoWeeks = _.filter(
+    lastTwoWeeksData,
+    value => value != null,
+  ).length;
+  const hasNonZeroDataPoint =
+    _.find(data, value => value != null && value > 0) != null;
+
+  return dataPointsInLastTwoWeeks >= 7 && hasNonZeroDataPoint;
+}
