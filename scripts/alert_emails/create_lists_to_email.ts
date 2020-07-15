20938da85139e003b1b80ffb79370709c266089a
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-07-14 18:04:30 -0700

    lint create_lists_to_email.ts

--- scripts/alert_emails/create_lists_to_email.ts
+++ scripts/alert_emails/create_lists_to_email.ts
@@ -17,43 +17,55 @@ import { exit } from 'process';
  * You can run via `yarn create-lists-to-email fipsToAlertFilename currentSnapshot`
  */
 (async () => {
-    const outputFolder = path.join(__dirname);
-    await fs.ensureDir(outputFolder);
+  const outputFolder = path.join(__dirname);
+  await fs.ensureDir(outputFolder);
 
-    const {fipsToAlertFilename, currentSnapshot} = await parseArgs();
-    const alertPath = path.join(outputFolder, fipsToAlertFilename);
+  const { fipsToAlertFilename, currentSnapshot } = await parseArgs();
+  const alertPath = path.join(outputFolder, fipsToAlertFilename);
 
-    const rawdata = fs.readFileSync(alertPath, "utf8");
-    const locationsWithAlerts: { [fips: string]: Alert } = JSON.parse(rawdata);
+  const rawdata = fs.readFileSync(alertPath, 'utf8');
+  const locationsWithAlerts: { [fips: string]: Alert } = JSON.parse(rawdata);
 
-    const db = getFirestore();
-    Object.keys(locationsWithAlerts).forEach(async (fips: string) => {
-        db.collection('alerts-subscriptions').where("locations", "array-contains", fips).get()
-            .then( (querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    db.collection('snapshots').doc(`${currentSnapshot}/locations/${fips}/emails/${doc.id}`).set({
-                        sentAt:null,
-                    });
-                });
-                return;
-            }).catch((err: any) => {console.log(err)});
-    });
+  const db = getFirestore();
+  Object.keys(locationsWithAlerts).forEach(async (fips: string) => {
+    db.collection('alerts-subscriptions')
+      .where('locations', 'array-contains', fips)
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          db.collection('snapshots')
+            .doc(`${currentSnapshot}/locations/${fips}/emails/${doc.id}`)
+            .set({
+              sentAt: null,
+            });
+        });
+        return;
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
+  });
 
-    console.log(`Done. Wrote users to firestore`);
+  console.log(`Done. Wrote users to firestore`);
 })();
 
-async function parseArgs(): Promise<{fipsToAlertFilename: string, currentSnapshot: string}> {
-    const args = process.argv.slice(2);
-     if (args.length == 2) {
-        const fipsToAlertFilename = args[0];
-        const currentSnapshot = args[1];
-        return {fipsToAlertFilename, currentSnapshot};
-    } else {
-        exitWithUsage();
-    }
+async function parseArgs(): Promise<{
+  fipsToAlertFilename: string;
+  currentSnapshot: string;
+}> {
+  const args = process.argv.slice(2);
+  if (args.length == 2) {
+    const fipsToAlertFilename = args[0];
+    const currentSnapshot = args[1];
+    return { fipsToAlertFilename, currentSnapshot };
+  } else {
+    exitWithUsage();
   }
+}
 
 function exitWithUsage(): never {
-    console.log('Usage: yarn create-lists-to-email fipsToAlertFilename currentSnapshot');
-    process.exit(-1);
+  console.log(
+    'Usage: yarn create-lists-to-email fipsToAlertFilename currentSnapshot',
+  );
+  process.exit(-1);
 }
