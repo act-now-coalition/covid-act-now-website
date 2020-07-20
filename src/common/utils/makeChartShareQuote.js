177e792745f07f8ba0f3bd17cc9e40a9b3e38105
chasulin <44076375+chasulin@users.noreply.github.com>
2020-07-20 19:50:53 -0400

    fixes share quotes, adds case density share quote

--- src/common/utils/makeChartShareQuote.js
+++ src/common/utils/makeChartShareQuote.js
@@ -13,31 +13,36 @@ export default function makeChartShareQuote(
   const countyName = county && county.county;
   const displayName = countyName ? `${countyName}, ${stateName}` : stateName;
 
-  if (chartIdentifier === '0') {
+  if (chartIdentifier === 0) {
     return `${displayName}’s infection growth rate is ${formatDecimal(
       stats[0],
     )}, according to @CovidActNow. See the chart: `;
-  } else if (chartIdentifier === '1') {
+  } else if (chartIdentifier === 1) {
     return `${formatPercent(
       stats[1],
       1,
     )} of those tested for COVID in ${displayName} tested positive, according to @CovidActNow. See the chart: `;
-  } else if (chartIdentifier === '2') {
+  } else if (chartIdentifier === 2) {
     return `${displayName}'s available ICU capacity is ${formatPercent(
       stats[2],
     )} full with COVID patients, according to @CovidActNow. See the chart: `;
-  } else if (chartIdentifier === '3') {
+  } else if (chartIdentifier === 3) {
     return `${displayName} is tracing only ${formatPercent(
       stats[3],
       0,
     )} of COVID cases, according to @CovidActNow. See the chart: `;
-  } else if (chartIdentifier === '4') {
+  } else if (chartIdentifier === 4) {
     if (projections && projections.baseline.dateOverwhelmed) {
       return `If all restrictions were completely lifted today, ${displayName}'s hospitals would overload on ${formatUtcDate(
         projections.baseline.dateOverwhelmed,
       )}, according to @CovidActNow. See the chart: `;
     }
     return `Assuming current trends and interventions continue, ${displayName}'s hospitals are unlikely to become overloaded in the next 3 months, according to @CovidActNow. See the chart: `;
+  } else if (chartIdentifier === 5) {
+    return `${displayName} is seeing ${formatDecimal(
+      stats[5],
+      1,
+    )} new cases per day per 100k people, according to @CovidActNow. See the chart: `;
   }
   return `I'm keeping track of ${displayName}'s COVID data and risk level with @CovidActNow. What does your community look like?`;
 }
