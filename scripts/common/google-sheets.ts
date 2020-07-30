fb0e0f76f78ddef64ecceb8b51be25d756623762
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-07-29 21:46:00 -0700

    wip

--- scripts/common/google-sheets.ts
+++ scripts/common/google-sheets.ts
@@ -1,4 +1,30 @@
-import { google } from 'googleapis';
+import { google, sheets_v4 } from 'googleapis';
+
+const spreadsheetId = '1cs3Wyh8Gda0H18_-x5RYp7AJ3FwB-I1ewAQTBWEk1YY';
+
+class GoogleSpreadsheet {
+  // path to the google service account file
+  keyFile: string;
+  auth: any;
+  sheets: sheets_v4.Sheets;
+  constructor(keyFile: string) {
+    // TODO(pablo): check for the file
+    this.keyFile = keyFile;
+  }
+
+  async authenticate() {
+    // TODO(pablo): handle failure
+    this.auth = await google.auth.getClient({
+      keyFile: this.keyFile,
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+    this.sheets = google.sheets('v4');
+  }
+
+  async appendRows(spreadsheetId: string, range: string, rows: any) {
+    // const response = await this.sheets.spreadsheets.values.append();
+  }
+}
 
 async function main() {
   // TODO
@@ -8,7 +34,6 @@ async function main() {
   });
 
   const sheets = google.sheets('v4');
-  const spreadsheetId = '1cs3Wyh8Gda0H18_-x5RYp7AJ3FwB-I1ewAQTBWEk1YY';
 
   const res = await sheets.spreadsheets.values.get({
     auth,
