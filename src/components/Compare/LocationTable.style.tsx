ad210a3d85ac82b27a84416ec031e8711c798255
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-08-06 12:35:35 -0700

    fix the layout of the inline table

--- src/components/Compare/LocationTable.style.tsx
+++ src/components/Compare/LocationTable.style.tsx
@@ -2,19 +2,23 @@ import styled from 'styled-components';
 import { TableHead } from '@material-ui/core';
 import { Cell } from './Compare.style';
 
-export const Container = styled.div`
+export const ModalContainer = styled.div`
   display: flex;
   flex-direction: column;
   height: 400px;
 `;
 
-export const Header = styled.div`
+export const ModalBody = styled.div`
+  flex: 1 0 auto;
+`;
+
+export const Container = styled.div``;
+
+export const Head = styled.div`
   flex: 0 0 auto;
 `;
 
-export const Body = styled.div`
-  flex: 1 0 auto;
-`;
+export const Body = styled.div``;
 
 export const ScrollContainer = styled.div`
   overflow: scroll;
