e2beb89ea11c5d21856614c6b904d6e65bb8900a
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-08-06 16:43:37 -0700

    fix scrolling on narrow viewports

--- src/components/Compare/Compare.style.tsx
+++ src/components/Compare/Compare.style.tsx
@@ -89,8 +89,10 @@ export const MetricCell = styled.td<{
   padding-top: 0.75rem;
   padding-bottom: 0.75rem;
   cursor: pointer;
+  min-width: 100px;
 
   &:first-child {
+    min-width: 130px;
     &:hover {
       color: ${COLOR_MAP.BLUE};
     }
