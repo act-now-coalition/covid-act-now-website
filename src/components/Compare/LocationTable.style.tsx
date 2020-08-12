3fce710158f162ebf739274da624debfda786f3a
chasulin <44076375+chasulin@users.noreply.github.com>
2020-08-11 21:03:43 -0400

    Compare: fixes table scroll (#1248)

--- src/components/Compare/LocationTable.style.tsx
+++ src/components/Compare/LocationTable.style.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
-import { locationNameCellWidth, metricCellWidth } from './Compare.style';
+import { TableHead } from '@material-ui/core';
+import { Cell, locationNameCellWidth, metricCellWidth } from './Compare.style';
+import { COLORS } from 'common';
 
 const minTableWidth = locationNameCellWidth + 5 * metricCellWidth;
 const pinnedBorderColor = '#CEBFAC';
@@ -41,4 +43,24 @@ export const Head = styled.div<{ isModal: boolean }>`
   min-height: 0;
   min-width: ${minTableWidth}px;
   ${props => (props.isModal ? PinnedTop : '')}
+`;
+
+export const TableContainer = styled.div<{ isModal: boolean }>`
+  border: ${({ isModal }) => !isModal && `1px solid ${COLORS.LIGHTGRAY}`};
+`;
+
+export const TableHeadContainer = styled(TableHead)<{ isModal: boolean }>`
+  ${Cell} {
+    vertical-align: bottom;
+    line-height: 1.1rem;
+    padding: 1rem 1rem 0.8rem;
+    background-color: ${({ isModal }) => isModal && 'black'};
+    color: ${({ isModal }) => isModal && 'white'};
+    border-bottom: ${({ isModal }) =>
+      !isModal && `2px solid ${COLORS.LIGHTGRAY}`};
+    &:nth-child(2) {
+      border-left: ${({ isModal }) =>
+        !isModal && `2px solid ${COLORS.LIGHTGRAY}`};
+    }
+  }
 `;
