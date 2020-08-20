08a41e6fdd89acb61f88056b6f89fd7afe7d9785
Alex Peterson <alexcpeterson7@gmail.com>
2020-08-20 14:40:29 -0400

    Enhance Daily New Cases Tooltip (#1300)
    Addresses https://trello.com/c/O9nRsq19/358-implement-new-new-cases-tool-tip

--- src/common/models/Projection.ts
+++ src/common/models/Projection.ts
@@ -77,6 +77,7 @@ export interface RtRange {
 
 export interface CaseDensityRange {
   caseDensity: number;
+  newCases: number | null;
   low: number;
   high: number;
 }
@@ -469,10 +470,15 @@ export class Projection {
   }
 
   private calcCaseDensityRange(): Array<CaseDensityRange | null> {
-    return this.caseDensityByCases.map(caseDensity =>
+    return this.caseDensityByCases.map((caseDensity, indx, arr) =>
       caseDensity === null
         ? null
-        : { caseDensity, low: caseDensity, high: caseDensity },
+        : {
+            caseDensity: caseDensity,
+            newCases: this.smoothedDailyCases[indx],
+            low: caseDensity,
+            high: caseDensity,
+          },
     );
   }
 
