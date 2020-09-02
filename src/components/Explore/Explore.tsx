3bae63e5cbbb48f47271bab9c5441effd6e6143d
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-09-02 08:52:21 -0700

    fix the metric name in the trends subtitle

--- src/components/Explore/Explore.tsx
+++ src/components/Explore/Explore.tsx
@@ -44,6 +44,8 @@ const Explore: React.FunctionComponent<{
   };
 
   const metricLabels = getMetricLabels();
+  const currentMetricName = metricLabels[currentMetric];
+
   const series = getSeries(currentMetric, projection);
   const { locationName, fips } = projection;
 
@@ -59,7 +61,7 @@ const Explore: React.FunctionComponent<{
         <Grid item sm={6} xs={12}>
           <Styles.Heading variant="h4">Trends</Styles.Heading>
           <Styles.Subtitle>
-            cases since march 1st in {locationName}
+            {currentMetricName} since march 1st in {locationName}
           </Styles.Subtitle>
         </Grid>
         <Grid item sm={6} xs={12}>
@@ -100,8 +102,8 @@ const Explore: React.FunctionComponent<{
       ) : (
         <EmptyChart height={400}>
           <p>
-            We don't have {metricLabels[currentMetric]} data for {locationName}.
-            Learn more about{' '}
+            We don't have {currentMetricName} data for {locationName}. Learn
+            more about{' '}
             <ExternalLink href="https://docs.google.com/document/d/1cd_cEpNiIl1TzUJBvw9sHLbrbUZ2qCxgN32IqVLa3Do/edit">
               our methodology
             </ExternalLink>{' '}
