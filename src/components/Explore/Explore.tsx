b6284d82676108419bee5296faed4dc3e7aab077
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-09-03 14:40:52 -0700

    normalize metrics per 100k population

--- src/components/Explore/Explore.tsx
+++ src/components/Explore/Explore.tsx
@@ -2,6 +2,8 @@ import React, { useState, useMemo, useEffect } from 'react';
 import { some, uniq } from 'lodash';
 import Grid from '@material-ui/core/Grid';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import { useTheme } from '@material-ui/core/styles';
 import { ParentSize } from '@vx/responsive';
 import { Projection } from 'common/models/Projection';
@@ -43,6 +45,7 @@ const Explore: React.FunctionComponent<{
   const [currentMetric, setCurrentMetric] = useState(
     (chartId && getMetricByChartId(chartId)) || ExploreMetric.CASES,
   );
+  const [normalizeData, setNormalizeData] = useState(false);
 
   const onChangeTab = (newMetric: number) => setCurrentMetric(newMetric);
 
@@ -66,9 +69,10 @@ const Explore: React.FunctionComponent<{
   };
 
   useEffect(() => {
-    const fetchSeries = () => getChartSeries(currentMetric, selectedLocations);
+    const fetchSeries = () =>
+      getChartSeries(currentMetric, selectedLocations, normalizeData);
     fetchSeries().then(series => setChartSeries(series));
-  }, [selectedLocations, currentMetric]);
+  }, [selectedLocations, currentMetric, normalizeData]);
 
   const hasData = some(chartSeries, ({ data }) => data.length > 0);
   const hasMultipleLocations = selectedLocations.length > 1;
@@ -80,14 +84,14 @@ const Explore: React.FunctionComponent<{
   return (
     <Styles.Container>
       <Grid container spacing={1}>
-        <Grid item sm={6} xs={12}>
+        <Grid item sm={9} xs={12}>
           <Styles.Heading variant="h4">Trends</Styles.Heading>
           <Styles.Subtitle>
-            {currentMetricName} since march 1st in{' '}
-            {getLocationNames(selectedLocations)}
+            {currentMetricName} {normalizeData ? 'per 100k population' : ''}{' '}
+            since march 1st in {getLocationNames(selectedLocations)}
           </Styles.Subtitle>
         </Grid>
-        <Grid item sm={6} xs={12}>
+        <Grid item sm xs={12}>
           <Styles.ShareBlock>
             <ShareImageButtonGroup
               imageUrl={getExportImageUrl(fips, currentMetric)}
@@ -106,7 +110,7 @@ const Explore: React.FunctionComponent<{
       />
       <Styles.ChartControlsContainer>
         <Grid container spacing={1}>
-          <Grid key="location-selector" item sm={6} xs={6}>
+          <Grid key="location-selector" item sm={9} xs={6}>
             <LocationSelector
               locations={autocompleteLocations}
               selectedLocations={selectedLocations}
@@ -116,6 +120,24 @@ const Explore: React.FunctionComponent<{
           {!hasMultipleLocations && (
             <Grid key="legend" item sm xs={12}>
               <Legend series={chartSeries} />
+            </Grid>
+          )}
+          {hasMultipleLocations && (
+            <Grid key="legend" item sm xs={12}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={normalizeData}
+                    onChange={() => {
+                      setNormalizeData(!normalizeData);
+                    }}
+                    name="normalize data"
+                    disableRipple
+                    id="normalize-data-control"
+                  />
+                }
+                label="Normalize Data"
+              />
             </Grid>
           )}
         </Grid>
