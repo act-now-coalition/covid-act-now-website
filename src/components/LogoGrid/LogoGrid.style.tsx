79513ecc15a5805718c800463ded8d877a84161d
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-09-14 10:43:40 -0700

    add the schmidt futures logo to partners

--- src/components/LogoGrid/LogoGrid.style.tsx
+++ src/components/LogoGrid/LogoGrid.style.tsx
@@ -1,30 +1,5 @@
 import styled from 'styled-components';
 
-export const StyledPartnerLogoGrid = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  margin: 0 -0.5rem 1.5rem;
-  position: relative;
-
-  > a {
-    flex: 1;
-    margin: 0 0.5rem 1rem;
-
-    &:last-child {
-      margin-bottom: 0;
-    }
-  }
-
-  @media (min-width: 900px) {
-    flex-direction: row;
-
-    > a {
-      margin-bottom: 0;
-    }
-  }
-`;
-
 export const Logo = styled.img`
   width: 100%;
   max-width: 300px;
