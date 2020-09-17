fb95ea557320998bd7cfae51f5b023bacec309af
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-09-16 18:27:30 -0700

    wip

--- src/screens/LocationPage/LocationPage.js
+++ src/screens/LocationPage/LocationPage.js
@@ -1,6 +1,5 @@
 import React, { useState, useMemo } from 'react';
 import { find } from 'lodash';
-import moment from 'moment';
 import { useParams } from 'react-router-dom';
 import US_STATE_DATASET from 'components/MapSelectors/datasets/us_states_dataset_01_02_2020';
 import { MAP_FILTERS } from './Enums/MapFilterEnums';
@@ -11,30 +10,7 @@ import EnsureSharingIdInUrl from 'components/EnsureSharingIdInUrl';
 import ChartsHolder from 'components/LocationPage/ChartsHolder';
 import { LoadingScreen } from './LocationPage.style';
 import { useProjections } from 'common/utils/model';
-import { findLocationForFips, findStateByFips } from 'common/locations';
-import { LOCATION_SUMMARY_LEVELS } from 'common/metrics/location_summary';
-
-function getPageTitle(projections) {
-  const location = findLocationForFips(projections.fips);
-  const state = findStateByFips(location.state_fips_code);
-  const locationName = location.county
-    ? `${location.county}, ${state.state}`
-    : `${location.state}`;
-  return `${locationName}, ${location.state_code} - America’s COVID Warning System`;
-}
-
-function getPageDescription(projections) {
-  const dateToday = moment().format('MMM DD, YYYY');
-  const location = findLocationForFips(projections.fips);
-  const state = findStateByFips(location.state_fips_code);
-  const alarmLevel = projections.getAlarmLevel();
-  const levelInfo = LOCATION_SUMMARY_LEVELS[alarmLevel];
-  const locationName = location.county
-    ? `${location.county}, ${state.state}/${location.state_code}`
-    : `${location.state}`;
-
-  return `${dateToday} COVID THREAT LEVEL: ${levelInfo.detail(locationName)}`;
-}
+import { getPageTitle, getPageDescription } from './utils';
 
 function LocationPage() {
   let { stateId, countyId, chartId } = useParams();
