d01d26d80026418614381c9bfebf169c9d3b83d6
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-09-23 16:56:46 -0700

    remove comment

--- src/index.js
+++ src/index.js
@@ -3,30 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import ReactGA from 'react-ga';
-
-// window.gtag('config', 'G-HFCDC7K5G1', {
-//   page_path: location.pathname,
-// });
-// window.gtag('config', 'UA-160622988-1', {
-//   page_path: location.pathname,
-// });
-
-// ReactGA.initialize(
-//   [
-//     {
-//       trackingId: 'UA-160622988-1',
-//       gaOptions: {
-//         name: 'tracker1',
-//       },
-//     },
-//     {
-//       trackingId: 'G-HFCDC7K5G1',
-//       gaOptions: { name: 'tracker2' },
-//     },
-//   ],
-//   { debug: true, alwaysSendToDefaultTracker: false },
-// );
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
