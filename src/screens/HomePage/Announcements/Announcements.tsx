7e9c25ecc70242c1cf84e501236e7adc1c87ebd4
Chris Kelly <ghop02@gmail.com>
2020-09-24 19:01:18 -0400

    Update website copy

--- src/screens/HomePage/Announcements/Announcements.tsx
+++ src/screens/HomePage/Announcements/Announcements.tsx
@@ -21,28 +21,27 @@ const Announcements = () => {
       </AnnouncementIntro>
       <Date>THURSDAY, Sept 24, 2020</Date>
       <AnnouncementBodyCopy>
-        We’re excited to be announcing{' '}
+        We are excited to announce{' '}
         <ExternalLink href="https://apidocs.covidactnow.org">
           v2 of our API
         </ExternalLink>
-        . it will provide all the same data that powers the public Covid Act Now
-        site, but in a machine-readable format, made for ease-of-use by those
-        wishing to programmatically ingest our data. (For a customizable Google
-        Spreadsheet version of our model, see{' '}
-        <ExternalLink href="https://covidactnow.org/resources/">
-          here
-        </ExternalLink>
-        .) To gain access, please use{' '}
+        . It provides all of the same data that powers Covid Act Now, but now in
+        a machine-readable format, made for ease-of-use by those wishing to
+        programmatically ingest our data. To gain access, please use this{' '}
         <ExternalLink href="https://apidocs.covidactnow.org/access">
-          this registration form
+          registration form
         </ExternalLink>{' '}
-        for your unique key. <br />
-        Please note that daily updates to v1 of our API will be stop on
-        10/5/2020. See our{' '}
+        to generate your unique key. Please note that daily updates to v1 of our
+        API will end on 10/5/2020. See our{' '}
         <ExternalLink href="https://apidocs.covidactnow.org/migrate">
           documentation
         </ExternalLink>{' '}
-        for migration instructions.
+        for migration instructions. For a customizable Google Spreadsheet
+        version of our model, see{' '}
+        <ExternalLink href="https://covidactnow.org/resources/">
+          here
+        </ExternalLink>
+        .
       </AnnouncementBodyCopy>
       <ButtonsContainer>
         <ReadMoreButton
