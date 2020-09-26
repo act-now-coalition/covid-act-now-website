934f8cfb2a331a7c7f81f53bfe922670fbd79431
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-09-26 16:11:39 -0700

    remove fullstory + fix title case for ga

--- src/components/Analytics/PageviewTracker.tsx
+++ src/components/Analytics/PageviewTracker.tsx
@@ -8,7 +8,7 @@ import { defaultTracker, legacyTracker } from './utils';
  * Initialize Google Analytics
  */
 function initializeGA() {
-  const options = { debug: false };
+  const options = { titleCase: false, debug: false };
   ReactGA.initialize(defaultTracker.trackingId, options);
   ReactGA.addTrackers([
     {
