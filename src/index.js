75c9af1a32612e03622219f984f8c182f02b4b66
Chris Kelly <ghop02@gmail.com>
2020-09-28 15:52:23 -0400

    Remove perf tracing

--- src/index.js
+++ src/index.js
@@ -5,14 +5,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import * as Sentry from '@sentry/react';
-import { Integrations } from '@sentry/tracing';
 
 Sentry.init({
   dsn:
     'https://4e1fa0b7df4d490488847bcc7966712b@o378922.ingest.sentry.io/5444052',
-  integrations: [new Integrations.BrowserTracing()],
-  // Enabling performance tracing for the following ratio of events
-  tracesSampleRate: 0.1,
 });
 
 ReactDOM.render(<App />, document.getElementById('root'));
