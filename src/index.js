062a2ac10fae43292e5c42eb34f48fbc69ab9f21
Chris Kelly <ghop02@gmail.com>
2020-09-28 16:58:14 -0400

    Add basic sentry config (#1569)
    * Adding basic sentry integration
    
    * Remove perf tracing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+  dsn:
+    'https://4e1fa0b7df4d490488847bcc7966712b@o378922.ingest.sentry.io/5444052',
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
