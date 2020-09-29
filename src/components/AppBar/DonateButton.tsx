9b714093a5b6ea130dba781ed91995196f8d42ae
chasulin <44076375+chasulin@users.noreply.github.com>
2020-09-29 13:50:08 -0400

    Donate button in AppBar (#1568)

---
+++ src/components/AppBar/DonateButton.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { StyledDonateButton, DonateButtonWrapper } from './AppBar.style';
+import { EventAction, EventCategory, trackEvent } from 'components/Analytics';
+import useScrollPosition from '@react-hook/window-scroll';
+import { Fade } from '@material-ui/core';
+
+const ButtonContent = () => {
+  const trackLabel = 'AppBar donate button';
+
+  return (
+    <Link to="/donate">
+      <StyledDonateButton
+        variant="contained"
+        color="primary"
+        disableRipple
+        disableFocusRipple
+        onClick={() => {
+          trackEvent(EventCategory.DONATE, EventAction.CLICK, trackLabel);
+        }}
+      >
+        Donate
+      </StyledDonateButton>
+    </Link>
+  );
+};
+
+/*
+Splits donate button into 2 separate components (with/without fade)
+so that the useScrollPosition hook is only called when necessary
+*/
+
+export const DonateButtonWithoutFade = (props: {}) => {
+  return (
+    <DonateButtonWrapper>
+      <ButtonContent />
+    </DonateButtonWrapper>
+  );
+};
+
+export const DonateButtonWithFade = () => {
+  // uses https://www.npmjs.com/package/@react-hook/window-scroll :
+  const scrollY = useScrollPosition();
+  // 170 approximates the height of the donation banner:
+  const isPassedBanner = scrollY > 170;
+
+  return (
+    <Fade in={isPassedBanner} timeout={150}>
+      <DonateButtonWrapper>
+        <ButtonContent />
+      </DonateButtonWrapper>
+    </Fade>
+  );
+};
