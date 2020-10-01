c0b6e1d88b28b51508b036c0e6f6a0ee06f1ec33
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-10-01 15:03:38 -0700

    fix type error

--- src/components/LocationPage/ChartsHolder.tsx
+++ src/components/LocationPage/ChartsHolder.tsx
@@ -111,7 +111,7 @@ const ChartsHolder = (props: {
               ))}
             </MainContentInner>
             <MainContentInner ref={exploreChartRef}>
-              <Explore fipsList={[props.projections.primary.fips]} />
+              <Explore initialFipsList={[props.projections.primary.fips]} />
             </MainContentInner>
           </ChartContentWrapper>
           <div ref={shareBlockRef}>
