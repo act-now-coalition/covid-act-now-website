471725b74195b3d85e2cb2758943fe643a56cd0e
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-10-08 09:50:12 -0700

    several fixes for the modal component

---
+++ src/components/Dialog/LockBodyScroll.tsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const LockBodyScroll = createGlobalStyle`
+  body {
+    height: 100vh;
+    overflow: hidden;
+  }
+`;
+
+export default LockBodyScroll;
