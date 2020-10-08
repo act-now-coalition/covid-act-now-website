53ad61af96c8a2e5e26909f1bb5c41a8cd9ab17c
Pablo Navarro <pnavarrc@gmail.com>
2020-10-07 20:05:44 -0700

    recommend: implement tabs (#1623)
    * implement recommend tabs
    * add variables for recommend colors

--- src/components/Recommend/RecommendModal.tsx
+++ src/components/Recommend/RecommendModal.tsx
@@ -1,22 +1,68 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { modalContent } from 'cms-content/recommendations';
-import { MarkdownBody } from 'components/Markdown';
+import * as Style from './RecommendModal.style';
+import SourceTabs from './RecommendTabs';
 
 const { federalTaskForce, harvard } = modalContent;
+
+// TODO: Update the CMS structure to include a hex color for the tabs and
+// a title for them
+const fedLevels = [
+  {
+    color: 'green',
+    label: 'Green',
+    body: federalTaskForce.levels[0].content,
+  },
+  {
+    color: 'yellow',
+    label: 'Yellow',
+    body: federalTaskForce.levels[1].content,
+  },
+  {
+    color: 'red',
+    label: 'Red',
+    body: federalTaskForce.levels[2].content,
+  },
+];
+
+const harvardLevels = [
+  {
+    color: 'green',
+    label: 'Green',
+    body: harvard.levels[0].content,
+  },
+  {
+    color: 'yellow',
+    label: 'Yellow',
+    body: harvard.levels[1].content,
+  },
+  {
+    color: 'orange',
+    label: 'Orange',
+    body: harvard.levels[2].content,
+  },
+  {
+    color: 'red',
+    label: 'Red',
+    body: harvard.levels[2].content,
+  },
+];
 
 const RecommendModalBody: React.FC = () => {
   return (
     <React.Fragment>
-      <h2>{modalContent.header}</h2>
+      <Style.Title>{modalContent.header}</Style.Title>
       <Grid container spacing={2}>
         <Grid key="fed" item sm={6} xs={12}>
-          <h3>{federalTaskForce.sourceName}</h3>
-          <MarkdownBody source={federalTaskForce.description} />
+          <Style.SourceTitle>{federalTaskForce.sourceName}</Style.SourceTitle>
+          <Style.SourceIntro source={federalTaskForce.description} />
+          <SourceTabs levels={fedLevels} />
         </Grid>
         <Grid key="harvard" item sm={6} xs={12}>
           <h3>{harvard.sourceName}</h3>
-          <MarkdownBody source={harvard.description} />
+          <Style.SourceIntro source={harvard.description} />
+          <SourceTabs levels={harvardLevels} />
         </Grid>
       </Grid>
     </React.Fragment>
