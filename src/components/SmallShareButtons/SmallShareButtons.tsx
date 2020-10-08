e59ffa3bc1d1f740ae7ae3c7c705d40a138ac565
chasulin <44076375+chasulin@users.noreply.github.com>
2020-10-08 11:37:57 -0400

    adds analytics tracking for share and copy link

--- src/components/SmallShareButtons/SmallShareButtons.tsx
+++ src/components/SmallShareButtons/SmallShareButtons.tsx
@@ -10,10 +10,16 @@ import {
 import { COLOR_MAP } from 'common/colors';
 import LinkIcon from '@material-ui/icons/Link';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { trackRecommendationsEvent } from 'common/utils/recommend';
+import { EventAction } from 'components/Analytics';
 
 //TODO (Chelsi): Add in final share quote
-const SmallShareButtons = (props: { shareUrl: string; shareQuote: string }) => {
-  const { shareUrl, shareQuote } = props;
+const SmallShareButtons = (props: {
+  shareUrl: string;
+  shareQuote: string;
+  trackLabel: string;
+}) => {
+  const { shareUrl, shareQuote, trackLabel } = props;
 
   const socialProps = {
     url: shareUrl,
@@ -21,15 +27,33 @@ const SmallShareButtons = (props: { shareUrl: string; shareQuote: string }) => {
     socialIconSize: 30,
   };
 
+  const trackRecommendationsShare = () => {
+    trackRecommendationsEvent(EventAction.SHARE, trackLabel);
+  };
+
+  const shareButtonProps = {
+    color: COLOR_MAP.BLUE,
+    onClick: trackRecommendationsShare,
+  };
+
+  const onCopyLink = () => {
+    trackRecommendationsEvent(EventAction.COPY_LINK, trackLabel);
+  };
+
   return (
     <ButtonsWrapper>
-      <ShareButtonContainer color={COLOR_MAP.BLUE}>
+      <ShareButtonContainer {...shareButtonProps}>
         <FacebookShareButtonInner {...socialProps} />
       </ShareButtonContainer>
-      <ShareButtonContainer color={COLOR_MAP.BLUE}>
+      <ShareButtonContainer {...shareButtonProps}>
         <TwitterShareButtonInner {...socialProps} />
       </ShareButtonContainer>
-      <CopyToClipboard text={shareUrl}>
+      <CopyToClipboard
+        text={shareUrl}
+        onCopy={() => {
+          onCopyLink();
+        }}
+      >
         <ShareButtonContainer color={COLOR_MAP.BLUE}>
           <LinkIcon />
         </ShareButtonContainer>
