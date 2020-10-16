0dfafbdc21a0bcd534373030f73caeeba4dce596
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-10-16 09:13:15 -0700

    track click banner button event

--- src/components/Banner/DonationBanner.tsx
+++ src/components/Banner/DonationBanner.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import Banner from './Banner';
 import { SurveyButton } from './Banner.style';
 import { Link } from 'react-router-dom';
+import { EventAction, EventCategory, trackEvent } from 'components/Analytics';
+
+const trackClick = () => {
+  trackEvent(
+    EventCategory.DONATE,
+    EventAction.CLICK_LINK,
+    'Banner Doante Button',
+  );
+};
 
 const renderDonationButton = () => (
-  <Link to="/donate" id="donate-homepage-banner">
+  <Link to="/donate" id="donate-homepage-banner" onClick={trackClick}>
     <SurveyButton
       variant="contained"
       color="primary"
