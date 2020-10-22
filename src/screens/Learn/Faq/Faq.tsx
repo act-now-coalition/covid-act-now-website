e147f264597984c827c3d207f8a018c7a66c7321
Pablo Navarro <pnavarrc@gmail.com>
2020-10-22 08:31:58 -0700

    add faq page meta tags (#1731)

--- src/screens/Learn/Faq/Faq.tsx
+++ src/screens/Learn/Faq/Faq.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Wrapper, PageHeader } from './Faq.style';
 import FaqSection from './FaqSection';
 import { faqContent } from 'cms-content/learn';
+import AppMetaTags from 'components/AppMetaTags/AppMetaTags';
 
 const faqHeader = faqContent.header;
 const faqSections = faqContent.sections;
 
 const Faq = () => {
   return (
-    <Wrapper>
-      <PageHeader>{faqHeader}</PageHeader>
-      <p>*** insert table of contents ***</p>
-      {faqSections.map((section: any, i: number) => (
-        <FaqSection content={section} />
-      ))}
-    </Wrapper>
+    <Fragment>
+      {/* TODO: Review the page metatags with the team */}
+      <AppMetaTags
+        canonicalUrl="/learn"
+        pageTitle="Frequently Asked Questions"
+        pageDescription="Frequently Asked Questions"
+      />
+      <Wrapper>
+        <PageHeader>{faqHeader}</PageHeader>
+        <p>*** insert table of contents ***</p>
+        {faqSections.map((section: any, i: number) => (
+          <FaqSection content={section} />
+        ))}
+      </Wrapper>
+    </Fragment>
   );
 };
 
