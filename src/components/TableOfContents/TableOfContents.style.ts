5b57092d26e058dec3d4b86928f8926a0eeada79
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-10-21 17:58:48 -0700

    use variables for styling

--- src/components/TableOfContents/TableOfContents.style.ts
+++ src/components/TableOfContents/TableOfContents.style.ts
@@ -1,26 +1,26 @@
 import styled from 'styled-components';
 import theme from 'assets/theme';
+import { COLOR_MAP } from 'common/colors';
 
 export const Container = styled.nav`
   font-family: Roboto;
   font-weight: 400;
   line-height: 200%;
-  letter-spacing: 0em;
 
   padding-bottom: ${theme.spacing(3)}px;
-  border-bottom: solid 1px #ddd;
+  border-bottom: solid 1px ${COLOR_MAP.LIGHTGRAY};
 
   ul {
     list-style-type: none;
     margin-block-start: 0;
     margin-block-end: 0;
-    margin-inline-start: 0px;
-    margin-inline-end: 0px;
+    margin-inline-start: 0;
+    margin-inline-end: 0;
     padding-inline-start: 0;
   }
 
   a {
-    color: #00bfea;
+    color: ${COLOR_MAP.BLUE};
     text-decoration: none;
   }
 `;
