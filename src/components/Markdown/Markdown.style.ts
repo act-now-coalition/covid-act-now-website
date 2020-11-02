685090617401f2fc92b148ac87aead769a4286ea
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-11-02 11:57:50 -0800

    centralize markdown styles

--- src/components/Markdown/Markdown.style.ts
+++ src/components/Markdown/Markdown.style.ts
@@ -1,10 +1,63 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { COLOR_MAP } from 'common/colors';
 import ReactMarkdown from 'react-markdown';
+import theme from 'assets/theme';
+
+export const StylesBlockElement = css`
+  margin-bottom: ${theme.spacing(2)}px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+export const StylesBody = css`
+  color: ${COLOR_MAP.GRAY_BODY_COPY};
+  line-height: 1.6;
+  font-size: 16px;
+`;
+
+export const StylesH2 = css`
+  margin-top: ${theme.spacing(1)}px;
+  margin-bottom: ${theme.spacing(3)}px;
+  font-size: 18px;
+  font-weight: 900;
+  line-height: 29px;
+`;
+
+export const StylesBlockQuoteHighlight = css`
+  background-color: ${COLOR_MAP.LIGHTGRAY_BG};
+  padding: ${theme.spacing(2)}px;
+  display: inline-block;
+  margin: 0.5rem 0;
+  width: 100%;
+  p {
+    color: ${COLOR_MAP.GREEN.BASE};
+    font-size: 1.125rem;
+    font-weight: 900;
+    line-height: 1.6;
+    margin: 0;
+  }
+`;
 
 export const MarkdownBody = styled(ReactMarkdown)`
+  ${StylesBody};
+
+  /* Inline  */
   a {
     color: ${COLOR_MAP.BLUE};
+  }
+
+  /* Block elements */
+  p,
+  ul,
+  li {
+    ${StylesBody}
+    ${StylesBlockElement}
+  }
+
+  h2 {
+    ${StylesH2}
+    color: #000;
   }
 `;
 
--- src/screens/Learn/Learn.style.tsx
+++ src/screens/Learn/Learn.style.tsx
@@ -1,6 +1,9 @@
-import styled, { css } from 'styled-components';
-import { COLOR_MAP } from 'common/colors';
-import { MarkdownContent } from 'components/Markdown';
+import styled from 'styled-components';
+import {
+  MarkdownContent,
+  StylesH2,
+  StylesBlockQuoteHighlight,
+} from 'components/Markdown';
 import theme from 'assets/theme';
 import { mobileBreakpoint } from 'assets/theme/sizes';
 
@@ -30,10 +33,7 @@ export const PageHeader = styled.h1`
 `;
 
 export const SectionHeader = styled.h2`
-  font-weight: 900;
-  font-size: 1.5rem;
-  line-height: 1.25;
-  margin: ${theme.spacing(1)}px 0 ${theme.spacing(3)}px 0;
+  ${StylesH2}
 `;
 
 export const BreadcrumbsContainer = styled.div`
@@ -44,60 +44,10 @@ export const BreadcrumbsContainer = styled.div`
   Markdown styles used throughout Learn
 */
 
-export const BodyCopyStyles = css`
-  color: ${COLOR_MAP.GRAY_BODY_COPY};
-  line-height: 1.6;
-  font-size: 16px;
-  font-weight: 400;
-
-  p {
-    &:not(:last-child) {
-      margin-bottom: ${theme.spacing(2)}px;
-    }
-  }
-`;
-
-const BlockquoteStyles = css`
-  background-color: ${COLOR_MAP.LIGHTGRAY_BG};
-  padding: ${theme.spacing(2)}px;
-  display: inline-block;
-  margin: 0.5rem 0;
-  width: 100%;
-
-  p {
-    color: ${COLOR_MAP.GREEN.BASE};
-    font-size: 1.125rem;
-    font-weight: 900;
-    line-height: 1.6;
-    margin: 0;
-  }
-`;
-
-export const PageIntroMarkdown = styled(MarkdownContent)`
-  p,
-  ul,
-  li {
-    ${BodyCopyStyles}
-  }
-  margin-bottom: ${theme.spacing(2)}px;
-`;
+export const PageIntroMarkdown = styled(MarkdownContent)``;
 
 export const BodyCopyMarkdown = styled(MarkdownContent)`
-  p,
-  ul,
-  li {
-    ${BodyCopyStyles}
-  }
-
   blockquote {
-    ${BlockquoteStyles}
-  }
-
-  h2 {
-    margin-top: ${theme.spacing(1)}px;
-    margin-bottom: ${theme.spacing(3)}px;
-    font-size: 18px;
-    font-weight: 900;
-    line-height: 29px;
+    ${StylesBlockQuoteHighlight}
   }
 `;
