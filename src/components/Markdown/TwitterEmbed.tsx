1f0346ba0871d384a446aecbee293991d2a1e539
Pablo Navarro <pnavarrc@gmail.com>
2020-11-03 16:13:11 -0800

    fix the twitter embed (#1841)

--- src/components/Markdown/TwitterEmbed.tsx
+++ src/components/Markdown/TwitterEmbed.tsx
@@ -1,39 +1,12 @@
-import { Dictionary } from 'lodash';
 import React from 'react';
+// @ts-ignore
+import { TwitterTweetEmbed } from 'react-twitter-embed';
 
-/**
- * Copy the embed code from https://publish.twitter.com/
- */
-const CaseStudyPublicHealthRx: React.FC = () => {
-  return (
-    <blockquote className="twitter-tweet">
-      <p lang="en" dir="ltr">
-        One of my favorite covid19 resources.{' '}
-        <a href="https://twitter.com/CovidActNow?ref_src=twsrc%5Etfw">
-          @CovidActNow
-        </a>{' '}
-        <br />
-        Their putting out extremely useful content. Check them out.{' '}
-        <a href="https://twitter.com/hashtag/healthcaremediacontributor?src=hash&amp;ref_src=twsrc%5Etfw">
-          #healthcaremediacontributor
-        </a>
-        <a href="https://twitter.com/hashtag/healthcareinfluencer?src=hash&amp;ref_src=twsrc%5Etfw">
-          #healthcareinfluencer
-        </a>
-        <a href="https://twitter.com/hashtag/thepublichealthpharmacist?src=hash&amp;ref_src=twsrc%5Etfw">
-          #thepublichealthpharmacist
-        </a>
-        <a href="https://t.co/281Y7mC995">pic.twitter.com/281Y7mC995</a>
-      </p>
-      &mdash; ThePublicHealthPharmacist (@PublicHealthRx){' '}
-      <a href="https://twitter.com/PublicHealthRx/status/1286762591520346113?ref_src=twsrc%5Etfw">
-        July 24, 2020
-      </a>
-    </blockquote>
-  );
-};
+const CaseStudyPublicHealthRx: React.FC = () => (
+  <TwitterTweetEmbed tweetId={'1286762591520346113'} />
+);
 
-const twitterEmbedsByUrl: Dictionary<React.FC> = {
+const twitterEmbedsByUrl: { [url: string]: React.FC } = {
   'https://twitter.com/PublicHealthRx/status/1286762591520346113': CaseStudyPublicHealthRx,
 };
 
