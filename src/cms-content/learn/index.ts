ecdb53ae60af47a573041d352ac4c5201ab4650e
chasulin <44076375+chasulin@users.noreply.github.com>
2020-11-09 17:28:59 -0500

    Products cms config cont. (#1878)

--- src/cms-content/learn/index.ts
+++ src/cms-content/learn/index.ts
@@ -3,12 +3,32 @@ import faq from './learn-faq.json';
 import glossary from './learn-glossary.json';
 import landing from './learn-landing.json';
 import caseStudies from './learn-case-studies.json';
-import { sanitizeID } from './utils';
-
-type Markdown = string;
+import { sanitizeID, Markdown } from '../utils';
 
 /*
-  For FAQ:
+  Learn Landing page:
+*/
+
+export interface LandingSection {
+  sectionTitle: string;
+  sectionId: string;
+  description: Markdown;
+  buttonCta: string;
+  buttonRedirect: string;
+}
+
+export interface LandingContent {
+  header: string;
+  intro: Markdown;
+  sections: LandingSection[];
+  metadataTitle: string;
+  metadataDescription: string;
+}
+
+export const landingPageContent = landing as LandingContent;
+
+/*
+  FAQ:
 */
 export interface Question {
   question: string;
@@ -48,7 +68,7 @@ function sanitizeFaq(faq: FaqContent) {
 export const faqContent = sanitizeFaq(faq) as FaqContent;
 
 /*
-  For Glossary:
+  Glossary:
 */
 
 export interface Term {
@@ -84,31 +104,10 @@ function sanitizeGlossary(glossary: GlossaryContent) {
 
 export const glossaryContent = sanitizeGlossary(glossary) as GlossaryContent;
 
-/*
-  For Landing page:
-*/
-
-export interface LandingSection {
-  sectionTitle: string;
-  sectionId: string;
-  description: Markdown;
-  buttonCta: string;
-  buttonRedirect: string;
-}
-
-export interface LandingContent {
-  header: string;
-  intro: Markdown;
-  sections: LandingSection[];
-  metadataTitle: string;
-  metadataDescription: string;
-}
-
-export const landingPageContent = landing as LandingContent;
-
 /**
  * Case Studies
  */
+
 export interface CaseStudy {
   header: string;
   shortTitle: string;
@@ -198,25 +197,45 @@ export const learnPages: TocItem[] = [
 ];
 
 /**
- * Products
- */
+ * Products - landing page:
+ **/
 
 export interface LandingPageButton {
   cta: string;
   redirect: string;
 }
 
-export interface ProductsSection {
-  sectionTitle: string;
-  sectionId: string;
-  sectionSubtitle: string;
-  sectionDescription: Markdown;
-  notes?: Markdown;
+export interface ProductsLandingSection {
+  productName: string;
+  productId: string;
+  productSubtitle: string;
+  productDescription: Markdown;
   buttons: LandingPageButton[];
 }
 
-export interface ProductsContent {
+export interface ProductsLandingContent {
   header: string;
-  intro: Markdown;
-  sections: ProductsSection[];
+  intro?: Markdown;
+  productsList: ProductsLandingSection[];
+  metadataTitle: string;
+  metadataDescription: string;
+}
+
+/**
+ * Products - full pages:
+ **/
+
+export interface BodySection {
+  sectionTitle: string;
+  sectionId: string;
+  sectionBody: Markdown;
+}
+
+export interface ProductPageContent {
+  productName: string;
+  productId: string;
+  productIntro: Markdown;
+  sections: BodySection[];
+  metadataTitle: string;
+  metadataDescription: string;
 }
