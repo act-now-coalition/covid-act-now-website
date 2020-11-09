69fb10478f946c730dc29fab401902ed51abd5d7
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-11-09 08:43:08 -0800

    remove contact us styles

--- src/screens/Contact/Contact.tsx
+++ src/screens/Contact/Contact.tsx
@@ -5,28 +5,27 @@ import { Anchor } from 'components/TableOfContents';
 import MainContent from 'components/PageContent';
 import { contactUsContent, sidebarItems } from 'cms-content/contact';
 
-const ContactUsPage: React.FC = () => {
-  const { header, intro, sections } = contactUsContent;
-  return (
-    <Fragment>
-      <AppMetaTags
-        canonicalUrl="/contact"
-        pageTitle="Contact us"
-        pageDescription="We’re here to help. Drop us a line."
-      />
-      <MainContent sidebarItems={sidebarItems}>
-        <Heading1>{header}</Heading1>
-        {intro && intro.length && <MarkdownContent source={intro} />}
-        {sections.map(({ sectionId, header, body }) => (
-          <Fragment key={sectionId}>
-            <Anchor id={sectionId} />
-            <Heading2>{header}</Heading2>
-            <MarkdownContent source={body} />
-          </Fragment>
-        ))}
-      </MainContent>
-    </Fragment>
-  );
-};
+const { header, intro, sections } = contactUsContent;
+
+const ContactUsPage: React.FC = () => (
+  <Fragment>
+    <AppMetaTags
+      canonicalUrl="/contact"
+      pageTitle="Contact us"
+      pageDescription="We’re here to help. Drop us a line."
+    />
+    <MainContent sidebarItems={sidebarItems}>
+      <Heading1>{header}</Heading1>
+      {intro && intro.length && <MarkdownContent source={intro} />}
+      {sections.map(({ sectionId, header, body }) => (
+        <Fragment key={sectionId}>
+          <Anchor id={sectionId} />
+          <Heading2>{header}</Heading2>
+          <MarkdownContent source={body} />
+        </Fragment>
+      ))}
+    </MainContent>
+  </Fragment>
+);
 
 export default ContactUsPage;
