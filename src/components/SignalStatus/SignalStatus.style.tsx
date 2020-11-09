30bac7394cf3238aaddcab7adab4c73b5e3699a7
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-11-09 12:07:19 -0800

    increase the size so it fits when condensed

--- src/components/SignalStatus/SignalStatus.style.tsx
+++ src/components/SignalStatus/SignalStatus.style.tsx
@@ -12,7 +12,7 @@ export const SignalStatusWrapper = styled(Box)<{
       monospace;
     font-weight: bold;
     font-size: ${props.isEmbed ? '13px' : '0.875rem'};
-    width: 5rem;
+    width: 5.5rem;
     margin-left: 1rem;
     line-height: 1.25rem;
     display: flex;
