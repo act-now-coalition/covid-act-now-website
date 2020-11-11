470b9022ef0365a422ddf2bdb5e1584cd6629e2b
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-11-10 17:00:15 -0800

    update page width

--- src/screens/National/National.style.ts
+++ src/screens/National/National.style.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import { COLOR_MAP } from 'common/colors';
 import theme from 'assets/theme';
-import { mobileBreakpoint } from 'assets/theme/sizes';
 
 export const Placeholder = styled.div`
   padding: ${theme.spacing(2)}px;
@@ -15,8 +14,8 @@ export const PageContent = styled.main`
   margin: 0 ${theme.spacing(2)}px;
   margin-bottom: ${theme.spacing(4)}px;
 
-  @media (min-width: ${mobileBreakpoint}) {
-    max-width: ${mobileBreakpoint};
+  @media (min-width: ${960}px) {
+    max-width: ${960}px;
     margin: 0 auto;
     margin-bottom: ${theme.spacing(4)}px;
   }
