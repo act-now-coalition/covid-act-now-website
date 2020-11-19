387fbaa0477b68b1439b36d005f472d9af2977a6
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-11-19 15:08:29 -0800

    component library setup

--- src/components/Button/Button.style.ts
+++ src/components/Button/Button.style.ts
@@ -1,15 +1,18 @@
 import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
+import theme from 'assets/theme';
 import { COLOR_MAP } from 'common/colors';
+
+const disableRippleProps = {
+  disableRipple: true,
+  disableElevation: true,
+  disableFocusRipple: true,
+};
 
 /**
  * Button that looks like a link.
  */
-export const LinkButton = styled(Button).attrs(props => ({
-  disableRipple: true,
-  disableElevation: true,
-  disableFocusRipple: true,
-}))`
+export const LinkButton = styled(Button).attrs(props => disableRippleProps)`
   text-transform: none;
 
   font-family: inherit;
@@ -33,4 +36,19 @@ export const LinkButton = styled(Button).attrs(props => ({
     text-decoration: underline;
     color: ${COLOR_MAP.BLUE};
   }
+`;
+
+const Outlined = styled(Button).attrs(props => ({
+  variant: 'outlined',
+  ...disableRippleProps,
+}))`
+  border-color: ${COLOR_MAP.LIGHTGRAY};
+`;
+
+export const ButtonPrimary = styled(Outlined)`
+  color: ${theme.colors.green};
+`;
+
+export const ButtonSecondary = styled(Outlined)`
+  color: ${theme.colors.lightBlue};
 `;
