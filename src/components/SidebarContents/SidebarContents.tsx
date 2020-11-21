100b5b3eabb232174adb3cf40b348adfc5531261
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-11-20 16:28:14 -0800

    use the aside element for the sidebar table of contents

--- src/components/SidebarContents/SidebarContents.tsx
+++ src/components/SidebarContents/SidebarContents.tsx
@@ -16,7 +16,7 @@ function getHash(item: TocItem) {
 const SidebarContents: React.FC<{ items: TocItem[] }> = ({ items }) => {
   const { hash, pathname } = useLocation();
   return (
-    <nav>
+    <aside>
       <Styles.TopLevelList>
         {items.map(topLevelItem => (
           <li key={topLevelItem.to}>
@@ -43,7 +43,7 @@ const SidebarContents: React.FC<{ items: TocItem[] }> = ({ items }) => {
           </li>
         ))}
       </Styles.TopLevelList>
-    </nav>
+    </aside>
   );
 };
 
