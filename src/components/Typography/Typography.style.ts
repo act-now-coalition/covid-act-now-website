54a07dad33bfa79a8f9d21a80c84606f8ba5b3e8
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-11-22 15:52:06 -0800

    add notes for the typography component

--- src/components/Typography/Typography.style.ts
+++ src/components/Typography/Typography.style.ts
@@ -2,7 +2,28 @@ import styled from 'styled-components';
 import Typography from '@material-ui/core/Typography';
 
 /**
- * https://material-ui.com/api/typography/
+ * Notes:
+ *
+ * 1. Use the Typography component from Material UI when possible, since it
+ *    has props that allows to customize some styling (align, gutterBottom, etc),
+ *    which can help us reduce the amount of style overrides.
+ *
+ *    Example:
+ *
+ *        <Subtitle1 align="center">how we determine risk factors</Subtitle1>
+ *
+ *    instead of
+ *
+ *      export const RiskFactorsSubtitle = styled(Subtitle1)`
+ *        text-align: center
+ *      `;
+ *
+ *     <RiskFactorsSubtitle>how we determine risk factors</RiskFactorsSubtitle>
+ *
+ * 2. The `theme` prop here comes from the theme provider from styled components
+ *    (see the `ScThemeProvider` wrapper in App.js. This would allow us to
+ *    dynamically change the theme on specific context (embeds, for example) or
+ *    at the application level (if we were to implement a dark theme).
  */
 
 export const Subtitle1 = styled(Typography).attrs(props => ({
