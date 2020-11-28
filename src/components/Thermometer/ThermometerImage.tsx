3a7c0e40263718f3541cddb0402b89ad48948034
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-11-27 19:09:26 -0800

    add correct role to the thermometer image

--- src/components/Thermometer/ThermometerImage.tsx
+++ src/components/Thermometer/ThermometerImage.tsx
@@ -2,36 +2,38 @@ import React from 'react';
 import { Level } from 'common/level';
 import { LEVEL_COLOR } from 'common/colors';
 import { ThermometerContainer, ThermometerRow } from './ThermometerImage.style';
+import { LOCATION_SUMMARY_LEVELS } from 'common/metrics/location_summary';
 
-const thermometerContent = [
-  {
-    level: Level.SUPER_CRITICAL,
-    color: `${LEVEL_COLOR[Level.SUPER_CRITICAL]}`,
-  },
-  {
-    level: Level.CRITICAL,
-    color: `${LEVEL_COLOR[Level.CRITICAL]}`,
-  },
-  {
-    level: Level.HIGH,
-    color: `${LEVEL_COLOR[Level.HIGH]}`,
-  },
-  {
-    level: Level.MEDIUM,
-    color: `${LEVEL_COLOR[Level.MEDIUM]}`,
-  },
-  {
-    level: Level.LOW,
-    color: `${LEVEL_COLOR[Level.LOW]}`,
-  },
+const thermometerLevels = [
+  Level.SUPER_CRITICAL,
+  Level.CRITICAL,
+  Level.HIGH,
+  Level.MEDIUM,
+  Level.LOW,
 ];
+
+const thermometerContent = thermometerLevels.map(level => ({
+  level,
+  color: LEVEL_COLOR[level],
+  name: LOCATION_SUMMARY_LEVELS[level].name,
+}));
+
+function getLevelName(level: Level) {
+  if (level === Level.UNKNOWN) {
+    return 'Unknown';
+  }
+  const currentLevel = thermometerContent.find(item => item.level === level);
+  return currentLevel?.name;
+}
 
 const ThermometerImage: React.FC<{ currentLevel: Level }> = ({
   currentLevel,
 }) => {
   const thresholdUnknown = currentLevel === Level.UNKNOWN;
+  const levelName = getLevelName(currentLevel);
+  const imageDescription = `Thermometer image showing that the current alert level is ${levelName}.`;
   return (
-    <ThermometerContainer>
+    <ThermometerContainer role="img" aria-label={imageDescription}>
       {thermometerContent.map((row, i) => (
         <ThermometerRow
           key={row.level}
