ee4caa31bb56271da2a8fbed0e8b5a688f972a46
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-11-30 09:01:08 -0800

    add more padding to the title

--- src/components/Dialog/Dialog.style.ts
+++ src/components/Dialog/Dialog.style.ts
@@ -35,6 +35,6 @@ export const StyledMuiDialogContent = styled(MuiDialogContent)`
   padding: 0 1rem 2.5rem;
 
   @media (min-width: 600px) {
-    padding: 0.5rem 1.5rem;
+    padding: 1rem 1.5rem;
   }
 `;
