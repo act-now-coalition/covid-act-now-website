159d1edff7394b3de01d8b031ceef3b67ee222b2
chasulin <44076375+chasulin@users.noreply.github.com>
2020-12-07 15:00:43 -0500

    Renames logo files (#2071)

--- src/assets/images/footerlogoDarkWithURL.tsx
+++ src/assets/images/footerlogoDarkWithURL.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import can_logo_png from './can_logo_url_dark.png';
+import can_logo_png from './covid-act-now-logo-url-dark.png';
 
 const LogoDarkWithURL = () => (
   <img src={can_logo_png} alt="Covid Act Now Logo"></img>
