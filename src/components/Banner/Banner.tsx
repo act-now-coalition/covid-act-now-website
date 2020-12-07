d03835b5d367e4062b6b7c7b3263df217447e714
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-12-07 12:47:22 -0800

    adjust spacing of the banner to not go beyond 100%

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -7,7 +7,7 @@ const Banner: React.FC<{
   renderButton?: () => React.ReactElement;
 }> = ({ message, renderButton }) => {
   return (
-    <Styles.MainContainer container spacing={1} role="banner">
+    <Styles.MainContainer container role="banner">
       <Grid item sm md lg>
         {message}
       </Grid>
