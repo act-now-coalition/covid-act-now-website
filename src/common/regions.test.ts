3200bb7add842ec34341aa4ff97d13ef45c0cb42
Pablo Navarro <pnavarrc@gmail.com>
2020-12-08 10:38:20 -0800

    Region API: Use region context to access location properties (#2076)
    * implement region context
    * update the location page header to use regions
    * restore link to state in county title + minor fixes
    * add assert for region hook

--- src/common/regions.test.ts
+++ src/common/regions.test.ts
@@ -2,24 +2,78 @@ import regions, { RegionType } from './regions';
 
 describe('regions', () => {
   test('states() returns all the states', () => {
-    const states = regions.states();
+    const states = regions.states;
     expect(states).toHaveLength(53);
   });
 
   test('counties() returns all the counties', () => {
-    const counties = regions.counties();
+    const counties = regions.counties;
     expect(counties).toHaveLength(3224);
   });
 
-  test('findByFipsCode returns a state for a state FIPS', () => {
-    const dc = regions.findByFipsCode('11');
-    expect(dc).toBeTruthy();
-    expect(dc?.regionType).toBe(RegionType.STATE);
+  describe('findByFipsCode', () => {
+    test('returns a state when passing a valid state FIPS', () => {
+      const dc = regions.findByFipsCode('11');
+      expect(dc).not.toBeNull();
+      expect(dc?.regionType).toBe(RegionType.STATE);
+    });
+
+    test('returns a county when passing a valid county FIPS', () => {
+      const county = regions.findByFipsCode('53033');
+      expect(county).not.toBeNull();
+      expect(county?.regionType).toBe(RegionType.COUNTY);
+    });
+
+    test('returns null if the FIPS doesn’t exist', () => {
+      const notFound = regions.findByFipsCode('xx');
+      expect(notFound).toBeNull();
+    });
   });
 
-  test('findByFipsCode returns null if the FIPS doesn’t exist', () => {
-    const notFound = regions.findByFipsCode('xx');
-    expect(notFound).toBe(null);
+  describe('findStateByUrlParams', () => {
+    test('returns the correct state when passing a state code', () => {
+      const state = regions.findStateByUrlParams('OR');
+      expect(state).not.toBeNull();
+      expect(state?.fipsCode).toBe('41');
+    });
+
+    test('returns the correct state when passing a valid URL segment', () => {
+      const state = regions.findStateByUrlParams('oregon-or');
+      expect(state).not.toBeNull();
+      expect(state?.fipsCode).toBe('41');
+    });
+
+    test('returns null for invalid params "xxx"', () => {
+      const state = regions.findStateByUrlParams('xxx');
+      expect(state).toBeNull();
+    });
+  });
+
+  describe('findCountyByUrlParams', () => {
+    test('returns the correct county for valid state and county IDs', () => {
+      const county = regions.findCountyByUrlParams(
+        'washington-wa',
+        'king_county',
+      );
+      expect(county).not.toBeNull();
+      expect(county?.fipsCode).toBe('53033');
+    });
+
+    test('returns the correct county when using state code as stateId', () => {
+      const county = regions.findCountyByUrlParams('wa', 'king_county');
+      expect(county).not.toBeNull();
+      expect(county?.fipsCode).toBe('53033');
+    });
+
+    test('returns null for invalid stateId and valid countyID', () => {
+      const county = regions.findCountyByUrlParams('xxx', 'king_county');
+      expect(county).toBe(null);
+    });
+
+    test('returns null for invalid stateId and countyID', () => {
+      const county = regions.findCountyByUrlParams('xxx', 'xxx');
+      expect(county).toBeNull();
+    });
   });
 });
 
@@ -41,7 +95,7 @@ describe('State', () => {
 describe('County', () => {
   const county = regions.findByFipsCode('53033');
   test('fullName', () => {
-    expect(county?.fullName()).toBe('King County, WA');
+    expect(county?.fullName()).toBe('King County, Washington');
   });
 
   test('relativeUrl', () => {
