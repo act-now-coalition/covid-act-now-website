63465f2b33a29de33962a7882fffcd3dc8bd16cc
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-12-08 11:54:57 -0800

    remove unused div

--- src/components/Markdown/YoutubeEmbed.tsx
+++ src/components/Markdown/YoutubeEmbed.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
+import { ParentSize } from '@vx/responsive';
 
 export function isYouTubeEmbed(embedUrl: string) {
   return embedUrl.startsWith('https://www.youtube.com/embed/');
 }
 
+const maxWidth = 560;
+const maxHeight = 315;
+
 const YouTubeEmbed: React.FC<{ embedUrl: string }> = ({ embedUrl }) => {
   return (
-    <iframe
-      title="YouTube embed"
-      width={560}
-      height={315}
-      src={embedUrl}
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      frameBorder="0"
-    />
+    <ParentSize>
+      {({ width }) => {
+        // Make sure that the embed keeps the original aspect ratio without
+        // being wider than the container.
+        const scaledWidth = Math.min(width, maxWidth);
+        const scaledHeight = (maxHeight / maxWidth) * scaledWidth;
+        return (
+          <iframe
+            title="YouTube embed"
+            width={scaledWidth}
+            height={scaledHeight}
+            src={embedUrl}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            frameBorder="0"
+          />
+        );
+      }}
+    </ParentSize>
   );
 };
 
