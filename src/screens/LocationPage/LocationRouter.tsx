1749ed75d6589878c5ff9bcc4690347ab6b4103e
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-12-10 13:44:33 -0800

    add msa support for useRegionFromParams

--- src/screens/LocationPage/LocationRouter.tsx
+++ src/screens/LocationPage/LocationRouter.tsx
@@ -1,47 +1,28 @@
 import React from 'react';
 import { useParams, Redirect } from 'react-router-dom';
-import regions, { RegionContext } from 'common/regions';
+import regions, {
+  RegionContext,
+  RegionType,
+  useRegionFromParams,
+} from 'common/regions';
 import LocationPage from './LocationPage';
 import RegionPage from './RegionPage';
 
-interface LocationPageUrlParams {
-  stateId: string | undefined;
-  countyId: string | undefined;
-  metroAreaUrlSegment: string | undefined;
-}
-
 const LocationRouter: React.FC = () => {
-  let { stateId, countyId, metroAreaUrlSegment } = useParams<
-    LocationPageUrlParams
-  >();
-
-  const state = stateId ? regions.findStateByUrlParams(stateId) : null;
-  const county =
-    stateId && countyId
-      ? regions.findCountyByUrlParams(stateId, countyId)
-      : null;
-
-  const metroArea = metroAreaUrlSegment
-    ? regions.findMetroAreaByUrlParams(metroAreaUrlSegment)
-    : null;
-
-  if (metroArea) {
-    return (
-      <RegionContext.Provider value={metroArea}>
-        <RegionPage region={metroArea} />
-      </RegionContext.Provider>
-    );
-  }
-
-  const region = county || state;
+  const region = useRegionFromParams();
 
   if (!region) {
     return <Redirect to="/" />;
   }
 
+  // TODO: We will centralize this, this is only used during development
   return (
     <RegionContext.Provider value={region}>
-      <LocationPage region={region} />
+      {region.regionType === RegionType.MSA ? (
+        <RegionPage region={region} />
+      ) : (
+        <LocationPage region={region} />
+      )}
     </RegionContext.Provider>
   );
 };
