d85e1500cfda6c102ff80829985bf978bb0cbd22
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-12-10 13:02:49 -0800

    wip

---
+++ src/components/RegionMap/MetroAreaMap.tsx
@@ -0,0 +1,131 @@
+import React from 'react';
+import { uniq } from 'lodash';
+import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
+import COUNTIES_JSON from 'components/Map/data/counties-small.json';
+import { geoBounds, geoCentroid, geoDistance } from 'd3-geo';
+import * as topojson from 'topojson-client';
+import { MetroArea } from 'common/regions';
+import * as RegionMapStyle from './RegionMap.style';
+
+const MetroAreaMap: React.FC<{
+  height?: number;
+  width?: number;
+  metroArea: MetroArea;
+}> = ({ height = 600, width = 800, metroArea }) => {
+  const countyFipsList = metroArea.counties.map(county => county.fipsCode);
+  const countiesTopoJson = buildCountyGeometries(countyFipsList);
+  const projectionConfig = getProjectionConfig(countiesTopoJson, width, height);
+
+  const stateFipsList = getStateFipsList(countyFipsList);
+  const statesTopoJson = buildStateGeometries(stateFipsList);
+
+  return (
+    <ComposableMap
+      projection={'geoConicEqualArea'}
+      height={height}
+      width={width}
+      projectionConfig={projectionConfig}
+    >
+      <Geographies key="states" geography={statesTopoJson}>
+        {({ geographies }) =>
+          geographies.map(geo => (
+            <Geography
+              key={geo.rsmKey}
+              geography={geo}
+              fill={'#ccc'}
+              strokeWidth={1}
+              stroke="white"
+              role="img"
+            />
+          ))
+        }
+      </Geographies>
+      <Geographies key="counties" geography={countiesTopoJson}>
+        {({ geographies }) =>
+          geographies.map(geo => (
+            <Geography
+              key={geo.rsmKey}
+              geography={geo}
+              fill={'black'}
+              strokeWidth={1}
+              stroke="white"
+              role="img"
+            />
+          ))
+        }
+      </Geographies>
+      <Geographies key="state-borders" geography={statesTopoJson}>
+        {({ geographies }) =>
+          geographies.map(geo => (
+            <RegionMapStyle.StateOutline
+              key={geo.rsmKey}
+              geography={geo}
+              role="img"
+            />
+          ))
+        }
+      </Geographies>
+    </ComposableMap>
+  );
+};
+
+function getProjectionConfig(
+  countiesTopoJson: any,
+  width: number,
+  height: number,
+) {
+  // Calculates the GeoJSON equivalent of countiesTopoJson
+  const countiesGeoJson = topojson.feature(
+    countiesTopoJson as any,
+    countiesTopoJson.objects.counties as any,
+  );
+
+  /*
+   * We need to calculate the center and scale factor for the geographic
+   * projection so the counties are centered and scaled correctly.
+   *
+   * We calculate the corners of the bounding box in geographic coordinates,
+   * and then the distance (in spherical coordinates) between the bottom left
+   * and top right corners of the bounding box.
+   *
+   * We calculate the scale by dividing the distance in pixels betwen the
+   * bottom left and top right corners of the SVG.
+   */
+  const [bottomLeft, topRight] = geoBounds(countiesGeoJson);
+  const center = geoCentroid(countiesGeoJson);
+  const distanceGeo = geoDistance(bottomLeft, topRight);
+  const distancePx = Math.sqrt(height * height + width * width);
+  return { scale: 0.8 * (distancePx / distanceGeo), center };
+}
+
+function buildCountyGeometries(countyFipsList: string[]) {
+  const objects = COUNTIES_JSON.objects;
+  const geometries = objects.counties.geometries.filter(geoCounty =>
+    countyFipsList.includes(geoCounty.id),
+  );
+  return {
+    ...COUNTIES_JSON,
+    objects: {
+      counties: { ...objects.counties, geometries },
+    },
+  };
+}
+
+function buildStateGeometries(stateFipsList: string[]) {
+  const objects = COUNTIES_JSON.objects;
+  const geometries = objects.states.geometries.filter(geoState =>
+    stateFipsList.includes(geoState.id),
+  );
+  return {
+    ...COUNTIES_JSON,
+    objects: {
+      states: { ...objects.states, geometries },
+    },
+  };
+}
+
+function getStateFipsList(countyFipsList: string[]) {
+  return uniq(countyFipsList.map(countyFips => countyFips.substr(0, 2)));
+}
+
+export default MetroAreaMap;
--- src/components/RegionMap/RegionMap.stories.tsx
+++ src/components/RegionMap/RegionMap.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import RegionMap from './RegionMap';
+import MetroAreaMap from './MetroAreaMap';
 import regions, { MetroArea } from 'common/regions';
 
 export default {
@@ -7,8 +8,27 @@ export default {
   component: RegionMap,
 };
 
+const Container: React.FC = ({ children }) => (
+  <div style={{ width: 300, height: 300, border: 'solid 1px #555' }}>
+    {children}
+  </div>
+);
+
 export const NYMetroArea = () => {
   const metroArea = regions.findByFipsCode('35620') as MetroArea;
   const countyFipsList = metroArea.counties.map(county => county.fipsCode);
-  return <RegionMap countyFipsList={countyFipsList} />;
+  return (
+    <Container>
+      <RegionMap countyFipsList={countyFipsList} />
+    </Container>
+  );
+};
+
+export const FargoMetroArea = () => {
+  const metroArea = regions.findByFipsCode('22020') as MetroArea;
+  return (
+    <Container>
+      <MetroAreaMap metroArea={metroArea} width={300} height={300} />
+    </Container>
+  );
 };
