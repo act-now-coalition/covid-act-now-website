8d5217efcd28ab3b20a5fa4eac5b53cec756e060
Chris Kelly <ghop02@gmail.com>
2020-12-10 16:06:51 -0500

    Fix lint error (#2129)

--- src/common/locations.ts
+++ src/common/locations.ts
@@ -1,7 +1,7 @@
 /** Helpers for dealing with the State / Counties dataset. */
 import US_STATE_DATASET from 'components/MapSelectors/datasets/us_states_dataset_01_02_2020.json';
 import urlJoin from 'url-join';
-import { each, sortBy, takeRight, has, partition, toLower } from 'lodash';
+import { each, has, partition, toLower } from 'lodash';
 import { assert } from './utils';
 import countyAdjacencyMsa from './data/county_adjacency_msa.json';
 import collegesByFips from './data/colleges_by_fips.json';
