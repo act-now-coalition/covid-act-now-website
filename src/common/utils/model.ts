0b4cb0b733604cb2a4e387e2341715378fe75b4c
Chris Kelly <ghop02@gmail.com>
2020-12-10 20:04:25 -0500

    Add metros to location summaries (#2132)
    * Add metro region queries + code to update location summaries
    
    * Add comment about filtering logic
    
    * Updating summaries with metros
    
    * Add find by fips strict + fix email alert generation
    
    * rename projection -> metroProjections

--- src/common/utils/model.ts
+++ src/common/utils/model.ts
@@ -11,6 +11,7 @@ import { fail } from 'common/utils';
 export enum APIRegionSubPath {
   COUNTIES = 'counties',
   STATES = 'states',
+  CBSAS = 'cbsas',
 }
 
 const cachedProjections: { [key: string]: Promise<Projections> } = {};
@@ -88,6 +89,32 @@ export function fetchAllCountyProjections(snapshotUrl: string | null = null) {
   const key = snapshotUrl || 'null';
   cachedCountiesProjections[key] = cachedCountiesProjections[key] || fetch();
   return cachedCountiesProjections[key];
+}
+
+/** Returns an array of `Projections` instances for all counties. */
+const cachedMetroProjections: { [key: string]: Promise<Projections[]> } = {};
+export function fetchAllMetroProjections(snapshotUrl: string | null = null) {
+  snapshotUrl = snapshotUrl || getSnapshotUrlOverride();
+  async function fetch() {
+    const all = await new Api(snapshotUrl).fetchAggregatedSummaryWithTimeseries(
+      APIRegionSubPath.CBSAS,
+    );
+    return all
+      .filter(
+        summaryWithTimeseries =>
+          // The cbsas endpoint will return all CBSAs, both metro and micro. We want to
+          // Only include summaries for metros (those that are included in `regions`).
+          regions.findByFipsCode(summaryWithTimeseries.fips) !== null,
+      )
+      .map(summaryWithTimeseries => {
+        const region = regions.findByFipsCode(summaryWithTimeseries.fips);
+        assert(region, 'Failed to find region ' + summaryWithTimeseries.fips);
+        return new Projections(summaryWithTimeseries, region);
+      });
+  }
+  const key = snapshotUrl || 'null';
+  cachedMetroProjections[key] = cachedMetroProjections[key] || fetch();
+  return cachedMetroProjections[key];
 }
 
 export function useProjectionsFromRegion(region: Region | null) {
