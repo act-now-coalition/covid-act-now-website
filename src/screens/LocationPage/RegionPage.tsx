eca32b1a76a8ae6ef43003fa1b6548a6a3834bab
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-12-14 11:23:07 -0800

    fetch region projection

--- src/screens/LocationPage/RegionPage.tsx
+++ src/screens/LocationPage/RegionPage.tsx
@@ -2,10 +2,18 @@ import React, { Fragment } from 'react';
 import { Region } from 'common/regions';
 import { getPageTitle } from './utils';
 import AppMetaTags from 'components/AppMetaTags/AppMetaTags';
+import { useProjectionsFromRegion } from 'common/utils/model';
 
 const RegionPage: React.FC<{ region: Region }> = ({ region }) => {
   const pageTitle = getPageTitle(region);
   const canonicalUrl = region.canonicalUrl;
+  const projections = useProjectionsFromRegion(region);
+
+  if (!projections) {
+    return null;
+  }
+
+  const projection = projections.primary;
 
   return (
     <Fragment>
