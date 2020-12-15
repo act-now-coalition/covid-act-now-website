6d9a2bb27adf4b8456c722a31ada3a207a7957a5
Chris Kelly <ghop02@gmail.com>
2020-12-14 20:38:23 -0600

    Change filtering

--- src/components/LocationPage/ChartsHolder.tsx
+++ src/components/LocationPage/ChartsHolder.tsx
@@ -24,7 +24,7 @@ import {
 } from 'common/utils/recommend';
 import { mainContent } from 'cms-content/recommendations';
 import { getRecommendationsShareUrl } from 'common/urls';
-import { Region, getStateCode, getStateName } from 'common/regions';
+import { Region, getStateCode, getStateName, MetroArea } from 'common/regions';
 
 // TODO: 180 is rough accounting for the navbar and searchbar;
 // could make these constants so we don't have to manually update
@@ -185,7 +185,7 @@ const ChartsHolder = ({ projections, region, chartId }: ChartsHolderProps) => {
             />
           ))}
         </MainContentInner>
-        {stateCode && stateName && (
+        {!(region instanceof MetroArea) && (
           <MainContentInner ref={exploreChartRef} id="explore-chart">
             <Explore
               initialFipsList={initialFipsList}
