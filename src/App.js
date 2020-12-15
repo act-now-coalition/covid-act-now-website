0eb75f10409172e1ba3008d14c4a80199cc20df2
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-12-15 09:45:22 -0800

    enable MSA routes

--- src/App.js
+++ src/App.js
@@ -73,8 +73,7 @@ export default function App() {
                 path="/us/:stateId/county/:countyId"
                 component={LocationPage}
               />
-              {/* Enable following route to test metro areas */}
-              {/* <Route
+              <Route
                 exact
                 path="/us/metro/:metroAreaUrlSegment"
                 component={LocationPage}
@@ -88,7 +87,7 @@ export default function App() {
                 exact
                 path="/us/metro/:metroAreaUrlSegment/recommendations"
                 component={LocationPage}
-              /> */}
+              />
               <Route
                 exact
                 path="/us/:stateId/chart/:chartId"
