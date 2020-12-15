20765f88ed0cd4056242b6b520456f1c0d9b0710
Pablo Navarro <pnavarrc@gmail.com>
2020-12-15 13:27:13 -0800

    update the alert email signup form to support MSA (#2187)
    * update the alert email signup form to support MSA
    * remove isValidEmail from Newsletter/utils
    * remove unused properties

---
+++ src/components/Newsletter/utils.ts
@@ -0,0 +1,21 @@
+import { getFirebase, firebase } from 'common/firebase';
+import { Region, County } from 'common/regions';
+
+export function getDefaultRegions(region: Region): Region[] {
+  return region instanceof County ? [region, region.state] : [region];
+}
+
+export function subcribeToLocations(emailAddress: string, fipsList: string[]) {
+  // Merge the locations with any existing ones since that's _probably_ what the user wants.
+  return getFirebase()
+    .firestore()
+    .collection('alerts-subscriptions')
+    .doc(emailAddress.toLocaleLowerCase())
+    .set(
+      {
+        locations: firebase.firestore.FieldValue.arrayUnion(...fipsList),
+        subscribedAt: firebase.firestore.FieldValue.serverTimestamp(),
+      },
+      { merge: true },
+    );
+}
