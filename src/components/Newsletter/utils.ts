9d5a29b2a44bb5c1fdf2c0324b1f6be28b16b717
Pablo Navarro Castillo <pnavarrc@gmail.com>
2020-12-15 12:57:06 -0800

    remove isValidEmail from Newsletter/utils

--- src/components/Newsletter/utils.ts
+++ src/components/Newsletter/utils.ts
@@ -1,17 +1,8 @@
 import { getFirebase, firebase } from 'common/firebase';
 import { Region, County } from 'common/regions';
 
-// Taken from https://ui.dev/validate-email-address-javascript/
-const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
 export function getDefaultRegions(region: Region): Region[] {
   return region instanceof County ? [region, region.state] : [region];
-}
-
-export function isValidEmail(emailAddress: string) {
-  return (
-    emailAddress && emailAddress.length > 1 && EMAIL_REGEX.test(emailAddress)
-  );
 }
 
 export function subcribeToLocations(emailAddress: string, fipsList: string[]) {
