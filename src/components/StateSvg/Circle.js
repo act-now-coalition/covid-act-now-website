b56604b0161a9d2a0659db6647218e71549f476b
chasulin <44076375+chasulin@users.noreply.github.com>
2020-12-16 00:01:28 -0500

    removes usused code and props from StateCircleSvg.js and Circle.js

--- src/components/StateSvg/Circle.js
+++ src/components/StateSvg/Circle.js
@@ -1,46 +1,12 @@
 import React from 'react';
 import * as States from './index';
-
-import { COLOR_MAP } from 'common/colors';
 import { CircleWrapper, StateWrapper, ActionWrapper } from './Circle.style';
 
-const CircleStateAction = ({
-  state,
-  ratio = 1,
-  actionBackgroundFill = '#FFFFFF',
-  fillColor,
-  hasAction = false,
-}) => {
+const CircleStateAction = ({ state, ratio = 1, fillColor }) => {
   const State = States[state];
-  const getAction = () => {
-    switch (fillColor) {
-      case COLOR_MAP.GREEN.BASE:
-        return (
-          <path
-            d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM10 17L5 12L6.41 10.59L10 14.17L17.59 6.58L19 8L10 17Z"
-            fill={fillColor}
-          />
-        );
-      case COLOR_MAP.RED.BASE:
-      case COLOR_MAP.ORANGE.BASE:
-      case COLOR_MAP.GRAY.BASE:
-        return (
-          <path
-            d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM13 17H11V15H13V17ZM13 13H11V7H13V13Z"
-            fill={
-              fillColor === COLOR_MAP.GRAY.BASE
-                ? COLOR_MAP.GRAY.DARK
-                : fillColor
-            } // for legibility
-          />
-        );
-      default:
-    }
-  };
 
   const DEFAULT_SIZE = 64;
   const DEFAULT_CIRCLE_RADIUS = 32;
-  const DEFAULT_SMALL_CIRCLE_RADIUS = 12;
   const ACTION_SIZE = 24;
 
   const size = ratio * DEFAULT_SIZE;
@@ -76,19 +42,7 @@ const CircleStateAction = ({
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <g>
-            {hasAction && (
-              <>
-                <circle
-                  cx="12"
-                  cy="12"
-                  r={ratio * DEFAULT_SMALL_CIRCLE_RADIUS}
-                  fill={actionBackgroundFill}
-                />
-                {getAction()}
-              </>
-            )}
-          </g>
+          <g></g>
         </svg>
       </ActionWrapper>
     </CircleWrapper>
--- src/components/StateSvg/StateCircleSvg.js
+++ src/components/StateSvg/StateCircleSvg.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import Circle from './Circle';
 
-const StateCircleSvg = ({ state, ratio, fillColor, hasAction = false }) => {
-  return (
-    <Circle
-      ratio={ratio}
-      actionBackgroundFill="#FFFFFF"
-      hasAction={hasAction}
-      state={state}
-      fillColor={fillColor}
-    />
-  );
+const StateCircleSvg = ({ state, ratio, fillColor }) => {
+  return <Circle ratio={ratio} state={state} fillColor={fillColor} />;
 };
 
 export default StateCircleSvg;
