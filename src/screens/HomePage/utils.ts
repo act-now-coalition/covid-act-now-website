89c3c17eb476a32b3ddad07e7567eb54b4fa6d1d
Pablo Navarro <pnavarrc@gmail.com>
2020-12-17 09:19:43 -0800

    remove unused function (#2215)

--- src/screens/HomePage/utils.ts
+++ src/screens/HomePage/utils.ts
@@ -1,16 +1,6 @@
-import { STATES } from 'common/locations';
-import { shuffle, random, take, chain } from 'lodash';
+import { chain } from 'lodash';
 import { getAllStates, SummaryForCompare } from 'common/utils/compare';
 import { Metric } from 'common/metric';
-
-/**
- * Returns between 1 and 3 random state fips codes.
- */
-export function getRandomStateFipsList() {
-  const numStates = random(1, 3);
-  const stateList = take(shuffle(STATES), numStates);
-  return stateList.map(state => state.state_fips_code);
-}
 
 function sortByCaseIncidenceDesc(stateInfo: SummaryForCompare) {
   return -1 * (stateInfo.metricsInfo.metrics[Metric.CASE_DENSITY]?.value || 0);
