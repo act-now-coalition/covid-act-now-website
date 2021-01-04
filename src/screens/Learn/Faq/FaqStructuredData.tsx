6a24d2f3929970e159502847931a3821f7e18935
chasulin <44076375+chasulin@users.noreply.github.com>
2021-01-04 16:59:44 -0500

    reworks syntax

--- src/screens/Learn/Faq/FaqStructuredData.tsx
+++ src/screens/Learn/Faq/FaqStructuredData.tsx
@@ -1,32 +1,25 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import {
   JSONLD,
   Question,
   Answer,
+  Generic,
   GenericCollection,
   // @ts-ignore: getting error for this import :(
 } from 'react-structured-data';
-import {
-  faqContent,
-  FaqSection,
-  Question as QuestionItem,
-} from 'cms-content/learn';
+import { faqQuestionItems } from 'cms-content/learn';
 
 const FaqStructuredData: React.FC = () => {
-  const { sections } = faqContent;
   return (
     <JSONLD>
-      <GenericCollection type="FAQPage">
-        {sections.map((section: FaqSection) => {
-          const { questions } = section;
-          questions.map((questionItem: QuestionItem, i: number) => {
-            return (
-              <Fragment key={`question-${i}`}>
-                <Question name={questionItem.question} />
-                <Answer text={questionItem.answer} />
-              </Fragment>
-            );
-          });
+      <GenericCollection type="FAQPage" jsonldtype="FAQPage">
+        {faqQuestionItems.map((item: any) => {
+          return (
+            <Generic type="FAQPage" jsonldtype="FAQPage">
+              <Question name={item.question} />
+              <Answer text={item.answer} />
+            </Generic>
+          );
         })}
       </GenericCollection>
     </JSONLD>
