6482f6d9c2d525d619a965001049798af8f7524b
chasulin <44076375+chasulin@users.noreply.github.com>
2021-01-04 12:25:39 -0500

    adds start of structured data for FAQ

---
+++ src/screens/Learn/Faq/FaqStructuredData.tsx
@@ -0,0 +1,36 @@
+import React, { Fragment } from 'react';
+import {
+  JSONLD,
+  Question,
+  Answer,
+  GenericCollection,
+  // @ts-ignore: getting error for this import :(
+} from 'react-structured-data';
+import {
+  faqContent,
+  FaqSection,
+  Question as QuestionItem,
+} from 'cms-content/learn';
+
+const FaqStructuredData: React.FC = () => {
+  const { sections } = faqContent;
+  return (
+    <JSONLD>
+      <GenericCollection type="FAQPage">
+        {sections.forEach((section: FaqSection) => {
+          const { questions } = section;
+          questions.map((questionItem: QuestionItem, i: number) => {
+            return (
+              <Fragment key={`question-${i}`}>
+                <Question>{questionItem.question}</Question>
+                <Answer>{questionItem.answer}</Answer>
+              </Fragment>
+            );
+          });
+        })}
+      </GenericCollection>
+    </JSONLD>
+  );
+};
+
+export default FaqStructuredData;
