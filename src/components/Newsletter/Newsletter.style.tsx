35b982a281a32dbbf51a3957704ae8c62fda08b2
Michael Lehenbauer <michael@covidactnow.org>
2021-01-05 15:04:46 -0800

    Update alert/newsleter signup to not store alert subscribers in Campaign Monitor. (#2341)
    For historical reasons, when somebody signed up for alerts or the newsletter, we were using a Campaign Monitor signup form that would:
    
    Always add the user to the "Alert Signups" list in Campaign Monitor.
    Optionally (depending on whether they checked the newsletter checkbox) add them to the "Full Sign Up List_Scrubbed" list in Campaign Monitor.
    But we don't actually use Campaign Monitor at all for alert emails anymore, so we don't need to sign them up to the first list, and it was costing us extra money.
    
    So I've regenerated the form so that it only signs them up for "Full Sign Up List_Scrubbed" and we only actually submit the form if they check the newsletter checkbox.
    
    Since we only sometimes submit the Campaign Monitor signup form now, I had to invent a new subscription confirmation UI if they sign up for alerts but not the newsletter, so I added a little UI to change the "Sign up" button to say "Subscribed!" for 3 seconds after they sign up for alerts (but not the newsletter).
    
    I've manually verified that checking the box and clicking "sign up" correctly results in the email getting added to "Full Sign Up List_Scrubbed" in Campaign Monitor as desired.

--- src/components/Newsletter/Newsletter.style.tsx
+++ src/components/Newsletter/Newsletter.style.tsx
@@ -58,33 +58,35 @@ export const StyledNewsletter = styled.div`
         display: none;
       }
     }
+  }
+`;
 
-    button[type='submit'] {
-      cursor: pointer;
-      display: block;
-      appearance: none;
-      box-sizing: border-box;
-      height: 3.5rem;
-      flex-shrink: 0;
-      flex: 1;
-      outline: 0;
-      border-top-right-radius: 4px;
-      border-bottom-right-radius: 4px;
-      appearance: none;
-      font-size: 0.875rem;
-      padding: 0.25rem 1.25rem;
-      line-height: 1rem;
-      text-transform: uppercase;
-      transition: 0.3s ease background-color;
-      background-color: ${palette.info.main};
-      border: 1px solid ${palette.info.dark};
-      color: ${palette.white};
-      font-weight: 700;
-      font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+export const SubscribeButton = styled.button<{ confirmation: boolean }>`
+  cursor: pointer;
+  display: block;
+  appearance: none;
+  box-sizing: border-box;
+  height: 3.5rem;
+  flex-shrink: 0;
+  flex: 1;
+  outline: 0;
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
+  appearance: none;
+  font-size: 0.875rem;
+  padding: 0.25rem 1.25rem;
+  line-height: 1rem;
+  text-transform: uppercase;
+  transition: 0.3s ease background-color;
+  background-color: ${props =>
+    props.confirmation ? palette.success.light : palette.info.main};
+  border: 1px solid ${palette.info.dark};
+  color: ${palette.white};
+  font-weight: 700;
+  font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
 
-      &:hover {
-        background-color: ${palette.info.dark};
-      }
-    }
+  &:hover {
+    background-color: ${props =>
+      props.confirmation ? palette.success.light : palette.info.dark};
   }
 `;
