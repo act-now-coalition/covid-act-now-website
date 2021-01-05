28a2994461084bedbaa0cbae9dd1f9a10fc52b6c
chasulin <44076375+chasulin@users.noreply.github.com>
2021-01-05 12:53:32 -0500

    renames prop

--- src/screens/HomePage/utils.ts
+++ src/screens/HomePage/utils.ts
@@ -2,11 +2,11 @@ import { chain } from 'lodash';
 import regions from 'common/regions';
 
 /* Sets homepage explore presets to be 5 largest metro areas */
-export function getLocationFipsCodesForExplore(numStates: number) {
+export function getLocationFipsCodesForExplore(numRegions: number) {
   return chain(regions.metroAreas)
     .sortBy(metro => metro.population)
     .reverse()
-    .take(numStates)
+    .take(numRegions)
     .map(locationInfo => locationInfo.fipsCode)
     .value();
 }
