605002a12ac4e0f0cf36db820274f446f89741cb
chasulin <44076375+chasulin@users.noreply.github.com>
2021-01-06 11:27:23 -0500

    Update page titles and descriptions (#2354)

--- cypress/integration/ModelPage_spec.js
+++ cypress/integration/ModelPage_spec.js
@@ -3,7 +3,7 @@ describe('/state/:id', () => {
     cy.visit('/us/ny');
     cy.title().should(
       'eq',
-      'New York (NY) - COVID Risk Map & Key Metrics - Covid Act Now',
+      'New York (NY) - COVID Data & Key Metrics - Covid Act Now',
     );
     cy.get('head meta[name="description"]')
       .should('have.attr', 'content')
