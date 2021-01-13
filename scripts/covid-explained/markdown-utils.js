9c5b18f65460825b8e69950b7beba1ba825e603a
Pablo Navarro Castillo <pnavarrc@gmail.com>
2021-01-12 17:42:20 -0800

    add covid explained scripts

---
+++ scripts/covid-explained/markdown-utils.js
@@ -0,0 +1,66 @@
+import { chain } from 'lodash';
+import remark from 'remark';
+import visit from 'unist-util-visit';
+
+function isInternalLink(url) {
+  return (
+    url.startsWith('https://explaincovid.org') ||
+    url.startsWith('https://covidexplained.org')
+  );
+}
+
+export function logInternalLinks(markdownContent) {
+  remark().use(pluginLogInternalLinks).processSync(markdownContent);
+}
+
+export function transformInternalLinks(markdownContent) {
+  return remark()
+    .use(pluginTransformInternalLinks)
+    .processSync(markdownContent)
+    .toString();
+}
+
+// URL Mapping between Covid Explained URLs and CAN URLs
+const linkReplacements = [
+  {
+    from: 'https://explaincovid.org/basics/masks-what-is-the-deal',
+    to: '/deep-dives/masks-what-is-the-deal',
+  },
+];
+
+const linkMap = chain(linkReplacements)
+  .map(({ from, to }) => [from, to])
+  .fromPairs()
+  .value();
+
+// Remark Plugins
+function pluginTransformInternalLinks() {
+  function transformer(tree, file) {
+    visit(tree, 'link', visitor);
+
+    function visitor(node) {
+      const { url } = node;
+      if (linkMap[url]) {
+        node.url = linkMap[url];
+      }
+      return node;
+    }
+  }
+  return transformer;
+}
+
+// Log internal links to the console
+function pluginLogInternalLinks() {
+  function transformer(tree) {
+    visit(tree, 'link', visitor);
+
+    function visitor(node) {
+      const { url } = node;
+      if (isInternalLink(url)) {
+        console.log(`    ${url}`);
+      }
+      return node;
+    }
+  }
+  return transformer;
+}
---
+++ scripts/covid-explained/report-internal-links.ts
@@ -0,0 +1,29 @@
+/**
+ * Parses the content of the Covid Explained articles to migrate and logs
+ * internal links.
+ *
+ * yarn covid-explained-internal-links
+ */
+import { getItemsToMigrate } from './utils';
+import { logInternalLinks } from './markdown-utils';
+
+async function main() {
+  const migrationItems = await getItemsToMigrate();
+  for (const item of migrationItems) {
+    console.log(item.originalPath);
+    logInternalLinks(item.content.body);
+  }
+  return true;
+}
+
+if (require.main === module) {
+  main()
+    .then(() => {
+      console.log('Done');
+      process.exit(0);
+    })
+    .catch(err => {
+      console.error(err);
+      process.exit(-1);
+    });
+}
