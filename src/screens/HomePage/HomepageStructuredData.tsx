27ae2369237df79174867176cddcea014747350d
chasulin <44076375+chasulin@users.noreply.github.com>
2021-01-19 11:58:33 -0500

    adds org structured data to homepage

--- src/screens/HomePage/HomepageStructuredData.tsx
+++ src/screens/HomePage/HomepageStructuredData.tsx
@@ -3,17 +3,26 @@ import { getMapImageUrl } from 'common/urls';
 import { Helmet } from 'react-helmet';
 
 const HomepageStructuredData: React.FC = () => {
-  const jsonData = JSON.stringify({
+  const imageData = JSON.stringify({
     '@context': 'https://schema.org',
     '@type': 'ImageObject',
     url: getMapImageUrl(),
     width: 1200,
     height: 630,
   });
+  const organizationData = JSON.stringify({
+    '@context': 'https://schema.org',
+    '@type': 'Organization',
+    name: 'Covid Act Now',
+    url: 'https://covidactnow.org/',
+    logo:
+      'https://covidactnow.org/static/media/covid-act-now-logo.0ac0983b.png',
+  });
 
   return (
     <Helmet>
-      <script type="application/ld+json">{jsonData}</script>
+      <script type="application/ld+json">{imageData}</script>
+      <script type="application/ld+json">{organizationData}</script>
     </Helmet>
   );
 };
