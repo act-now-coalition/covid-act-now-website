df785c8ca68ec487069a213ab8238318d26fc875
chasulin <44076375+chasulin@users.noreply.github.com>
2021-01-19 13:23:26 -0500

    fixes can logo url

--- src/screens/HomePage/HomepageStructuredData.tsx
+++ src/screens/HomePage/HomepageStructuredData.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { getMapImageUrl } from 'common/urls';
 import { Helmet } from 'react-helmet';
+import canLogo from 'assets/images/covid-act-now-logo.png';
+import urlJoin from 'url-join';
 
 const HomepageStructuredData: React.FC = () => {
+  const canLogoUrl = urlJoin('https://covidactnow.org', canLogo);
+
   const imageData = JSON.stringify({
     '@context': 'https://schema.org',
     '@type': 'ImageObject',
@@ -10,11 +14,12 @@ const HomepageStructuredData: React.FC = () => {
     width: 1200,
     height: 630,
   });
+
   const organizationData = JSON.stringify({
     '@context': 'https://schema.org',
     '@type': 'Organization',
     name: 'Covid Act Now',
-    url: 'https://covidactnow.org/',
+    url: canLogoUrl,
     logo:
       'https://covidactnow.org/static/media/covid-act-now-logo.0ac0983b.png',
   });
