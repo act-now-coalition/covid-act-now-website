b204458157f2b9fa7d12b4ca7e0cce8d6a25141d
chasulin <44076375+chasulin@users.noreply.github.com>
2021-01-20 17:52:21 -0500

    Adds toggle component

---
+++ src/screens/HomePage/Toggle/Toggle.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { SwitchComponent } from 'components/SharedComponents';
+import { Wrapper } from './Toggle.style';
+
+const Toggle: React.FC<{
+  showCounties: boolean;
+  onClickSwitch: () => void;
+}> = ({ showCounties, onClickSwitch }) => {
+  return (
+    <Wrapper>
+      <SwitchComponent
+        leftLabel="States"
+        rightLabel="Counties"
+        checked={showCounties}
+        onChange={onClickSwitch}
+      />
+    </Wrapper>
+  );
+};
+
+export default Toggle;
