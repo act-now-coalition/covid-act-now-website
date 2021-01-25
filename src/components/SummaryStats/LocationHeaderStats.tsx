4f887e1f745717bfbd0131084da944d6b2f99a15
Pablo Navarro <pnavarrc@gmail.com>
2021-01-25 14:47:00 -0800

    analytics: add tracking for the search box (#2416)
    * add key to the recommend half
    
    * disable debug tracking
    
    * improve event label
    
    * updates getOptionLabel
    
    Co-authored-by: chasulin <44076375+chasulin@users.noreply.github.com>

--- src/components/SummaryStats/LocationHeaderStats.tsx
+++ src/components/SummaryStats/LocationHeaderStats.tsx
@@ -184,8 +184,9 @@ const LocationHeaderStats = (props: {
           isHeader={props.isHeader}
           condensed={props.condensed}
         >
-          {ALL_METRICS.map(metric => (
+          {ALL_METRICS.map((metric, i) => (
             <SummaryStat
+              key={`stat-${i}`}
               onClick={() => props.onMetricClick && props.onMetricClick(metric)}
               chartType={metric}
               value={props.stats[metric] as number}
