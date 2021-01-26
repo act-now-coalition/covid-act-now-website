439bee4ff8e2c2868d3ec9aeca34b98af92ef722
chasulin <44076375+chasulin@users.noreply.github.com>
2021-01-26 11:27:01 -0500

    Fixes broken internal urls (#2539)

--- src/components/Markdown/utils.ts
+++ src/components/Markdown/utils.ts
@@ -1,11 +1,28 @@
+export function isInternalUrlWithCan(href: string) {
+  return href.includes('covidactnow.org');
+}
+
 export function isInternalLink(href: string) {
-  return href.startsWith('#') || href.startsWith('/');
+  return (
+    href.startsWith('#') || href.startsWith('/') || isInternalUrlWithCan(href)
+  );
+}
+
+export function formatInternalLink(href: string) {
+  if (isInternalUrlWithCan(href)) {
+    const canUrl = new URL(href);
+    return `${canUrl.pathname}${canUrl.hash}`;
+  } else {
+    return href;
+  }
 }
 
 export function isValidURL(href: string): boolean {
   const isInternal = isInternalLink(href);
   try {
-    isInternal ? new URL(href, 'https://covidactnow.org') : new URL(href);
+    isInternal
+      ? new URL(formatInternalLink(href), 'https://covidactnow.org')
+      : new URL(href);
   } catch (err) {
     console.error(err, href);
     return false;
