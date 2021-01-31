e38892139d196b4edfb8364ebf66fb0138c645cb
Pablo Navarro Castillo <pnavarrc@gmail.com>
2021-01-31 14:31:24 -0800

    add tracking props to LinkButton

---
+++ src/components/LinkButton/LinkButton.style.ts
@@ -0,0 +1,12 @@
+import MuiButton from '@material-ui/core/Button';
+import styled from 'styled-components';
+
+export const StyledButton = styled(MuiButton).attrs(props => ({
+  disableRipple: true,
+  disableFocusRipple: true,
+}))`
+  &.Mui-focusVisible {
+    outline: blue auto 1px;
+    outline: -webkit-focus-ring-color auto 1px;
+  }
+`;
---
+++ src/components/LinkButton/LinkButton.tsx
@@ -0,0 +1,60 @@
+import React, { ComponentProps, DetailedHTMLProps } from 'react';
+import { Link, LinkProps } from 'react-router-dom';
+import { assert } from 'common/utils';
+import { StyledButton } from './LinkButton.style';
+import { EventAction, EventCategory, trackEvent } from 'components/Analytics';
+
+type AnchorLinkType = DetailedHTMLProps<
+  React.AnchorHTMLAttributes<HTMLAnchorElement>,
+  HTMLAnchorElement
+>;
+
+type StyledButtonProps = ComponentProps<typeof StyledButton>;
+
+type ExternalLinkButtonProps = StyledButtonProps & AnchorLinkType;
+
+type InternalLinkButtonProps = StyledButtonProps & LinkProps;
+
+interface TrackingProps {
+  trackingCategory: EventCategory;
+  trackingAction?: EventAction;
+  trackingLabel: string;
+}
+
+export type LinkButtonProps = InternalLinkButtonProps | ExternalLinkButtonProps;
+
+const LinkButton: React.FC<LinkButtonProps & TrackingProps> = props => {
+  assert(
+    props.href || props.to,
+    "LinkButton needs either 'to' or 'href' as props",
+  );
+
+  const isInternalLink = props.to;
+  const { trackingAction, trackingCategory, trackingLabel } = props;
+
+  const onClick = (ev: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    const action =
+      trackingAction || isInternalLink
+        ? EventAction.NAVIGATE
+        : EventAction.CLICK_LINK;
+    if (trackingCategory && trackingLabel) {
+      trackEvent(trackingCategory, action, trackingLabel);
+    }
+    props.onClick && props.onClick(ev);
+  };
+
+  if (isInternalLink) {
+    return <StyledButton {...props} component={Link} onClick={onClick} />;
+  } else {
+    return (
+      <StyledButton
+        {...props}
+        target="_blank"
+        rel="noopener noreferrer"
+        onClick={onClick}
+      />
+    );
+  }
+};
+
+export default LinkButton;
