6b0e817f24822697dbe31153d45446e13c76ce1f
chasulin <44076375+chasulin@users.noreply.github.com>
2021-01-30 21:32:59 -0500

    implements mobile mocks

--- src/components/Search/Homepage/HomepageSearchAutocomplete.style.tsx
+++ src/components/Search/Homepage/HomepageSearchAutocomplete.style.tsx
@@ -1,12 +1,13 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Paper, TextField } from '@material-ui/core';
 import COLORS, { COLOR_MAP } from 'common/colors';
 import { materialSMBreakpoint } from 'assets/theme/sizes';
 import SearchIcon from '@material-ui/icons/Search';
+import ArrowBackOutlinedIcon from '@material-ui/icons/ArrowBackOutlined';
 
 const maxMenuHeight = 240;
-export const desktopWidth = 732;
-export const mobileWidth = 350;
+const desktopWidth = 732;
+const mobileWidth = 350;
 
 export const StyledTextField = styled(TextField).attrs(props => ({
   variant: 'outlined',
@@ -25,15 +26,16 @@ export const StyledTextField = styled(TextField).attrs(props => ({
 `;
 
 export const Wrapper = styled.div<{ isOpen: boolean }>`
-  box-shadow: ${({ isOpen }) => isOpen && '0px 2px 24px rgba(0, 0, 0, 0.12)'};
-  border-radius: ${({ isOpen }) => (isOpen ? '4px 4px 0 0' : '4px')};
-  padding: 1.5rem 0.5rem 0;
-  position: relative;
-  width: ${mobileWidth}px;
+  ${({ isOpen }) => (isOpen ? MobileWrapperOpened : MobileWrapperClosed)};
 
   @media (min-width: ${materialSMBreakpoint}) {
     width: ${desktopWidth}px;
     padding: 1.5rem 1.5rem 0;
+
+    position: relative;
+    height: unset;
+    box-shadow: ${({ isOpen }) => isOpen && '0px 2px 24px rgba(0, 0, 0, 0.12)'};
+    border-radius: ${({ isOpen }) => (isOpen ? '4px 4px 0 0' : '4px')};
   }
 `;
 
@@ -42,15 +44,60 @@ export const SearchBarIcon = styled(SearchIcon)`
   margin-left: 0.5rem;
 `;
 
-export const SearchDirections = styled.span`
-  font-size: 0.875rem;
+const MobileWrapperOpened = css`
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: white;
+  height: 100%;
+  width: 100%;
+  z-index: 10000;
+
+  padding: 1.5rem 0.5rem 0;
+  padding: 0.75rem 0.5rem 1.5rem;
+  padding: 1.5rem;
+`;
+
+const MobileWrapperClosed = css`
+  width: ${mobileWidth}px;
+  position: relative;
+  height: unset;
+  padding: 0.75rem 0.5rem 1.5rem;
+`;
+
+export const MobileSearchDirections = styled.div<{ isOpen: boolean }>`
+  display: ${({ isOpen }) => (isOpen ? 'flex' : 'none')};
   color: ${COLOR_MAP.GRAY_BODY_COPY};
-  display: flex;
-  justify-content: center;
-  margin: 0.25rem 0 0.75rem;
-  height: 24px;
+  margin-bottom: 1.25rem;
+  font-size: 1rem;
+  line-height: 1.4;
+
+  span {
+    position: relative;
+    width: 100%;
+    text-align: center;
+  }
 
   @media (min-width: ${materialSMBreakpoint}) {
+    display: none;
+  }
+`;
+
+export const BackArrowIcon = styled(ArrowBackOutlinedIcon)`
+  position: absolute;
+  color: black;
+  font-size: 1.5rem;
+`;
+
+export const DesktopSearchDirections = styled.span`
+  display: none;
+  @media (min-width: ${materialSMBreakpoint}) {
+    color: ${COLOR_MAP.GRAY_BODY_COPY};
+    display: flex;
+    justify-content: center;
+    margin: 0.25rem 0 0.75rem;
+    height: 24px;
+
     font-size: 1rem;
     margin: 0.75rem 0 0.75rem;
   }
@@ -58,7 +105,7 @@ export const SearchDirections = styled.span`
 
 export const ListContainer = styled.ul`
   padding: 0 1.25rem 0;
-  max-height: ${maxMenuHeight}px;
+  // max-height: ${maxMenuHeight}px;
 
   li {
     padding: 0;
@@ -66,16 +113,20 @@ export const ListContainer = styled.ul`
 
   @media (min-width: ${materialSMBreakpoint}) {
     padding: 0 1.75rem 0;
+    max-height: ${maxMenuHeight}px;
   }
 `;
 
 export const StyledPaper = styled(Paper)`
-  box-shadow: 0px 26px 24px rgba(0, 0, 0, 0.12);
+  // box-shadow: 0px 26px 24px rgba(0, 0, 0, 0.12);
   width: ${mobileWidth}px;
   border-radius: 0 0 4px 4px;
+  box-shadow: none;
+  width: 100%;
 
   @media (min-width: ${materialSMBreakpoint}) {
     max-height: ${maxMenuHeight}px;
     width: ${desktopWidth}px;
+    box-shadow: 0px 26px 24px rgba(0, 0, 0, 0.12);
   }
 `;
