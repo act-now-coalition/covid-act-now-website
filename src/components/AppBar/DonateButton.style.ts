c9940ec6b02c9a879b49a14411a1f6eecd55910f
chasulin <44076375+chasulin@users.noreply.github.com>
2021-02-01 19:42:29 -0500

    makes optimize script async. removes code related to homepage+donate button experiments

--- src/components/AppBar/DonateButton.style.ts
+++ src/components/AppBar/DonateButton.style.ts
@@ -3,35 +3,22 @@ import { COLOR_MAP } from 'common/colors';
 import { mobileBreakpoint } from 'assets/theme/sizes';
 import LinkButton from 'components/LinkButton';
 
-const DonateButtonBase = styled(LinkButton).attrs(props => ({
+export const StyledDonateButton = styled(LinkButton).attrs(props => ({
   variant: 'contained',
 }))`
   margin: auto 0;
   display: inline-flex;
   box-shadow: none;
-
-  @media (min-width: ${mobileBreakpoint}) {
-    margin-left: 2rem;
-  }
-`;
-
-export const StyledDonateButtonA = styled(DonateButtonBase)`
-  background-color: white;
-  color: ${COLOR_MAP.PURPLE};
-  border: 2px solid ${COLOR_MAP.GRAY.LIGHT};
-
-  &:hover {
-    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.12);
-  }
-`;
-
-export const StyledDonateButtonB = styled(DonateButtonBase)`
   background-color: ${COLOR_MAP.PURPLE};
   color: white;
 
   &:hover {
     background-color: ${COLOR_MAP.PURPLE};
     color: white;
+  }
+
+  @media (min-width: ${mobileBreakpoint}) {
+    margin-left: 2rem;
   }
 `;
 
