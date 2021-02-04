fc315bd202d03e327cbfc7826c8abf7df9fc2ed5
Pablo Navarro <pnavarrc@gmail.com>
2021-02-04 12:21:21 -0800

    vaccination: implement email alert footer component (#2649)

---
+++ src/components/EmailAlertsFooter/EmailAlertsFooter.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import EmailAlertsFooter from './EmailAlertsFooter';
+
+export default {
+  title: 'Shared Components/EmailAlertsFooter',
+  component: EmailAlertsFooter,
+};
+
+export const Example = () => <EmailAlertsFooter />;
