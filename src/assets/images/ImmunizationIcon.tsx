ff349c7207d4cd60974ea4417c0498bf7e2be969
Pablo Navarro Castillo <pnavarrc@gmail.com>
2021-02-04 16:12:54 -0800

    fix story and immunization icon

--- src/assets/images/ImmunizationIcon.tsx
+++ src/assets/images/ImmunizationIcon.tsx
@@ -8,7 +8,7 @@ const ImmunizationIcon: React.FC = () => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <g clip-path="url(#clip0)">
+    <g clipPath="url(#clip0)">
       <path
         d="M16.9965 0.789001C13.6175 3.771 9.18146 5.58 4.32046 5.58V18.86C4.32046 25.683 16.9965 31.212 16.9965 31.212C16.9965 31.212 29.6715 25.683 29.6715 18.86V5.58C24.8125 5.58 20.3735 3.771 16.9965 0.789001Z"
         fill="#00BFEA"
