3354f46bfd8ecfa9f19369e38c51ecfe637a321c
chasulin <44076375+chasulin@users.noreply.github.com>
2021-02-04 20:47:03 -0500

    removes counties and states from metro page defaults

--- src/components/EmailAlertsForm/utils.ts
+++ src/components/EmailAlertsForm/utils.ts
@@ -1,12 +1,10 @@
 import { getFirebase, firebase } from 'common/firebase';
-import { Region, County, MetroArea } from 'common/regions';
+import { Region, County } from 'common/regions';
 import { getGeolocatedRegions } from 'common/regions/utils';
 
 export function getDefaultRegions(region: Region): Region[] {
   if (region instanceof County) {
     return [region, region.state];
-  } else if (region instanceof MetroArea) {
-    return [region, ...region.counties, ...region.states];
   } else {
     return [region];
   }
