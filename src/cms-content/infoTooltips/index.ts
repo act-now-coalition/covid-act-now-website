4986579ef3f92a0603b4be548895e22a5da76a73
chasulin <44076375+chasulin@users.noreply.github.com>
2021-02-08 19:12:15 -0500

    types and passes content through infoTooltips/index

--- src/cms-content/infoTooltips/index.ts
+++ src/cms-content/infoTooltips/index.ts
@@ -1,4 +1,7 @@
 import { Markdown } from '../utils';
+import toolTipContent from './info-tooltips.json';
+import { Metric } from 'common/metric';
+import { assert } from 'common/utils';
 
 export interface InfoTooltip {
   title: string;
@@ -6,3 +9,30 @@ export interface InfoTooltip {
   body: Markdown;
   cta: Markdown;
 }
+
+function findContentById(id: string): InfoTooltip {
+  const tooltipContent = toolTipContent.tooltip.find(
+    (item: InfoTooltip) => item.id === id,
+  );
+  assert(
+    tooltipContent,
+    `Tooltip content unexpectedly not found for id: ${id}`,
+  );
+  return tooltipContent;
+}
+
+export const locationPageHeaderTooltipContent: InfoTooltip = findContentById(
+  'location-page-header',
+);
+
+type MetricToTooltipContent = {
+  [key in Metric]: InfoTooltip;
+};
+
+export const metricToTooltipContentMap: MetricToTooltipContent = {
+  [Metric.CASE_DENSITY]: findContentById('daily-new-cases'),
+  [Metric.CASE_GROWTH_RATE]: findContentById('infection-rate'),
+  [Metric.POSITIVE_TESTS]: findContentById('positive-test-rate'),
+  [Metric.VACCINATIONS]: findContentById('percent-vaccinated'),
+  [Metric.HOSPITAL_USAGE]: findContentById('icu-capacity-used'),
+};
