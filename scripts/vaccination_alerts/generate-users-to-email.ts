f940513629050a4395e61bae1431e4e968218643
Pablo Navarro Castillo <pnavarrc@gmail.com>
2021-02-08 18:29:13 -0800

    generate the user to email

--- scripts/vaccination_alerts/generate-users-to-email.ts
+++ scripts/vaccination_alerts/generate-users-to-email.ts
@@ -1,47 +1,87 @@
 import path from 'path';
-import fs from 'fs';
 import _ from 'lodash';
 import { getFirestore } from '../common/firebase';
+import {
+  EmailFips,
+  getLocationsToAlert,
+  readVaccinationAlerts,
+  getUserLocationsToAlert,
+  groupByFips,
+} from './utils';
 
 /**
+ * Given a `vaccination-alerts.json` file, determines the locations and users
+ * subscribed to alerts on those locations that will need to be notified of
+ * vaccination information updates.
  *
+ * 1. Determine the list of states with updated vaccination information using the
+ *    information in `vaccination-alerts.json`.
+ *
+ * 2. For each user, determine the list of states that cover the locations that
+ *    the user is subscribed to and that have vaccination updates.
+ *
+ * 3. Update the Firebase collection that contains the list of users to email for
+ *    each state and vaccination information version.
  */
+
 const alertsFilePath = path.join(__dirname, 'vaccination-alerts.json');
 
 async function main() {
-  // get locations from file
-  // Check for the file
-  const vaccineAlerts = fs.readFileSync(alertsFilePath, 'utf8');
-  const vaccineAlertsInfo = JSON.parse(vaccineAlerts);
+  const vaccinationAlertsInfo = readVaccinationAlerts(alertsFilePath);
 
-  const fipsUpdated = _.keys(vaccineAlertsInfo);
+  // Deterime the list of locations with updated vaccination infromation.
+  const locationsToAlert = getLocationsToAlert(vaccinationAlertsInfo);
 
-  console.log(vaccineAlertsInfo);
-
-  // query subscriptions using streaming
-  // if the user is part of locations to update, save the document
-  // process documents to create (user, state) pairs
-  // update Firestore collection
   const db = await getFirestore();
+  const alertSubscriptions = await db.collection('alerts-subscriptions').get();
 
-  const docList: [] = [];
+  const emailFipsList: EmailFips[] = [];
+  alertSubscriptions.forEach(doc => {
+    const email = doc.id;
+    const { locations: userLocations } = doc.data();
 
-  db.collection('alerts-subscriptions')
-    .limit(10)
-    .stream()
-    .on('data', docSnapshot => {
-      const { locations } = docSnapshot.data();
-      const email = docSnapshot.id;
-      console.log(`${email}: ${locations.join(', ')}`);
+    // We get the list of all the locations for each user, get the state of
+    // each location and find which states have updated vaccination information.
+    const userFipsToAlert = getUserLocationsToAlert(
+      userLocations,
+      locationsToAlert,
+    );
 
-      // should we email?
-      // fipsCode in vaccine-alerts.json
-    })
-    .on('end', () => {
-      console.log('Done');
-    });
+    if (userFipsToAlert.length > 0) {
+      // If the user is subscribed to locations with updates, we add a pair with
+      // [email, fipsCode] to `emailFipsList` for each state.
+      userFipsToAlert.forEach(fipsCode => {
+        emailFipsList.push([email, fipsCode]);
+      });
+    }
+  });
+
+  // Once we have a list of all the [email, fipsCode] pairs to email, we group them
+  // by fipsCode and update the collection with vaccination alerts.
+  const groupedByFips = groupByFips(emailFipsList);
+
+  for (const fipsCode in groupedByFips) {
+    const { emailAlertVersion } = vaccinationAlertsInfo[fipsCode];
+    const emailList = groupedByFips[fipsCode];
+    const emailMap = _.fromPairs(
+      emailList.map(email => [email, { sentAt: null }]),
+    );
+
+    await db
+      .collection(`vaccination-alerts/${fipsCode}/emailVersions`)
+      .doc(`${emailAlertVersion}`)
+      .set({ emails: emailMap });
+  }
 }
 
 if (require.main === module) {
-  main();
+  main()
+    .then(() => {
+      console.log('Done.');
+      process.exit(0);
+    })
+    .catch(err => {
+      console.error('Error', err);
+      process.exit(-1);
+    });
 }
--- scripts/vaccination_alerts/utils.ts
+++ scripts/vaccination_alerts/utils.ts
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import _ from 'lodash';
 import { getFirestore } from '../common/firebase';
 import {
@@ -16,6 +17,10 @@ export interface RegionVaccineVersion {
 export interface RegionVaccineVersionMap {
   [fipsCode: string]: RegionVaccineVersion;
 }
+
+export type FipsCode = string;
+export type Email = string;
+export type EmailFips = [Email, FipsCode];
 
 const VACCINATION_VERSIONS_COLLECTION = 'vaccination-info-updates';
 
@@ -57,4 +62,57 @@ export function getUpdatedVaccinationInfo(
     }
   }
   return updatedInfoByFips;
+}
+
+export function readVaccinationAlerts(
+  filePath: string,
+): RegionVaccinePhaseInfoMap {
+  const data = fs.readFileSync(filePath, 'utf8');
+  return JSON.parse(data);
+}
+
+/**
+ * Given a list of [email, fips] pairs, returns a list of emails grouped
+ * by FIPS code.
+ */
+export function groupByFips(
+  emailFipsList: EmailFips[],
+): { [fipsCode: string]: string[] } {
+  return _.chain(emailFipsList)
+    .groupBy(([email, fips]) => fips)
+    .map((values, key) => [key, values.map(_.first)])
+    .fromPairs()
+    .value();
+}
+
+/**
+ * Returns the list of locations that we need to notify of vaccination information
+ * changes. For now, is just the list of keys in the `vaccination-alerts.json` file.
+ */
+export function getLocationsToAlert(
+  vaccinationAlertInfo: RegionVaccinePhaseInfoMap,
+): FipsCode[] {
+  return _.keys(vaccinationAlertInfo);
+}
+
+/**
+ * Returns the list of FIPS codes that the user should be notified given a list of
+ * locations with vaccination updates. For now, we return the list of states that
+ * contains the user subscriptions, even if the user is not subscribed to the state
+ * directly.
+ *
+ * Example: The user is subscribed to ['42001', '29005', '56', '12', '35620'], and we
+ * have updates for ['29', '01'], then we will notify the user for the changes in
+ * vaccination information for '29', even if the user is not subscribed to alerts for
+ * '29' directly.
+ */
+export function getUserLocationsToAlert(
+  userLocations: FipsCode[],
+  locationsToAlert: FipsCode[],
+): FipsCode[] {
+  return _.chain(userLocations)
+    .map(fipsCode => fipsCode.substr(0, 2))
+    .uniq()
+    .intersection(locationsToAlert)
+    .value();
 }
