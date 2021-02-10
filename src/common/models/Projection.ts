44fa0896f97fa8969f66739f347248c80fdcdd4a
Chris Kelly <ghop02@gmail.com>
2021-02-10 09:54:41 -0500

    remove knox county test positivity block + lewiston, OH vax block

--- src/common/models/Projection.ts
+++ src/common/models/Projection.ts
@@ -41,9 +41,7 @@ export const DISABLED_METRICS: { [metric in Metric]: DisabledFipsList } = {
   [Metric.CASE_DENSITY]: new DisabledFipsList([]),
   [Metric.CASE_GROWTH_RATE]: new DisabledFipsList([]),
   [Metric.HOSPITAL_USAGE]: new DisabledFipsList([]),
-  [Metric.POSITIVE_TESTS]: new DisabledFipsList([
-    '47093', // https://trello.com/c/pmB5mGZU/883-disabled-knox-county-tn-test-positivity
-  ]),
+  [Metric.POSITIVE_TESTS]: new DisabledFipsList([]),
   [Metric.VACCINATIONS]: new DisabledFipsList([
     //https://trello.com/c/9OCK0e3O/878-maine-vaccination-metric-data-off-by-2-decimal-places-likely-ratio-vs-percentage-bug
     '23001',
@@ -63,7 +61,6 @@ export const DISABLED_METRICS: { [metric in Metric]: DisabledFipsList } = {
     '23029',
     '23031',
     '30340',
-    '30620',
     '38860',
   ]),
 };
