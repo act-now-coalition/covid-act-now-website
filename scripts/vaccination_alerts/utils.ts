fade2c5c47339e57ce4a5b96a7e0b42bc1d2afa0
Pablo Navarro Castillo <pnavarrc@gmail.com>
2021-02-10 16:01:52 -0800

    code cleanup

--- scripts/vaccination_alerts/utils.ts
+++ scripts/vaccination_alerts/utils.ts
@@ -171,8 +171,6 @@ export function generateEmailData(emailAddress: string, fipsCode: string) {
   const emailData = getEmailAlertData(fipsCode);
   const emailHtmlContent = renderEmail(emailData);
 
-  fs.writeFileSync('alert.html', emailHtmlContent);
-
   return {
     Subject: emailData.emailSubjectLine,
     To: [emailAddress],
