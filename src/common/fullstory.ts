be0956d05b88f5b9127bec480019a61d93e33249
Pablo Navarro Castillo <pnavarrc@gmail.com>
2021-02-11 15:28:07 -0800

    minor fix

--- src/common/fullstory.ts
+++ src/common/fullstory.ts
@@ -52,28 +52,14 @@ function initStorage() {
 // From https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#testing_for_availability
 function storageAvailable(storageType: string) {
   let storage;
-  const x = '__storage_test__';
+  const testKey = '__storage_test__';
   try {
     // @ts-ignore
     storage = window[storageType];
-    storage.setItem(x, x);
-    storage.removeItem(x);
+    storage.setItem(testKey, testKey);
+    storage.removeItem(testKey);
     return true;
   } catch (e) {
-    return (
-      e instanceof DOMException &&
-      // everything except Firefox
-      (e.code === 22 ||
-        // Firefox
-        e.code === 1014 ||
-        // test name field too, because code might not be present
-        // everything except Firefox
-        e.name === 'QuotaExceededError' ||
-        // Firefox
-        e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
-      // acknowledge QuotaExceededError only if there's something already stored
-      storage &&
-      storage.length !== 0
-    );
+    return false;
   }
 }
