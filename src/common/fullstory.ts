29833038a4d849bbd9d63cd2a22e5e5757d9173f
Pablo Navarro Castillo <pnavarrc@gmail.com>
2021-02-11 11:17:48 -0800

    better detection of localStorage availability

--- src/common/fullstory.ts
+++ src/common/fullstory.ts
@@ -20,6 +20,10 @@ enum RecordSession {
 }
 
 export function initFullStory() {
+  if (!storageAvailable('localStorage')) {
+    return;
+  }
+
   initStorage();
 
   // If we can't find the value in localStorage we won't enable recordings in FullStory
@@ -42,5 +46,34 @@ function initStorage() {
     // It doesn't matter if we can't write to localStorage, we only need this
     // for 1% of the users.
     console.warn(`Error writing to localStorage`, err);
+  }
+}
+
+// From https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#testing_for_availability
+function storageAvailable(storageType: string) {
+  let storage;
+  const x = '__storage_test__';
+  try {
+    // @ts-ignore
+    storage = window[storageType];
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === 'QuotaExceededError' ||
+        // Firefox
+        e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
   }
 }
