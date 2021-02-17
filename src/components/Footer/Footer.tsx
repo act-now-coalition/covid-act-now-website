cc1868fa045b916f612c9fa6edaa7c66f019a72a
Pablo Navarro <pnavarrc@gmail.com>
2021-02-16 17:56:09 -0800

    remove the blog link from the footer (#2753)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import Logo from 'assets/images/footerlogoDarkWithURL';
 import { useIsEmbed } from 'common/utils/hooks';
-import ExternalLink from 'components/ExternalLink';
 import FooterSocialLinks from './FooterSocialLinks';
 import {
   StyledFooter,
@@ -13,7 +12,7 @@ import {
   StyledFooterDivider,
 } from './Footer.style';
 
-const Footer = ({ children }: { children: React.ReactNode }) => {
+const Footer = () => {
   const { pathname } = useLocation();
   const isMapPage = pathname.startsWith('/us') || pathname.startsWith('/state');
   const isEmbed = useIsEmbed();
@@ -23,37 +22,32 @@ const Footer = ({ children }: { children: React.ReactNode }) => {
   }
 
   return (
-    <div>
-      <StyledFooter>
-        <StyledFooterInner isMapPage={isMapPage}>
-          <StyledFooterContent>
-            <Logo />
-            <StyledFooterBodyNav>
-              <Link to="/">Map</Link>
-              <Link to="/learn">Learn</Link>
-              <Link to="/tools">Tools</Link>
-              <ExternalLink href="https://blog.covidactnow.org">
-                Blog
-              </ExternalLink>
-              <Link to="/about">About</Link>
-              <Link to="/contact">Contact us</Link>
-            </StyledFooterBodyNav>
-            <StyledFooterDivider />
-            <StyledFooterBodyLinks>
-              <FooterSocialLinks />
-            </StyledFooterBodyLinks>
-            <StyledFooterDivider />
-            <StyledFooterBodyLinks>
-              <Link to="/contact">Contact</Link>
-              {' ∙ '}
-              <Link to="/terms">Terms</Link>
-              {' ∙ '}
-              <a href="mailto:press@covidactnow.org">Press</a>
-            </StyledFooterBodyLinks>
-          </StyledFooterContent>
-        </StyledFooterInner>
-      </StyledFooter>
-    </div>
+    <StyledFooter>
+      <StyledFooterInner isMapPage={isMapPage}>
+        <StyledFooterContent>
+          <Logo />
+          <StyledFooterBodyNav>
+            <Link to="/">Map</Link>
+            <Link to="/learn">Learn</Link>
+            <Link to="/tools">Tools</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact us</Link>
+          </StyledFooterBodyNav>
+          <StyledFooterDivider />
+          <StyledFooterBodyLinks>
+            <FooterSocialLinks />
+          </StyledFooterBodyLinks>
+          <StyledFooterDivider />
+          <StyledFooterBodyLinks>
+            <Link to="/contact">Contact</Link>
+            {' ∙ '}
+            <Link to="/terms">Terms</Link>
+            {' ∙ '}
+            <a href="mailto:press@covidactnow.org">Press</a>
+          </StyledFooterBodyLinks>
+        </StyledFooterContent>
+      </StyledFooterInner>
+    </StyledFooter>
   );
 };
 
