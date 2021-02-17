90c45b5710266f8bc99d1a5ca84f8ced28651f1c
chasulin <44076375+chasulin@users.noreply.github.com>
2021-02-16 19:43:16 -0500

    Learn alerts section into CMS config + UI (#2751)

---
+++ src/screens/Learn/Alerts/Alerts.tsx
@@ -0,0 +1,52 @@
+import React, { Fragment } from 'react';
+import {
+  BreadcrumbsContainer,
+  SectionName,
+  LearnHeading1,
+} from '../Learn.style';
+import AppMetaTags from 'components/AppMetaTags/AppMetaTags';
+import { MarkdownContent } from 'components/Markdown';
+import PageContent from 'components/PageContent';
+import { learnPages } from 'cms-content/learn';
+import Breadcrumbs from 'components/Breadcrumbs';
+import { formatMetatagDate } from 'common/utils';
+import Footer from 'screens/Learn/Footer/Footer';
+import { aboutOurAlertsContent } from 'cms-content/learn';
+
+const Alerts: React.FC = () => {
+  const {
+    pageHeader,
+    sections,
+    metadataTitle,
+    metadataDescription,
+  } = aboutOurAlertsContent;
+
+  const date = formatMetatagDate();
+
+  return (
+    <Fragment>
+      <AppMetaTags
+        canonicalUrl="/subscribe"
+        pageTitle={metadataTitle}
+        pageDescription={`${date} ${metadataDescription}`}
+      />
+      <PageContent sidebarItems={learnPages}>
+        <BreadcrumbsContainer>
+          <Breadcrumbs item={{ to: '/learn', label: 'Learn' }} />
+        </BreadcrumbsContainer>
+        <LearnHeading1>{pageHeader}</LearnHeading1>
+        {sections.map((section: any, i: number) => (
+          <Fragment key={i}>
+            <SectionName id={section.sectionId}>
+              {section.sectionHeader}
+            </SectionName>
+            <MarkdownContent source={section.sectionBody} />
+          </Fragment>
+        ))}
+        <Footer />
+      </PageContent>
+    </Fragment>
+  );
+};
+
+export default Alerts;
