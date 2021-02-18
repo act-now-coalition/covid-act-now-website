96b9c2656b8ddc941a51c2f135a4b4be419926a0
chasulin <44076375+chasulin@users.noreply.github.com>
2021-02-18 14:32:11 -0500

    disclaimer spacing update

--- src/components/Disclaimer/Disclaimer.style.tsx
+++ src/components/Disclaimer/Disclaimer.style.tsx
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 import { COLOR_MAP } from 'common/colors';
+import { materialSMBreakpoint } from 'assets/theme/sizes';
 
 export const DisclaimerWrapper = styled.div`
   max-width: 600px;
-  margin: 0.5rem 0 1.5rem;
-  padding: 1rem;
+  padding: 1.5rem 0 0.75rem;
   color: ${COLOR_MAP.GRAY_BODY_COPY};
   font-size: 0.875rem;
 
-  @media (max-width: 900px) {
-    margin: 0 1rem 1.5rem;
+  @media (min-width: ${materialSMBreakpoint}) {
+    padding: 1.5rem 0 2rem;
   }
 `;
