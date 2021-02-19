b93d5c82255fb53af7fbd3ab91e80d4bca3bdd6d
Pablo Navarro <pnavarrc@gmail.com>
2021-02-18 16:35:11 -0800

    add source link to vaccination block (#2775)

--- src/components/VaccinationEligibilityBlock/VaccinationEligibilityBlock.tsx
+++ src/components/VaccinationEligibilityBlock/VaccinationEligibilityBlock.tsx
@@ -1,18 +1,25 @@
-import React, { useState } from 'react';
-import { Container } from './VaccinationEligibilityBlock.style';
+import React from 'react';
 import { Heading2, Paragraph } from 'components/Markdown';
 import TabsPanel, { TabInfo } from 'components/TabsPanel';
 import { Region, getStateName } from 'common/regions';
+import ExternalLink from 'components/ExternalLink';
+import { trackEvent, EventCategory, EventAction } from 'components/Analytics';
 import { getEligibilityInfo } from './utils';
+import {
+  Container,
+  Section,
+  Source,
+} from './VaccinationEligibilityBlock.style';
 import EligibilityPanel from './EligibilityPanel';
 import { COLOR_MAP } from 'common/colors';
 
 const VaccinationEligibilityBlock: React.FC<{ region: Region }> = ({
   region,
 }) => {
+  // TODO: Assert that the region is a county, state or a single-state metro
   const stateName = getStateName(region);
   const eligibilityData = getEligibilityInfo(region);
-  const { mostRecentPhaseName } = eligibilityData;
+  const { mostRecentPhaseName, sourceName, sourceUrl } = eligibilityData;
 
   const onSelectTab = (newSelectedTab: number) => {
     // TODO: Tracking
@@ -49,11 +56,31 @@ const VaccinationEligibilityBlock: React.FC<{ region: Region }> = ({
         Eligibility varies throughout {stateName}, so you may also want to check
         your county or city’s health department website.
       </Paragraph>
-      <TabsPanel tabList={tabList} onSelectTab={onSelectTab} />
-      {/* Buttons */}
-      {/* Source */}
+      <Section>
+        <TabsPanel tabList={tabList} onSelectTab={onSelectTab} />
+      </Section>
+      <Section>{/* Buttons */}</Section>
+      <Section>
+        <Source>
+          Source:{' '}
+          <ExternalLink
+            href={sourceUrl}
+            onClick={() => trackSourceClick(region)}
+          >
+            {sourceName}
+          </ExternalLink>
+        </Source>
+      </Section>
     </Container>
   );
 };
+
+function trackSourceClick(region: Region) {
+  trackEvent(
+    EventCategory.VACCINATION,
+    EventAction.CLICK_LINK,
+    `Source: ${region.fullName}`,
+  );
+}
 
 export default VaccinationEligibilityBlock;
