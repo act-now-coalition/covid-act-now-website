0277ffe962ae71633d584a436b312a32ab9c7bdf
chasulin <44076375+chasulin@users.noreply.github.com>
2021-02-24 10:56:07 -0500

    removes unused useeffect import

--- src/components/ShareButtons/ShareButtonGroup.tsx
+++ src/components/ShareButtons/ShareButtonGroup.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import { ClickAwayListener, useMediaQuery } from '@material-ui/core';
 import { useTheme } from '@material-ui/core/styles';
