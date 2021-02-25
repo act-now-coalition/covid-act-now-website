5cc5502c4ca452f4c09804e0b0efb5ef7f092ba6
Chris Kelly <ghop02@gmail.com>
2021-02-25 11:47:57 -0500

    Move logos into product item

--- src/screens/DataApi/DataApi.tsx
+++ src/screens/DataApi/DataApi.tsx
@@ -11,54 +11,26 @@ import {
 } from 'cms-content/learn/data-api';
 import { MarkdownDataApi, DataApiSection } from './DataApi.style';
 import { TocItem } from 'cms-content/utils';
-import { chunk } from 'lodash';
-import { Grid } from '@material-ui/core';
-import { LogoGridItem } from 'components/LogoGrid/LogoGrid';
 import GovLogoGrid from 'screens/About/GovLogoGrid';
 
 const {
   header,
-  logos,
   intro,
   productsList,
   metadataTitle,
   metadataDescription,
 } = productsLandingContent;
 
-const usingDataItem = {
-  productName: "Who's using our data",
-  productId: 'whos-using-our-data',
-  productSubtitle: '',
-  productDescription:
-    'Our API is used by organizations and individuals around the country including:',
-};
-
-const sidebarContent = [usingDataItem, ...productsLandingContent.productsList];
-
 export const sidebarSections: TocItem[] = [
   {
     label: 'Data API',
     to: '/data-api',
-    items: sidebarContent.map(product => ({
+    items: productsList.map(product => ({
       to: `/data-api#${product.productId}`,
       label: product.productName,
     })),
   },
 ];
-
-const WhosUsingOurDataSection: React.FC<{
-  logos: LogoItem[];
-}> = ({ logos }) => {
-  return (
-    <DataApiSection key={usingDataItem.productId}>
-      <Heading2 id={usingDataItem.productId}>
-        {usingDataItem.productName}
-      </Heading2>
-      <MarkdownDataApi source={usingDataItem.productDescription} />
-      <GovLogoGrid logos={logos} />
-    </DataApiSection>
-  );
-};
 
 const DataApi = () => {
   const date = formatMetatagDate();
@@ -72,11 +44,11 @@ const DataApi = () => {
       <PageContent sidebarItems={sidebarSections}>
         <LearnHeading1>{header}</LearnHeading1>
         <MarkdownContent source={intro} />
-        <WhosUsingOurDataSection logos={logos} />
         {productsList.map((product: ProductsLandingSection) => (
           <DataApiSection key={product.productId}>
             <Heading2 id={product.productId}>{product.productName}</Heading2>
             <MarkdownDataApi source={product.productDescription} />
+            {product.logos && <GovLogoGrid logos={product.logos} />}
           </DataApiSection>
         ))}
       </PageContent>
