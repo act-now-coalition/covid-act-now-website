0953e41e04918e5a6c23d874567c4b72240df348
chasulin <44076375+chasulin@users.noreply.github.com>
2021-02-25 18:22:12 -0500

    Vaccinations button icon fix (#2882)

--- src/components/VaccinationEligibilityBlock/ButtonBlock.style.tsx
+++ src/components/VaccinationEligibilityBlock/ButtonBlock.style.tsx
@@ -17,11 +17,6 @@ const PurpleButton = css`
   background-color: ${COLOR_MAP.PURPLE};
   color: white;
 
-  svg {
-    color: white;
-    opacity: 0.5;
-  }
-
   &:hover {
     background-color: ${COLOR_MAP.PURPLE};
     box-shadow: 0px 4px 30px rgba(0, 0, 0, 0.12);
