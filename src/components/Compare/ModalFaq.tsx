f40ff72b857971ac3ef80b910dbfd1dee03c44f6
Michael Lehenbauer <mikelehen@gmail.com>
2021-02-25 15:28:52 -0800

    Add initial FAQ copy.

--- src/components/Compare/ModalFaq.tsx
+++ src/components/Compare/ModalFaq.tsx
@@ -22,6 +22,18 @@ const ModalFaq = (props: { handleCloseModal: () => void }) => {
         <Content>
           <Header>Compare table</Header>
           <Subheader>Frequently asked questions</Subheader>
+          <Question>How do you define “vulnerability”?</Question>
+          <Answer>
+            “Vulnerability” in this context refers to a location’s risk of
+            suffering more severely from COVID, including increased cases and
+            deaths, and recovering more slowly from the physical and economic
+            harm caused by COVID.
+            <br />
+            <br />
+            To calculate vulnerability, we use Surgo Venture’s COVID Community
+            Vulnerability Index (CCVI), which uses key social, health, and
+            economic factors to score every U.S. state and county.
+          </Answer>
           <Question>How are “metro” and “non-metro” counties defined?</Question>
           <Answer>
             You can think of this as urban or rural, but just keep in mind that
