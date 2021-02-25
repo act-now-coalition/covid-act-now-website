a2958e2b654a6fa4dc09ff015781d1f02bc71d0b
Chris Kelly <ghop02@gmail.com>
2021-02-24 21:48:46 -0500

    Adding section + logo grid

--- src/screens/DataApi/DataApi.tsx
+++ src/screens/DataApi/DataApi.tsx
@@ -5,30 +5,60 @@ import { Heading2, MarkdownContent } from 'components/Markdown';
 import { formatMetatagDate } from 'common/utils';
 import { LearnHeading1 } from '../Learn/Learn.style';
 import {
+  LogoItem,
   productsLandingContent,
   ProductsLandingSection,
 } from 'cms-content/learn/data-api';
 import { MarkdownDataApi, DataApiSection } from './DataApi.style';
 import { TocItem } from 'cms-content/utils';
+import { chunk } from 'lodash';
+import { Grid } from '@material-ui/core';
+import { LogoGridItem } from 'components/LogoGrid/LogoGrid';
+import GovLogoGrid from 'screens/About/GovLogoGrid';
 
 const {
   header,
+  logos,
   intro,
   productsList,
   metadataTitle,
   metadataDescription,
 } = productsLandingContent;
 
+const usingDataItem = {
+  productName: "Who's using our data",
+  productId: 'whos-using-our-data',
+  productSubtitle: '',
+  productDescription:
+    'Our API is used by organizations and individuals around the country including:',
+};
+
+const sidebarContent = [usingDataItem, ...productsLandingContent.productsList];
+
 export const sidebarSections: TocItem[] = [
   {
     label: 'Data API',
     to: '/data-api',
-    items: productsLandingContent.productsList.map(product => ({
+    items: sidebarContent.map(product => ({
       to: `/data-api#${product.productId}`,
       label: product.productName,
     })),
   },
 ];
+
+const WhosUsingOurDataSection: React.FC<{
+  logos: LogoItem[];
+}> = ({ logos }) => {
+  return (
+    <DataApiSection key={usingDataItem.productId}>
+      <Heading2 id={usingDataItem.productId}>
+        {usingDataItem.productName}
+      </Heading2>
+      <MarkdownDataApi source={usingDataItem.productDescription} />
+      <GovLogoGrid logos={logos} />
+    </DataApiSection>
+  );
+};
 
 const DataApi = () => {
   const date = formatMetatagDate();
@@ -42,6 +72,7 @@ const DataApi = () => {
       <PageContent sidebarItems={sidebarSections}>
         <LearnHeading1>{header}</LearnHeading1>
         <MarkdownContent source={intro} />
+        <WhosUsingOurDataSection logos={logos} />
         {productsList.map((product: ProductsLandingSection) => (
           <DataApiSection key={product.productId}>
             <Heading2 id={product.productId}>{product.productName}</Heading2>
