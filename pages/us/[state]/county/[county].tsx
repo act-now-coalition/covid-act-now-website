c74c9de9908700d6cfc885ce9a78fdc8fa8a7c35
Pablo Navarro Castillo <pnavarrc@gmail.com>
2021-02-26 15:43:22 -0800

    add location summaries

--- pages/us/[state]/county/[county].tsx
+++ pages/us/[state]/county/[county].tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
-function CountyPage({ region }) {
+function CountyPage({ region, locationSummary }) {
+  console.log(JSON.stringify(locationSummary, null, 2));
+  const { metrics } = locationSummary;
   return (
     <div>
       <h1>{region.fullName}</h1>
+      <span>{`risk level: ${locationSummary.level}`}</span>
+
+      <h2>Overall risk: {locationSummary.level}</h2>
+      <ul>
+        <li>{`level: ${metrics[0].level}, value: ${metrics[0].value}`}</li>
+        <li>{`level: ${metrics[1].level}, value: ${metrics[1].value}`}</li>
+        <li>{`level: ${metrics[2].level}, value: ${metrics[2].value}`}</li>
+        <li>{`level: ${metrics[5].level}, value: ${metrics[5].value}`}</li>
+        <li>{`level: ${metrics[6].level}, value: ${metrics[6].value}`}</li>
+      </ul>
     </div>
   );
 }
@@ -15,6 +27,12 @@ export async function getStaticProps({ params }) {
   const region = regions.findCountyByUrlParams(state, county);
   // get location summaries
   // get projections
+
+  const locationSummaries = await import(
+    '../../../../src/common/location_summaries'
+  );
+
+  const locationSummary = locationSummaries.getSummaryFromFips(region.fipsCode);
 
   // TODO: Implement region.toJSON(), the props here need to be serializable
   return {
@@ -29,6 +47,7 @@ export async function getStaticProps({ params }) {
           stateCode: region.state.stateCode,
         },
       },
+      locationSummary,
     },
   };
 }
