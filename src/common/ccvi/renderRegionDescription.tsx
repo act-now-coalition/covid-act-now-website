4b766144a0613154c801965643c3f10a134f60a6
chasulin <44076375+chasulin@users.noreply.github.com>
2021-03-02 19:25:34 -0500

    updates renderRegionDescription to account for most and least vulnerable regions

--- src/common/ccvi/renderRegionDescription.tsx
+++ src/common/ccvi/renderRegionDescription.tsx
@@ -3,27 +3,65 @@ import { CcviLevel, getCcviLevel } from './index';
 import { formatPercent } from 'common/utils';
 import { Region, State, County } from 'common/regions';
 
+const mostVulnerableFips = ['48', '12105', '37107'];
+const leastVulnerableFips = ['50', '30063', '30033'];
+
 export function renderRegionDescription(
   overallScore: number,
   region: Region,
   percentPopulationVulnerable?: number,
 ): React.ReactElement {
+  const isMostVulnerable = mostVulnerableFips.includes(region.fipsCode);
+  const isLeastVulnerable = leastVulnerableFips.includes(region.fipsCode);
+
   if (region instanceof State) {
-    return renderStateDescription(overallScore, percentPopulationVulnerable);
+    return renderStateDescription(
+      overallScore,
+      isMostVulnerable,
+      isLeastVulnerable,
+      percentPopulationVulnerable,
+    );
   } else if (region instanceof County) {
-    return renderCountyOrMetroDescription(overallScore, 'counties');
+    return renderCountyOrMetroDescription(
+      overallScore,
+      isMostVulnerable,
+      isLeastVulnerable,
+      'counties',
+    );
   } else {
-    return renderCountyOrMetroDescription(overallScore, 'metros');
+    return renderCountyOrMetroDescription(
+      overallScore,
+      isMostVulnerable,
+      isLeastVulnerable,
+      'metros',
+    );
   }
 }
 
 function renderStateDescription(
   overallScore: number,
+  isMostVulnerable: boolean,
+  isLeastVulnerable: boolean,
   percentPopulationVulnerable?: number,
 ): React.ReactElement {
   const level = getCcviLevel(overallScore);
 
-  if (level === CcviLevel.VERY_HIGH) {
+  if (isMostVulnerable) {
+    return (
+      <>
+        has the <strong>highest vulnerability</strong> of all US states
+        {percentPopulationVulnerable
+          ? `, with ${percentPopulationVulnerable}% of the population in a high vulnerability area.`
+          : '.'}
+      </>
+    );
+  } else if (isLeastVulnerable) {
+    return (
+      <>
+        is the <strong>least vulnerable</strong> state in the US.
+      </>
+    );
+  } else if (level === CcviLevel.VERY_HIGH) {
     return (
       <>
         is one of the <strong>most vulnerable</strong> states
@@ -73,12 +111,27 @@ function renderStateDescription(
 
 function renderCountyOrMetroDescription(
   overallScore: number,
+  isMostVulnerable: boolean,
+  isLeastVulnerable: boolean,
   regionType: string,
 ): React.ReactElement {
   const level = getCcviLevel(overallScore);
   const scoreAsPercent = formatPercent(overallScore);
 
-  if (level === CcviLevel.HIGH || level === CcviLevel.VERY_HIGH) {
+  if (isMostVulnerable) {
+    return (
+      <>
+        has the <strong>highest vulnerability</strong> of all US {regionType}.
+      </>
+    );
+  } else if (isLeastVulnerable) {
+    return (
+      <>
+        is the <strong>least vulnerable</strong>{' '}
+        {regionType === 'counties' ? 'county' : 'metro'} in the US .
+      </>
+    );
+  } else if (level === CcviLevel.HIGH || level === CcviLevel.VERY_HIGH) {
     return (
       <>
         is <strong>more vulnerable than {scoreAsPercent}</strong> of U.S.{' '}
