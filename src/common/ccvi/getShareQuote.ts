6a3b3f7ab89cb2873778076e7a8c2970a9a6ac7e
chasulin <44076375+chasulin@users.noreply.github.com>
2021-03-02 19:00:11 -0500

    wip

---
+++ src/common/ccvi/getShareQuote.ts
@@ -0,0 +1,7 @@
+import { Region, State, County, MetroArea } from 'common/regions';
+
+export function getShareQuote(overallScore: number, region: Region): string {
+  const baseText =
+    'Communities across the U.S. are not all equally vulnerable to the effects of the pandemic. According to @CovidActNow, ';
+  return 'hi';
+}
