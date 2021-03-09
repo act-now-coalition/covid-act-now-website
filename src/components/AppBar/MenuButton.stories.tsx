4fe1fd1271af77505537e8dfd787eeca7d18d1c7
Pablo Navarro Castillo <pnavarrc@gmail.com>
2021-03-09 13:36:57 -0800

    fix storybook for hamburger menu button

--- src/components/AppBar/MenuButton.stories.tsx
+++ src/components/AppBar/MenuButton.stories.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import MenuButton from './MenuButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 export default {
   title: 'Building Blocks/Buttons',
   component: MenuButton,
 };
 
-export const HamburgerMenu = () => <MenuButton />;
+export const HamburgerMenu = () => (
+  <MenuButton endIcon={<MenuIcon />}>Menu</MenuButton>
+);
