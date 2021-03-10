f05995eb53bc00c6b060da0fdc3bea603c35933c
Pablo Navarro Castillo <pnavarrc@gmail.com>
2021-03-10 11:05:09 -0800

    add error boundaries around location page blocks

--- src/components/LocationPage/ChartsHolder.style.tsx
+++ src/components/LocationPage/ChartsHolder.style.tsx
@@ -20,28 +20,6 @@ export const ChartContentWrapper = styled.div`
   }
 `;
 
-export const MainContentInner = styled.div`
-  margin: 0 1rem;
-
-  @media (min-width: 932px) {
-    max-width: 900px;
-    margin: 0 auto;
-  }
-
-  @media (min-width: 1350px) {
-    margin: 0 445px 0 auto;
-  }
-
-  @media (min-width: 1750px) {
-    margin: 0 auto;
-  }
-
-  @media print {
-    margin: 0 auto;
-    padding: 0 1rem;
-  }
-`;
-
 export const HeaderWrapperStyles = css`
   display: flex;
   flex-direction: row;
