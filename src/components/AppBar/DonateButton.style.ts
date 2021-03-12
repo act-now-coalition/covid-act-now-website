fd169d293effd3796c9e4a4746dd1e83b3360bf7
Pablo Navarro <pnavarrc@gmail.com>
2021-03-11 16:28:13 -0800

    restore outlined donate button (#3033)

--- src/components/AppBar/DonateButton.style.ts
+++ src/components/AppBar/DonateButton.style.ts
@@ -4,30 +4,25 @@ import { mobileBreakpoint } from 'assets/theme/sizes';
 import LinkButton from 'components/LinkButton';
 
 export const StyledDonateButton = styled(LinkButton).attrs(props => ({
-  variant: 'contained',
+  variant: 'outlined',
 }))`
+  background-color: white;
   margin: auto 0;
   display: inline-flex;
+  border: 2px solid ${COLOR_MAP.GRAY.LIGHT};
+  color: ${COLOR_MAP.PURPLE};
   box-shadow: none;
-  background-color: ${COLOR_MAP.PURPLE};
-  color: white;
-
   &:hover {
-    background-color: ${COLOR_MAP.PURPLE};
-    color: white;
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.12);
+    background-color: white;
   }
-
   @media (min-width: ${mobileBreakpoint}) {
     margin-left: 2rem;
   }
 `;
 
 export const DonateButtonWrapper = styled.div`
-  a {
-    display: flex;
-    height: 100%;
-    text-decoration: none;
-  }
+  display: flex;
 `;
 
 export const StyledDonateButtonHeart = styled(LinkButton)`
