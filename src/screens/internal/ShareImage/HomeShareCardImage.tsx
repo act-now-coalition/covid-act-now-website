659ad8d31c0dfb35a99a2e1f989eed473ee51d02
Andrew <andrew@covidactnow.org>
2021-03-14 20:02:55 -0700

    Split ShareCardImage into Home and Location versions

---
+++ src/screens/internal/ShareImage/HomeShareCardImage.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import SocialLocationPreview from 'components/SocialLocationPreview/SocialLocationPreview';
+import { ShareCardWrapper } from './ShareCardImage.style';
+import { DarkScreenshotWrapper } from './ShareImage.style';
+import { SCREENSHOT_CLASS } from 'components/Screenshot';
+import { Header } from './LocationShareCardImage';
+
+/**
+ * Screen that just shows the appropriate share card so that we can take a
+ * screenshot that we then use as our OpenGraph image.
+ */
+const HomeShareCardImage = () => {
+  // FIXME: do we need <ScreenshotReady> on this one? it was missing before
+  return (
+    <DarkScreenshotWrapper className={SCREENSHOT_CLASS}>
+      <Header isHomePage />
+      <ShareCardWrapper isHomePage>
+        <SocialLocationPreview />;
+      </ShareCardWrapper>
+    </DarkScreenshotWrapper>
+  );
+};
+
+export default HomeShareCardImage;
