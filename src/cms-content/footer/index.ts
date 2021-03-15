387ed9b74c79164aaa0fb0a70c3f58f19aec360e
chasulin <44076375+chasulin@users.noreply.github.com>
2021-03-15 18:34:47 -0400

    Footer fields in CMS config (#3066)

--- src/cms-content/footer/index.ts
+++ src/cms-content/footer/index.ts
@@ -6,9 +6,17 @@ interface LinkItem {
   cta: string;
 }
 
+interface FeaturedItem {
+  cta: string;
+  url: string;
+  description: string;
+  iconId: string;
+}
+
 interface FooterContent {
   learnLinks: LinkItem[];
   aboutUs: Markdown;
+  featuredSections: FeaturedItem[];
 }
 
 export const footerContent = footer as FooterContent;
