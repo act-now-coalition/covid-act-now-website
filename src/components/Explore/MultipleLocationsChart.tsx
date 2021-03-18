25d860fde6d16516433b43cce7e2e8fafe6e73f1
Pablo Navarro Castillo <pnavarrc@gmail.com>
2021-03-18 16:33:50 -0700

    fix tooltips for remaining charts

--- src/components/Explore/MultipleLocationsChart.tsx
+++ src/components/Explore/MultipleLocationsChart.tsx
@@ -3,7 +3,7 @@ import { isNumber, last, max, sortBy } from 'lodash';
 import { Group } from '@vx/group';
 import { scaleUtc, scaleLinear } from '@vx/scale';
 import { useTooltip } from '@vx/tooltip';
-import { formatDecimal, formatUtcDate } from 'common/utils';
+import { formatDecimal } from 'common/utils';
 import { Column } from 'common/models/Projection';
 import { Tooltip, RectClipGroup } from 'components/Charts';
 import { Series } from './interfaces';
@@ -16,7 +16,10 @@ import { Line } from '@vx/shape';
 import DateMarker from './DateMarker';
 import GridLines from './GridLines';
 import Axes from './Axes';
-import { TimeFormat } from 'common/utils/time-utils';
+import {
+  getColumnDate,
+  formatTooltipColumnDate,
+} from 'components/Charts/utils';
 
 interface LabelInfo {
   x: number;
@@ -25,7 +28,6 @@ interface LabelInfo {
   stroke: string;
 }
 
-const getDate = (d: Column) => new Date(d.x);
 const getY = (d: Column) => d.y;
 
 function getSeriesOpacity(
@@ -53,7 +55,7 @@ const MultipleLocationsTooltip: React.FC<{
       width={'210px'}
       top={top}
       left={left}
-      title={formatUtcDate(new Date(pointInfo.x), TimeFormat.MMM_D_YYYY)}
+      title={formatTooltipColumnDate(pointInfo)}
     >
       <Styles.TooltipSubtitle>
         {currentSeries && currentSeries.tooltipLabel}
@@ -158,7 +160,7 @@ const MultipleLocationsChart: React.FC<{
     [showTooltip],
   );
 
-  const getXPosition = (d: Column) => dateScale(getDate(d)) || 0;
+  const getXPosition = (d: Column) => dateScale(getColumnDate(d)) || 0;
   const getYPosition = (d: Column) => yScale(getY(d));
   const seriesLabels = formatCurrentValueLabels(
     seriesList,
