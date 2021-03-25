efb6e71f2e34172ded6e114cad45efa30061e52a
chasulin <44076375+chasulin@users.noreply.github.com>
2021-03-25 14:45:05 -0400

    trims faq ids

--- src/cms-content/utils.ts
+++ src/cms-content/utils.ts
@@ -1,7 +1,7 @@
-import { deburr, words } from 'lodash';
+import { deburr, words, trim } from 'lodash';
 
 export function sanitizeID(sectionId: string): string {
-  return deburr(words(sectionId).join('-')).toLowerCase();
+  return trim(deburr(words(sectionId).join('-')).toLowerCase());
 }
 
 export type Markdown = string;
