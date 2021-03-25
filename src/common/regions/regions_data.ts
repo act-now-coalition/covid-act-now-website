218dee8811b21b9044567ac1cdc6bffddd2ecdf0
Pablo Navarro Castillo <pnavarrc@gmail.com>
2021-03-25 11:51:46 -0700

    remove chain from regions_data

--- src/common/regions/regions_data.ts
+++ src/common/regions/regions_data.ts
@@ -1,4 +1,4 @@
-import { chain, Dictionary, keyBy } from 'lodash';
+import { flatten, map, values, Dictionary, keyBy } from 'lodash';
 import US_STATE_DATASET from 'components/MapSelectors/datasets/us_states_dataset_01_02_2020.json';
 import countyAdjacencyMsa from 'common/data/county_adjacency_msa.json';
 import metroAreaDataset from 'common/data/msa-data.json';
@@ -66,84 +66,77 @@ export const getStateFips = (region: Region): string | null => {
 };
 
 function buildStates(): State[] {
-  return chain(state_dataset)
-    .map(stateInfo => {
-      return new State(
+  return map(
+    state_dataset,
+    stateInfo =>
+      new State(
         stateInfo.state,
         stateInfo.state_url_name,
         stateInfo.state_fips_code,
         stateInfo.population,
         stateInfo.state_code,
-      );
-    })
-    .value();
+      ),
+  );
 }
 
 function buildCounties(
   statesByFips: Dictionary<State>,
   countyAdjacency: { [fipsCode: string]: AdjacencyInfo },
 ): County[] {
-  return chain(state_county_map_dataset)
-    .map(stateData => stateData.county_dataset)
-    .flatten()
-    .map(countyInfo => {
-      /**
-       * TODO: The following counties in New York State have the same
-       * `full_fips_code ` (36061), but different `county_fips_code`.
-       * Determine how we want to handle this before shipping.
-       *
-       * - New York County (county_fips_code: 061)
-       * - Queens County (county_fips_code: 081)
-       * - Richmond County" (county_fips_code: 085)
-       * - Bronx County" (county_fips_code: 005)
-       */
-      const countyFips = `${countyInfo.state_fips_code}${countyInfo.county_fips_code}`;
-      const state = statesByFips[countyInfo.state_fips_code];
-      const adjacentCounties = countyAdjacency[countyFips]?.adjacent_counties;
-      return new County(
-        countyInfo.county,
-        countyInfo.county_url_name,
-        countyFips,
-        countyInfo.population,
-        state,
-        adjacentCounties || [],
-      );
-    })
-    .value();
+  return flatten(
+    map(state_county_map_dataset, stateData => stateData.county_dataset),
+  ).map(countyInfo => {
+    /**
+     * TODO: The following counties in New York State have the same
+     * `full_fips_code ` (36061), but different `county_fips_code`.
+     * Determine how we want to handle this before shipping.
+     *
+     * - New York County (county_fips_code: 061)
+     * - Queens County (county_fips_code: 081)
+     * - Richmond County" (county_fips_code: 085)
+     * - Bronx County" (county_fips_code: 005)
+     */
+    const countyFips = `${countyInfo.state_fips_code}${countyInfo.county_fips_code}`;
+    const state = statesByFips[countyInfo.state_fips_code];
+    const adjacentCounties = countyAdjacency[countyFips]?.adjacent_counties;
+    return new County(
+      countyInfo.county,
+      countyInfo.county_url_name,
+      countyFips,
+      countyInfo.population,
+      state,
+      adjacentCounties || [],
+    );
+  });
 }
 
 function buildMetroAreas(
   countiesByFips: Dictionary<County>,
   statesByFips: Dictionary<State>,
 ): MetroArea[] {
-  const statesByCode = chain(statesByFips)
-    .values()
-    .keyBy(state => state.stateCode)
-    .value();
+  const statesByCode = keyBy(values(statesByFips), state => state.stateCode);
 
-  return chain(metroAreaDataset.metro_areas)
-    .map(metro => {
-      const counties: County[] = metro.countyFipsCodes
-        .map(countyFips => countiesByFips[countyFips] || null)
-        .filter(county => county);
+  return map(metroAreaDataset.metro_areas, metro => {
+    const counties: County[] = metro.countyFipsCodes
+      .map(countyFips => countiesByFips[countyFips] || null)
+      .filter(county => county);
 
-      const [name, statesText] = metro.cbsaTitle.split(', ');
-      const stateCodes = statesText.split('-');
+    const [name, statesText] = metro.cbsaTitle.split(', ');
+    const stateCodes = statesText.split('-');
 
-      const states = stateCodes
-        .map(stateCode => statesByCode[stateCode])
-        .filter(state => state);
+    const states = stateCodes
+      .map(stateCode => statesByCode[stateCode])
+      .filter(state => state);
 
-      return new MetroArea(
-        name,
-        metro.urlSegment,
-        metro.cbsaCode,
-        metro.population,
-        counties,
-        states,
-      );
-    })
-    .value();
+    return new MetroArea(
+      name,
+      metro.urlSegment,
+      metro.cbsaCode,
+      metro.population,
+      counties,
+      states,
+    );
+  });
 }
 
 const states: State[] = buildStates();
