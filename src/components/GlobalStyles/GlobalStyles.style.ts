79ca93428dc47d74dd20199fcc9a3e3c783e2507
Pablo Navarro Castillo <pnavarrc@gmail.com>
2021-03-30 22:13:57 -0700

    add comment

--- src/components/GlobalStyles/GlobalStyles.style.ts
+++ src/components/GlobalStyles/GlobalStyles.style.ts
@@ -4,6 +4,7 @@ import theme from 'assets/theme';
 // TODO(pablo): We want to grab these from the ThemeProvider:
 // color: ${props => props.theme.colors.interactiveBase} but TS
 // complains if we do
+// See https://github.com/styled-components/styled-components/issues/1589#issuecomment-578294784
 const GlobalStyle = createGlobalStyle`
   a {
     color: ${theme.colors.interactiveBase};
