89bfe94389a96e47e5600e51884191b7008c4b3f
Fai <faihegberg@gmail.com>
2021-04-01 11:06:07 -0700

    Remove mock geolocation

--- src/common/hooks/useGeolocation.ts
+++ src/common/hooks/useGeolocation.ts
@@ -28,8 +28,7 @@ export default function useGeolocation(): UseGeolocationReturn {
   useEffect(() => {
     const fetchIpData = () => {
       setIsLoading(true);
-      //fetchGeolocationData()
-      mockGeolocationData()
+      fetchGeolocationData()
         .then(data =>
           setIpData({
             zipCode: data.zip,
@@ -51,39 +50,39 @@ export default function useGeolocation(): UseGeolocationReturn {
   return geolocationReturnObj;
 }
 
-// function fetchGeolocationData() {
-//   return fetch(
-//     `https://pro.ip-api.com/json/?key=${IP_API_KEY}`,
-//   ).then(response => response.json());
-// }
+function fetchGeolocationData() {
+  return fetch(
+    `https://pro.ip-api.com/json/?key=${IP_API_KEY}`,
+  ).then(response => response.json());
+}
 
 // Use this mock to simulate fetchGeolocationData, other data can be obtained from
 // https://ip-api.com/#<IP_ADDRESS> (https://ip-api.com/#8.8.8.8)
 
-function mockGeolocationData() {
-  return Promise.resolve({
-    query: '8.8.8.8',
-    status: 'success',
-    continent: 'North America',
-    continentCode: 'NA',
-    country: 'United States',
-    countryCode: 'US',
-    region: 'VA',
-    regionName: 'Virginia',
-    city: 'Ashburn',
-    district: '',
-    zip: '20149',
-    lat: 39.03,
-    lon: -77.5,
-    timezone: 'America/New_York',
-    offset: -18000,
-    currency: 'USD',
-    isp: 'Google LLC',
-    org: 'Google Public DNS',
-    as: 'AS15169 Google LLC',
-    asname: 'GOOGLE',
-    mobile: false,
-    proxy: false,
-    hosting: true,
-  });
-}
+// function mockGeolocationData() {
+//   return Promise.resolve({
+//     query: '8.8.8.8',
+//     status: 'success',
+//     continent: 'North America',
+//     continentCode: 'NA',
+//     country: 'United States',
+//     countryCode: 'US',
+//     region: 'VA',
+//     regionName: 'Virginia',
+//     city: 'Ashburn',
+//     district: '',
+//     zip: '20149',
+//     lat: 39.03,
+//     lon: -77.5,
+//     timezone: 'America/New_York',
+//     offset: -18000,
+//     currency: 'USD',
+//     isp: 'Google LLC',
+//     org: 'Google Public DNS',
+//     as: 'AS15169 Google LLC',
+//     asname: 'GOOGLE',
+//     mobile: false,
+//     proxy: false,
+//     hosting: true,
+//   });
+// }
