9fc45fa475bf7177464f89f5976c391d0d429cc4
chasulin <44076375+chasulin@users.noreply.github.com>
2021-04-05 13:22:40 -0400

    remove unused import

--- src/components/VaccinationEligibilityBlock/AllAdultsEligibleBlock/AllAdultsEligibleBlock.stories.tsx
+++ src/components/VaccinationEligibilityBlock/AllAdultsEligibleBlock/AllAdultsEligibleBlock.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import AllAdultsEligibleBlock from './AllAdultsEligibleBlock';
-import regions from 'common/regions';
 
 export default {
   title: 'Shared Components/VaccinationEligibilityBlock/AllAdultsEligible',
