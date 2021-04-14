9d077f4c0858dfe27799b44516918fd37144c76f
Andrew Schamp <schamp@gmail.com>
2021-04-13 19:11:07 -0700

    add getRegionsDB and useRegionsDB utilities (#3341)
    * Add getRegionsDB helper
    
    * create useRegionsDB hook
    
    Co-authored-by: Andrew <andrew@covidactnow.org>

--- src/common/regions/region_db.ts
+++ src/common/regions/region_db.ts
@@ -12,7 +12,7 @@ import {
 } from './preprocessed_regions_data';
 import { assert } from 'common/utils';
 
-class RegionDB {
+export class RegionDB {
   public states: State[];
   public counties: County[];
   public metroAreas: MetroArea[];
@@ -148,5 +148,11 @@ const regions = new RegionDB(
   statesByStateCode,
 );
 
+/**
+ * Async helper for decoupling regions DB incrementally
+ */
+export const getRegionsDB = async () => {
+  return regions;
+};
+
 export default regions;
-export { RegionDB };
