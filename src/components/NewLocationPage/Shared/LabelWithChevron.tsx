27992ebe7f86c80ed9a31573f84b2b373b2c857c
chasulin <44076375+chasulin@users.noreply.github.com>
2021-04-21 17:06:43 -0400

    Fix bug on TextAndIconWithSpecialWrapping (#3405)

--- src/components/NewLocationPage/Shared/LabelWithChevron.tsx
+++ src/components/NewLocationPage/Shared/LabelWithChevron.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Chevron } from './Shared.style';
-import { TextAndIconWithSpecialWrapping } from 'components/MenuContent';
+import TextAndIconWithSpecialWrapping from 'components/TextAndIconWithSpecialWrapping/TextAndIconWithSpecialWrapping';
 
 const LabelWithChevron: React.FC<{ text: string }> = ({ text }) => {
   return <TextAndIconWithSpecialWrapping text={text} icon={<Chevron />} />;
