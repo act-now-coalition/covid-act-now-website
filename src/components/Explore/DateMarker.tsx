6a3b3cfad568f19409e8329076d80baffcc8619b
Fai <46338131+faihegberg@users.noreply.github.com>
2021-04-26 13:26:09 -0600

    Fix "days ago" display on chart (#3424)

--- src/components/Explore/DateMarker.tsx
+++ src/components/Explore/DateMarker.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import * as Styles from './Explore.style';
 import { weeksAgo } from './utils';
+import { Series } from './interfaces';
+import { findPointByDate } from 'components/Explore/utils';
+import { getColumnDate } from 'components/Charts/utils';
 
-const DateMarker: React.FC<{ left: number; date: Date }> = ({ left, date }) => {
+/**
+ * DateMarker uses findPointByDate function in the same manner as SingleLocationToolTip.
+ * This ensures that the delta between today and the date shown on hover ("x days ago")
+ * corresponds to the date shown in the tooltip (e.g. "Apr 1, 2021").
+ */
+const DateMarker: React.FC<{
+  left: number;
+  seriesList: Series[];
+  date: Date;
+}> = ({ left, seriesList, date }) => {
+  const [, seriesSmooth] = seriesList;
+  const pointSmooth = findPointByDate(seriesSmooth.data, date);
+  const dateToCompareTo = pointSmooth ? getColumnDate(pointSmooth) : date;
   const today = new Date();
-  return today < date ? null : (
+  today.setHours(0, 0, 0, 0);
+  return today < dateToCompareTo ? null : (
     <Styles.DateMarker style={{ left }}>
-      {weeksAgo(date, today)}
+      {weeksAgo(dateToCompareTo, today)}
     </Styles.DateMarker>
   );
 };
