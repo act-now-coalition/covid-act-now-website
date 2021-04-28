22318e3bc134dcaff54ca1af429a34ba969315c2
chasulin <44076375+chasulin@users.noreply.github.com>
2021-04-28 11:24:09 -0400

    adjusts progress bar sizing

--- src/components/NewLocationPage/VaccinationProgressBarBlock/VaccinationProgressBarBlock.style.tsx
+++ src/components/NewLocationPage/VaccinationProgressBarBlock/VaccinationProgressBarBlock.style.tsx
@@ -3,8 +3,14 @@ import { GrayTitle } from 'components/NewLocationPage/Shared/Shared.style';
 import { materialSMBreakpoint } from 'assets/theme/sizes';
 
 export const ProgressBarContainer = styled.div`
-  max-width: 288px;
   width: 100%;
+  max-width: 350px;
+  margin: auto;
+
+  @media (min-width: ${materialSMBreakpoint}) {
+    max-width: 288px;
+    margin: 0;
+  }
 `;
 
 export const Title = styled(GrayTitle)`
