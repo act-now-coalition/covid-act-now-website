53415d0ac922a35cd8452444c0684ab6d0742750
Fai <46338131+faihegberg@users.noreply.github.com>
2021-04-27 20:51:43 -0600

    Create location name blocks (#3456)

---
+++ src/components/NewLocationPage/LocationName/LocationName.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import LocationName from './LocationName';
+import regions from 'common/regions';
+
+export default {
+  title: 'Location page redesign/Location Name',
+  component: LocationName,
+};
+
+export const NewYork = () => {
+  const state = regions.findByFipsCodeStrict('36');
+  return <LocationName region={state} />;
+};
+
+export const FairFieldCounty = () => {
+  const county = regions.findByFipsCodeStrict('09001');
+  return <LocationName region={county} />;
+};
+
+export const EastBatonRougeParish = () => {
+  const county = regions.findByFipsCodeStrict('22033');
+  return <LocationName region={county} />;
+};
+
+export const AbileneMetro = () => {
+  const metroArea = regions.findByFipsCodeStrict('10180');
+  return <LocationName region={metroArea} />;
+};
+
+export const BostonMetro = () => {
+  const metroArea = regions.findByFipsCodeStrict('14460');
+  return <LocationName region={metroArea} />;
+};
