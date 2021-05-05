22ee23a65053c8f33d1940fb0b39dea0ca52ee24
chasulin <44076375+chasulin@users.noreply.github.com>
2021-05-05 12:56:27 -0400

    darkens initiated blue slightly

--- src/common/colors.ts
+++ src/common/colors.ts
@@ -75,7 +75,7 @@ export const COLOR_MAP = {
     // rename these
     BASE: '#3567FD',
     DARK: '#002CB4',
-    MEDIUM_DARK: 'rgba(53, 103, 253, 0.75)',
+    MEDIUM_DARK: 'rgba(53, 103, 253, 0.8)',
     MEDIUM: 'rgba(53, 103, 253, .6)',
     LIGHT: 'rgba(53, 103, 253, .15)',
     PURPLE: '#5361FD',
