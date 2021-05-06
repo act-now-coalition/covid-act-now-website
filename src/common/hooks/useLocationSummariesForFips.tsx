d9960ae2c240cc9a48858402949f475bed05ac6b
Michael Lehenbauer <mikelehen@gmail.com>
2021-05-06 15:22:48 -0700

    Get location summaries synchronously from the main bundle if possible.
    Fixes https://trello.com/c/Mu3twLii/1306-fix-issue-where-the-screen-flashes-our-bottom-banner-when-navigating-to-location-pages-from-homepage

--- src/common/hooks/useLocationSummariesForFips.tsx
+++ src/common/hooks/useLocationSummariesForFips.tsx
@@ -1,16 +1,6 @@
-import { useEffect, useState } from 'react';
-import { LocationSummary, useSummaries } from 'common/location_summaries';
+import { useSummaries } from 'common/location_summaries';
 
 export default function useLocationSummariesForFips(fips: string) {
-  const [locationSummaryForFips, setLocationSummaryForFips] = useState<
-    LocationSummary
-  >();
   const summaries = useSummaries();
-
-  useEffect(() => {
-    if (summaries) {
-      setLocationSummaryForFips(summaries[fips]);
-    }
-  }, [summaries, fips]);
-  return locationSummaryForFips;
+  return summaries?.[fips] || null;
 }
