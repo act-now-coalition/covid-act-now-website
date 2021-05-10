6c12736f3c5e03f72bac18f89f2f8b0e49dde688
chasulin <44076375+chasulin@users.noreply.github.com>
2021-05-10 13:46:58 -0400

    deletes MenuItem and its styled components

--- src/components/Search/Search.style.tsx
+++ src/components/Search/Search.style.tsx
@@ -13,35 +13,8 @@ export const StyledPaper = styled(Paper)`
   border-top: none;
 `;
 
-export const MenuItemWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  width: 100%;
-
-  strong {
-    font-weight: 600;
-  }
-
-  span {
-    color: rgba(0, 0, 0, 0.7);
-  }
-`;
-
 export const IconWrapper = styled.div`
   margin-right: 0.75rem;
-`;
-
-export const LocationName = styled.p`
-  margin: 0;
-`;
-
-export const Zipcode = styled.span`
-  font-style: italic;
-`;
-
-export const StyledLink = styled(Link)`
-  color: inherit;
-  text-decoration: inherit;
 `;
 
 export const SearchBarIcon = styled(SearchIcon)`
