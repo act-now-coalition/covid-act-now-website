6f853482e02c8d6d105f4287a367ab5edd3ed9ed
chasulin <44076375+chasulin@users.noreply.github.com>
2021-05-10 13:45:46 -0400

    deletes SearchAutocomplete files

--- src/components/Search/index.ts
+++ src/components/Search/index.ts
@@ -1,6 +1,4 @@
-import SearchAutocomplete from './SearchAutocomplete';
 import HomepageSearchAutocomplete from './Homepage/HomepageSearchAutocomplete';
 
 export * from './utils';
 export { HomepageSearchAutocomplete };
-export default SearchAutocomplete;
