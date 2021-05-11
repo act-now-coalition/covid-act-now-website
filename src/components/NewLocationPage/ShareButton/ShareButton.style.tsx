55a351c4ec208b6818c4a6207855cbe57738e69c
Fai <faihegberg@gmail.com>
2021-05-11 13:39:29 -0600

    Delete share button style sheet

--- src/components/NewLocationPage/ShareButton/ShareButton.style.tsx
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import { OutlinedButton } from 'components/ButtonSystem';
-
-export const StyledShareButton = styled(OutlinedButton)`
-  font-weight: normal;
-`;
