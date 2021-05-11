314484fb353f94293f3743bec711f7ce342f9de6
chasulin <44076375+chasulin@users.noreply.github.com>
2021-05-11 14:17:05 -0400

    CMS: learn edits, modal config structure, vulnerabilities modal (#3574)

---
+++ src/cms-content/modals/index.ts
@@ -0,0 +1,24 @@
+import vulnerabilities from './vulnerabilities.json';
+import { Markdown, sanitizeID } from '../utils';
+
+interface Link {
+  cta: string;
+  url: string;
+}
+
+export interface Modal {
+  body: Markdown;
+  header: string;
+  modalId: string;
+  links?: Link[];
+}
+
+function sanitizeModal(modal: Modal): Modal {
+  return {
+    ...modal,
+    links: modal.links || [],
+    modalId: sanitizeID(modal.modalId),
+  };
+}
+
+export const vulnerabilitiesModal = sanitizeModal(vulnerabilities) as Modal;
