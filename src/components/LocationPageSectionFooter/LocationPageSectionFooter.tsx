f17f55989f565805fc0ca2bf5ae82c0ca587ddb5
Fai <faihegberg@gmail.com>
2021-05-18 14:51:09 -0600

    Add share buttons to footer

--- src/components/LocationPageSectionFooter/LocationPageSectionFooter.tsx
+++ src/components/LocationPageSectionFooter/LocationPageSectionFooter.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Wrapper } from './LocationPageSectionFooter.style';
+import ShareButtons from 'components/SharedComponents/ShareButtons';
+import { EventAction, EventCategory, trackEvent } from 'components/Analytics';
 
 const LocationPageSectionFooter: React.FC = ({ children }) => {
-  return <Wrapper>{children}</Wrapper>;
+  return (
+    <>
+      <Wrapper>{children}</Wrapper>
+      <ShareButtons
+        eventCategory={EventCategory.RECOMMENDATIONS}
+        shareUrl={'...'}
+        shareQuote={'...'}
+      />
+    </>
+  );
 };
 
 export default LocationPageSectionFooter;
