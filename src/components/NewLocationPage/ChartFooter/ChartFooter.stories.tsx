7209aa47e5c5ad6a653cd18543df65f2d4318475
Fai <faihegberg@gmail.com>
2021-05-19 23:03:03 -0600

    Create styled components

--- src/components/NewLocationPage/ChartFooter/ChartFooter.stories.tsx
+++ src/components/NewLocationPage/ChartFooter/ChartFooter.stories.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
-import ChartFooter from './ChartFooter';
 import ShareButton from 'components/NewLocationPage/ShareButton/ShareButton';
+import {
+  FooterContainer,
+  ButtonContainer,
+  Disclaimer,
+} from './ChartFooter.style';
 
 export default {
   title: 'Location page redesign/Chart Footer',
-  component: ChartFooter,
 };
 
 const footerText =
   'Over the last week, Texas has averaged 3,173 new confirmed cases per day (10.9 for every 100,000 residents).';
-const footerLinkOrLegend = (
-  <a href={'https://www.google.com'}>About this data</a>
-);
 
-export const ChartFooterExample = () => {
+export const ChartFooterButtonOnly = () => {
   return (
-    <ChartFooter
-      shareButton={<ShareButton onClickShare={() => {}} />}
-      footerText={footerText}
-      footerLinkOrLegend={footerLinkOrLegend}
-    />
+    <FooterContainer>
+      <Disclaimer />
+      <ButtonContainer>
+        <ShareButton onClickShare={() => {}} />
+      </ButtonContainer>
+    </FooterContainer>
+  );
+};
+
+export const ChartFooterWithText = () => {
+  return (
+    <FooterContainer>
+      <Disclaimer>{footerText}</Disclaimer>
+      <ButtonContainer>
+        <ShareButton onClickShare={() => {}} />
+      </ButtonContainer>
+    </FooterContainer>
   );
 };
--- src/components/NewLocationPage/ChartFooter/ChartFooter.style.tsx
+++ src/components/NewLocationPage/ChartFooter/ChartFooter.style.tsx
@@ -1,42 +1,19 @@
 import styled from 'styled-components';
 import { COLOR_MAP } from 'common/colors';
-import BaseButton from 'components/ButtonSystem/BaseButton/BaseButton';
-import { Chevron } from '../Shared/Shared.style';
 
-export const Footer = styled.div`
+export const FooterContainer = styled.div`
   display: flex;
-`;
-
-export const Disclaimer = styled.p`
-  width: 42rem;
-  font-size: 1rem;
-  line-height: 1.5rem;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 1.5rem;
   color: ${COLOR_MAP.GRAY_BODY_COPY};
 `;
 
-export const StyledSpan = styled.span``;
-
-export const TextComponent = styled.p`
-  color: ${COLOR_MAP.GREY_4};
-  margin: 0.7rem 0 0;
+export const ButtonContainer = styled.div`
+  flex-grow: 0;
 `;
 
-export const IconWrapper = styled.div`
-  margin-right: 1rem;
-`;
-
-export const StyledBaseButton = styled(BaseButton)`
-  font-weight: inherit;
-  font-size: inherit;
-  text-transform: none;
-  letter-spacing: 0;
-  padding: 0;
-
-  &:hover {
-    background-color: transparent;
-    ${Chevron} {
-      transform: translate(6px, 5.5px);
-      transition: transform 0.06s ease-in-out;
-    }
-  }
+export const Disclaimer = styled.p`
+  flex-grow: 1;
+  margin: 0;
 `;
