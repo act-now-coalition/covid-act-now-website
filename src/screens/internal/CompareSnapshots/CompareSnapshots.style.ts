86c20d929247c96890e701e90437d52c24567d17
Michael Lehenbauer <michael@covidactnow.org>
2021-05-21 17:40:58 -0700

    Better fix for compare lag when you use the Select combo boxes. (#3648)

--- src/screens/internal/CompareSnapshots/CompareSnapshots.style.ts
+++ src/screens/internal/CompareSnapshots/CompareSnapshots.style.ts
@@ -4,10 +4,4 @@ export const Wrapper = styled.div`
   background-color: white;
   padding: 1rem 2rem;
   min-height: calc(100vh - 64px);
-
-  // HACK: Material-UI adds padding to the body tag when a Select combo box is
-  // open (see https://stackoverflow.com/q/60682426), which causes all the
-  // graphs to get resized and causes the page to lag. So force the div width to
-  // always match the viewport.
-  width: 100vw;
 `;
