7e0e9cd9df8e835bc76364559e99c89488fc886b
Fai <faihegberg@gmail.com>
2021-05-22 22:49:47 -0600

    Revise styled components

--- src/components/LocationPageSectionFooter/LocationPageSectionFooter.tsx
+++ src/components/LocationPageSectionFooter/LocationPageSectionFooter.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import { Wrapper } from './LocationPageSectionFooter.style';
 
 const LocationPageSectionFooter: React.FC = ({ children }) => {
-  return (
-    <>
-      <Wrapper>{children}</Wrapper>
-    </>
-  );
+  return <Wrapper>{children}</Wrapper>;
 };
 
 export default LocationPageSectionFooter;
