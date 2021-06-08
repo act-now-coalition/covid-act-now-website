23f2d766d0498b3522a842446a8547d2d08d4aab
Fai <faihegberg@gmail.com>
2021-06-08 16:27:42 -0600

    Add necessary props to tabs

--- src/components/NewLocationPage/CompareLocationTabs/CompareLocationTabs.stories.tsx
+++ src/components/NewLocationPage/CompareLocationTabs/CompareLocationTabs.stories.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
 import CompareLocationTabs from './CompareLocationTabs';
+import {
+  HomepageLocationScope,
+  homepageLabelMap,
+  GeoScopeFilter,
+} from 'common/utils/compare';
 
 export default {
   title: 'Location page redesign/Compare Location Tabs',
   component: CompareLocationTabs,
 };
 
-export const CompareLocationTabsExample = () => {
-  return <CompareLocationTabs />;
+const homepageOptions = [
+  HomepageLocationScope.COUNTY,
+  HomepageLocationScope.MSA,
+  HomepageLocationScope.STATE,
+];
+
+const countyOptions = [
+  GeoScopeFilter.NEARBY,
+  GeoScopeFilter.STATE,
+  GeoScopeFilter.COUNTRY,
+];
+
+export const CompareOnHomePage = () => {
+  return (
+    <CompareLocationTabs
+      options={homepageOptions}
+      value={homepageLabelMap[HomepageLocationScope.MSA].plural}
+      isCounty={false}
+    />
+  );
+};
+
+export const CompareOnCounty = () => {
+  return (
+    <CompareLocationTabs
+      options={countyOptions}
+      value={'VT'}
+      isCounty={true}
+      stateId={'VT'}
+    />
+  );
 };
--- src/components/NewLocationPage/CompareLocationTabs/CompareLocationTabs.tsx
+++ src/components/NewLocationPage/CompareLocationTabs/CompareLocationTabs.tsx
@@ -1,25 +1,58 @@
 import React, { useState } from 'react';
 import { ButtonGroup, Button } from './CompareLocationTabs.style';
+import {
+  HomepageLocationScope,
+  homepageLabelMap,
+  GeoScopeFilter,
+} from 'common/utils/compare';
 
-const CompareLocationTabs: React.FC = () => {
-  const [alignment, setAlignment] = useState<string | null>('left');
-  const handleAlignment = (
+function getGeoScopeCopy(
+  geoScope: GeoScopeFilter | HomepageLocationScope,
+  stateId?: string,
+) {
+  console.log(typeof geoScope);
+  switch (geoScope) {
+    case GeoScopeFilter.NEARBY:
+      return 'Nearby';
+    case GeoScopeFilter.STATE:
+      return stateId;
+    case GeoScopeFilter.COUNTRY:
+      return 'USA';
+    default:
+      return null;
+  }
+}
+
+const CompareLocationTabs: React.FC<{
+  options: Array<HomepageLocationScope | GeoScopeFilter>;
+  value: string;
+  isCounty: boolean;
+  stateId?: string;
+}> = ({ options, value, isCounty, stateId }) => {
+  const [selectedOption, setSelectedOption] = useState(value);
+  const handleSelectedOption = (
     event: React.MouseEvent<HTMLElement>,
-    newAlignment: string | null,
+    newSelection: string,
   ) => {
-    setAlignment(newAlignment);
+    setSelectedOption(newSelection);
   };
   return (
-    <ButtonGroup value={alignment} exclusive onChange={handleAlignment}>
-      <Button value="left" aria-label="left aligned">
-        Nearby
-      </Button>
-      <Button value="center" aria-label="centered">
-        TX
-      </Button>
-      <Button value="right" aria-label="right aligned">
-        USA
-      </Button>
+    <ButtonGroup
+      value={selectedOption}
+      exclusive
+      onChange={handleSelectedOption}
+    >
+      {options.map(option =>
+        isCounty ? (
+          <Button value={getGeoScopeCopy(option, stateId)}>
+            {getGeoScopeCopy(option, stateId)}
+          </Button>
+        ) : (
+          <Button value={homepageLabelMap[option].plural}>
+            {homepageLabelMap[option].plural}
+          </Button>
+        ),
+      )}
     </ButtonGroup>
   );
 };
