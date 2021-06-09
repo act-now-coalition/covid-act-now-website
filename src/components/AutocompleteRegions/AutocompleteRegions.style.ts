617107b96145e72543c6f588044025f2be536df3
Fai <faihegberg@gmail.com>
2021-06-09 15:54:04 -0600

    Adjust font size

--- src/components/AutocompleteRegions/AutocompleteRegions.style.ts
+++ src/components/AutocompleteRegions/AutocompleteRegions.style.ts
@@ -12,7 +12,7 @@ export const StyledTextField = styled(TextField)<{
   .MuiAutocomplete-inputRoot[class*='MuiOutlinedInput-root']
     .MuiAutocomplete-input {
     font-weight: 500;
-    font-size: 16px;
+    font-size: 1rem;
     min-width: ${props => props.$placeholderMinWidth};
   }
 `;
