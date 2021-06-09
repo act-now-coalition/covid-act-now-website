cd4b3edb4405413eb3fa4aa3ad8e455100b78e7e
chasulin <44076375+chasulin@users.noreply.github.com>
2021-06-09 15:00:51 -0400

    fixes recommendations sizing

--- src/components/LocationPage/ChartsHolder.style.tsx
+++ src/components/LocationPage/ChartsHolder.style.tsx
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components';
 import palette from 'assets/theme/palette';
 import { Typography } from '@material-ui/core';
 import { COLOR_MAP } from 'common/colors';
-import { mobileBreakpoint } from 'assets/theme/sizes';
+import { mobileBreakpoint, materialSMBreakpoint } from 'assets/theme/sizes';
 
 export const LocationPageContentWrapper = styled.div`
   @media print {
@@ -53,10 +53,14 @@ export const BetaTag = styled.div`
 export const DisclaimerWrapper = styled.div`
   max-width: 600px;
   color: ${COLOR_MAP.GRAY_BODY_COPY};
-  font-size: 1rem;
+  font-size: 0.9rem;
   padding-right: 2rem;
 
   p {
     font-size: inherit;
+  }
+
+  @media (min-width: ${materialSMBreakpoint}) {
+    font-size: 1rem;
   }
 `;
