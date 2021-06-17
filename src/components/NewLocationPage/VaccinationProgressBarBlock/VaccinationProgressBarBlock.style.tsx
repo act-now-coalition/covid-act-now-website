15d4f2f425471856872e56031331a66c89b55733
Michael Lehenbauer <mikelehen@gmail.com>
2021-06-16 19:52:28 -0700

    New VaccineProgressBar.
    * Move NewLocationPage/VaccinationProgressBarBlock/ProgressBar to a top-level
      VaccineProgressBar component for use in compare and tooltips.
    * Color the bar based on the vaccination percent and add hatched styling for
      partially-vaccinated part of bar.

--- src/components/NewLocationPage/VaccinationProgressBarBlock/VaccinationProgressBarBlock.style.tsx
+++ src/components/NewLocationPage/VaccinationProgressBarBlock/VaccinationProgressBarBlock.style.tsx
@@ -2,17 +2,6 @@ import styled from 'styled-components';
 import { GrayTitle } from 'components/NewLocationPage/Shared/Shared.style';
 import { materialSMBreakpoint } from 'assets/theme/sizes';
 
-export const ProgressBarContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  margin: auto;
-
-  @media (min-width: ${materialSMBreakpoint}) {
-    max-width: 288px;
-    margin: 0;
-  }
-`;
-
 export const Title = styled(GrayTitle)`
   display: none;
 
