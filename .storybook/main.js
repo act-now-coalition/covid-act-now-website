4e02a5b7d7b833d66cb193b6824bba085239c710
Michael Lehenbauer <michael@covidactnow.org>
2021-06-17 14:32:23 -0700

    Speed up storybook builds (From ~10s to ~2.5s). (#3829)
    I noticed the build was hanging for a long time on "70% - sealing React Docgen
    Typescript Plugin" so I did some googling and found
    https://github.com/storybookjs/storybook/issues/12585#issuecomment-701292052
    which has a workaround, though it breaks the "Docs" tab in StoryBook.

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,4 +6,8 @@ module.exports = {
     '@storybook/preset-create-react-app',
     '@storybook/addon-a11y',
   ],
+
+  // This speeds up builds significantly at the cost of doc generation. See:
+  // https://github.com/storybookjs/storybook/issues/12585#issuecomment-701292052
+  typescript: { reactDocgen: 'react-docgen' }
 };
