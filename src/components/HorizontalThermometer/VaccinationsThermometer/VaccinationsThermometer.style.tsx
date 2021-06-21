849960044b2931d27e0f6a1c64ac4a6eb68aa911
chasulin <44076375+chasulin@users.noreply.github.com>
2021-06-21 15:58:30 -0400

    drops label font size to .75rem on mobile

--- src/components/HorizontalThermometer/VaccinationsThermometer/VaccinationsThermometer.style.tsx
+++ src/components/HorizontalThermometer/VaccinationsThermometer/VaccinationsThermometer.style.tsx
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 import { COLOR_MAP } from 'common/colors';
+import { materialSMBreakpoint } from 'assets/theme/sizes';
 
 export const Label = styled.text`
-  font-size: 0.875rem;
+  font-size: 0.75rem;
   fill: ${COLOR_MAP.GRAY_BODY_COPY};
   dominant-baseline: hanging;
   text-anchor: middle;
+
+  @media (min-width: ${materialSMBreakpoint}) {
+    font-size: 0.875rem;
+  }
 `;
