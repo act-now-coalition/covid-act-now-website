aa02c5e0f8ba425dee4b55fe2485dc8904f47380
Fai <faihegberg@gmail.com>
2021-06-24 14:13:45 -0600

    Change compare table title on location page

--- src/components/Compare/CompareTable.tsx
+++ src/components/Compare/CompareTable.tsx
@@ -265,7 +265,9 @@ const CompareTable = (props: {
     <Wrapper $isModal={props.isModal} $isHomepage={props.isHomepage}>
       {!props.isModal && (
         <HeaderContainer $isHomepage={props.isHomepage}>
-          <CompareHeader $isHomepage={props.isHomepage}>Compare</CompareHeader>
+          <CompareHeader $isHomepage={props.isHomepage}>
+            {props.isHomepage ? 'Compare' : 'Counties'}
+          </CompareHeader>
           {!disableFilters && (
             <Filters
               isHomepage={props.isHomepage}
