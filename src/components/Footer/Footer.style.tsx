b384ae3d9f5a63210a3cdc77d90eab8b4c201822
Fai <46338131+faihegberg@users.noreply.github.com>
2021-06-24 11:10:11 -0600

    End homepage upsell A/B test (#3887)

--- src/components/Footer/Footer.style.tsx
+++ src/components/Footer/Footer.style.tsx
@@ -1,27 +1,19 @@
 import styled from 'styled-components';
 import { COLOR_MAP } from 'common/colors';
-import { materialSMBreakpoint, mobileBreakpoint } from 'assets/theme/sizes';
+import { mobileBreakpoint } from 'assets/theme/sizes';
 import { mobileBreakpointPlus } from 'components/MenuContent/Menu.style';
 
-export const StyledFooter = styled.footer<{ extraPaddingBottom?: boolean }>`
+export const StyledFooter = styled.footer`
   box-sizing: content-box;
   background: ${COLOR_MAP.BLACK};
   padding: 2rem 1.5rem;
-  padding-bottom: ${({ extraPaddingBottom }) =>
-    extraPaddingBottom && '9.75rem'};
-
-  @media (min-width: ${materialSMBreakpoint}) {
-    padding-bottom: ${({ extraPaddingBottom }) => extraPaddingBottom && '8rem'};
-  }
 
   @media (min-width: ${mobileBreakpoint}) {
     padding: 3rem;
-    padding-bottom: ${({ extraPaddingBottom }) => extraPaddingBottom && '8rem'};
   }
 
   @media (min-width: ${mobileBreakpointPlus}) {
     padding: 4rem;
-    padding-bottom: ${({ extraPaddingBottom }) => extraPaddingBottom && '8rem'};
   }
 `;
 
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import { useLocation } from 'react-router-dom';
 import { StyledFooter } from './Footer.style';
 import MenuContent from 'components/MenuContent';
 import { trackEvent, EventCategory, EventAction } from 'components/Analytics';
@@ -7,21 +6,11 @@ import { ThemeProvider, ThemeContext } from 'styled-components';
 import { megaMenuFooter } from 'assets/theme';
 import LogoNonUrl from 'assets/images/LogoNonUrl';
 import { useIsEmbed } from 'common/utils/hooks';
-import {
-  Experiment,
-  ExperimentID,
-  Variant,
-  VariantID,
-} from 'components/Experiment';
 
 const Footer: React.FC = () => {
   const isEmbed = useIsEmbed();
 
   const theme = useContext(ThemeContext);
-  const { pathname } = useLocation();
-  // Extra padding added to the footer for the homepage upsell experiment,
-  // which implements a fixed banner to the bottom of the screen:
-  const extraPaddingBottom = pathname.includes('/us');
 
   const onClick = (label: string) => {
     trackEvent(EventCategory.FOOTER, EventAction.NAVIGATE, label);
@@ -38,21 +27,9 @@ const Footer: React.FC = () => {
         megaMenu: megaMenuFooter,
       }}
     >
-      <Experiment id={ExperimentID.HOMEPAGE_UPSELL}>
-        <Variant id={VariantID.A}>
-          <StyledFooter role="contentinfo">
-            <MenuContent onClick={onClick} Logo={LogoNonUrl} />
-          </StyledFooter>
-        </Variant>
-        <Variant id={VariantID.B}>
-          <StyledFooter
-            role="contentinfo"
-            extraPaddingBottom={extraPaddingBottom}
-          >
-            <MenuContent onClick={onClick} Logo={LogoNonUrl} />
-          </StyledFooter>
-        </Variant>
-      </Experiment>
+      <StyledFooter role="contentinfo">
+        <MenuContent onClick={onClick} Logo={LogoNonUrl} />
+      </StyledFooter>
     </ThemeProvider>
   );
 };
