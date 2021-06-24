e637ca10e22b3468d658c76914865c1561561feb
chasulin <44076375+chasulin@users.noreply.github.com>
2021-06-24 18:15:59 -0400

    edit hook

--- src/common/hooks/useChartHeightForBreakpoint.ts
+++ src/common/hooks/useChartHeightForBreakpoint.ts
@@ -1,19 +1,8 @@
-import { useEffect, useState } from 'react';
 import { useBreakpoint } from 'common/hooks';
 
 export default function useChartHeightForBreakpoint(): number {
   const chartHeightDesktop = 375;
   const chartHeightMobile = 250;
-  const [chartHeight, setChartHeight] = useState<number>(chartHeightDesktop);
   const isMobile = useBreakpoint(600);
-
-  useEffect(() => {
-    if (isMobile) {
-      setChartHeight(chartHeightMobile);
-    } else {
-      setChartHeight(chartHeightDesktop);
-    }
-  }, [isMobile]);
-
-  return chartHeight;
+  return isMobile ? chartHeightMobile : chartHeightDesktop;
 }
