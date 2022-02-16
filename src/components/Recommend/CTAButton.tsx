2d347dff2f75d0f54c3b507766494cbeb59c9c14
Fai <fai@covidactnow.org>
2022-02-16 13:11:01 -0800

    Shorten comment

--- src/components/Recommend/CTAButton.tsx
+++ src/components/Recommend/CTAButton.tsx
@@ -24,7 +24,8 @@ const CTAButton: React.FC<{
   category: RecommendCategory;
 }> = ({ cta, category }) => {
   /**
-   * Although CTA buttons are optional, in Netlify, if a parent (i.e. CTA button) is optional, all fields underneath it (i.e. buttonType, text, url) must be optional.
+   * Although CTA buttons are optional, in Netlify, if a parent (i.e. CTA button) is optional,
+   * all fields underneath it (i.e. buttonType, text, url) must be optional.
    * This leaves room for content writers to fill in some fields for the CTA button, but not all.
    * `validFields` confirms all fields are filled prior to returning a button.
    */
