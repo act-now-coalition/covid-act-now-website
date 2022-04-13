d3246ecaa02490f9e5af868fff9a2fc4c8d7c13e
Pablo Navarro Castillo <pnavarrc@gmail.com>
2022-04-13 07:40:40 -0700

    remove outdated comment

--- src/components/SocialLocationPreview/SocialLocationPreviewMap.stories.tsx
+++ src/components/SocialLocationPreview/SocialLocationPreviewMap.stories.tsx
@@ -6,13 +6,6 @@ export default {
   component: SocialLocationPreviewMap,
 };
 
-// const SocialLocationPreview = (props: {
-//   border?: boolean;
-//   isEmbed?: boolean;
-//   isRiskMap?: boolean;
-//   Footer?: ComponentType;
-//   isEmbedPreview?: boolean;
-// }) => {
 export const CommunityLevelsMap = () => {
   return (
     <div style={{ width: 600 }}>
