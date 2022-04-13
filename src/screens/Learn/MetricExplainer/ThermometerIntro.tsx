d130aaa6fe61c3e0ccc8911d2d57128f07f0cdce
chasulin <44076375+chasulin@users.noreply.github.com>
2022-04-13 15:13:11 -0400

    update intro thermometer

--- src/screens/Learn/MetricExplainer/ThermometerIntro.tsx
+++ src/screens/Learn/MetricExplainer/ThermometerIntro.tsx
@@ -3,13 +3,7 @@ import Thermometer, { ThermometerBox } from 'components/Thermometer';
 import { LOCATION_SUMMARY_LEVELS } from 'common/metrics/location_summary';
 import { Level } from 'common/level';
 
-const levels = [
-  Level.SUPER_CRITICAL,
-  Level.CRITICAL,
-  Level.HIGH,
-  Level.MEDIUM,
-  Level.LOW,
-];
+const levels = [Level.HIGH, Level.MEDIUM, Level.LOW];
 
 const items = levels.map(level => {
   const summaryLevel = LOCATION_SUMMARY_LEVELS[level];
@@ -17,7 +11,7 @@ const items = levels.map(level => {
     title: summaryLevel.name + ' risk',
     description: '',
     color: summaryLevel.color,
-    roundTop: level === Level.SUPER_CRITICAL,
+    roundTop: level === Level.HIGH,
     roundBottom: level === Level.LOW,
   };
 });
