9fb356413e10a4ec25bb9715558bc3387f64deb6
Pablo Navarro Castillo <pnavarrc@gmail.com>
2022-04-13 08:00:22 -0700

    update preview image legend title

--- src/components/SocialLocationPreview/SocialLocationPreviewMap.tsx
+++ src/components/SocialLocationPreview/SocialLocationPreviewMap.tsx
@@ -71,7 +71,7 @@ const SocialLocationPreview = (props: {
 };
 
 const RiskMapLegend = ({ isEmbed = false }: { isEmbed?: boolean }) => (
-  <Legend condensed={!isEmbed}>
+  <Legend condensed={!isEmbed} header="Community Level">
     {[Level.HIGH, Level.MEDIUM, Level.LOW].map(level => (
       <LegendItem
         key={`legend-${level}`}
