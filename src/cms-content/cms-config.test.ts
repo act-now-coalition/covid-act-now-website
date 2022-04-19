c60781158cd150f6212bcdc26df8c1a259f17525
chasulin <44076375+chasulin@users.noreply.github.com>
2022-04-19 12:00:36 -0400

    Revert cms branch (#5623)

--- src/cms-content/cms-config.test.ts
+++ src/cms-content/cms-config.test.ts
@@ -17,8 +17,7 @@ describe('CMS Config', () => {
     // cms-content-entry is the current CMS branch we work off of. During development
     // this branch is often changed.  This check ensures that it is
     // set to the default before merging.
-    // TODO(8.2) - Restore cms-content-entry after 8.2 launch
-    expect(configBranchName).toBe('can-82');
+    expect(configBranchName).toBe('cms-content-entry');
   });
 
   test('Config branch is cms-vaccine-info for vaccination CMS config', () => {
