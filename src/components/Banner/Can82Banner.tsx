6785973c5330581ba2ff4ebe68944551ee0eae9d
Fai <fai@covidactnow.org>
2022-04-19 09:30:57 -0700

    Banner link update and tiny copy edit

--- src/components/Banner/Can82Banner.tsx
+++ src/components/Banner/Can82Banner.tsx
@@ -4,15 +4,29 @@ import {
   CondensedBannerWrapperHomepage,
   CondensedBannerWrapperLocationPage,
 } from './Banner.style';
-import { Link } from 'react-router-dom';
+import ExternalLink from 'components/ExternalLink';
+import { EventAction, EventCategory, trackEvent } from 'components/Analytics';
 
-//TODO(8.2) - update with correct learn more URL
+const trackClick = () => {
+  trackEvent(
+    EventCategory.HOMEPAGE_BANNER,
+    EventAction.CLICK_LINK,
+    'Banner Learn More Link',
+  );
+};
 
 const Can82Banner: React.FC = () => (
   <CondensedBanner role="banner">
     We've made some changes to align with the current state of COVID and CDC
     guidelines. All metrics that we've previously tracked will continue to be
-    available. <Link to="/covid-risk-levels-metrics">Learn more</Link>
+    available.{' '}
+    <ExternalLink
+      href="https://mailchi.mp/actnowcoalition/site-updates"
+      id="banner-learn-more-link"
+      onClick={trackClick}
+    >
+      Learn more
+    </ExternalLink>
   </CondensedBanner>
 );
 
