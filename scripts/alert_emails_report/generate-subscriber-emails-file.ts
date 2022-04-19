5f2b1496e97069cf6dee2d34eb6cd666a5ce409b
Pablo Navarro Castillo <pnavarrc@gmail.com>
2022-04-19 09:13:50 -0700

    output the email subscribers as csv

--- scripts/alert_emails_report/generate-subscriber-emails-file.ts
+++ scripts/alert_emails_report/generate-subscriber-emails-file.ts
@@ -1,13 +1,18 @@
+import fs from 'fs';
 import _ from 'lodash';
 import { fetchAllAlertSubscriptions } from '../alert_emails/firestore';
 import { getFirestore } from '../common/firebase';
+import { isValidEmail } from '../../src/common/utils';
 
 async function main() {
   const db = getFirestore();
   const subscriptions = await fetchAllAlertSubscriptions(db);
-  const fs = require('fs');
-  const output = subscriptions.map(s => s.email).join(',');
-  fs.writeFileSync('subscriber-emails.txt', output);
+  const emailsCsvColumn = subscriptions
+    .map(s => s.email)
+    .filter(isValidEmail)
+    .join('\n');
+
+  fs.writeFileSync('subscriber-emails.csv', emailsCsvColumn);
 }
 
 if (require.main === module) {
