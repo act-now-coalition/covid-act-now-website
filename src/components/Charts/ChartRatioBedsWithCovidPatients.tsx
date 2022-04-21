fc9b718cd2eff1aac7b974eb92af038b8a953141
Sean McClure <sean.mcclure17@gmail.com>
2022-04-21 16:37:47 -0400

    Pin Patients w/ Covid chart height to (max value + 5%)

--- src/components/Charts/ChartRatioBedsWithCovidPatients.tsx
+++ src/components/Charts/ChartRatioBedsWithCovidPatients.tsx
@@ -4,8 +4,8 @@ import { formatPercent } from 'common/utils';
 import { Column } from 'common/models/Projection';
 import { RATIO_BEDS_WITH_COVID_PATIENTS_LEVEL_INFO_MAP } from 'common/metrics/ratio_beds_with_covid_patients';
 
-// TODO(8.2) - confirm thresholds/chart/tooltip content, y-cap
-const CAP_Y = 1;
+// Add CAP_PADDING% extra padding above the maximum chart value
+const CAP_PADDING = 0.05;
 
 const getPointText = (valueY: number) => formatPercent(valueY, 1);
 
@@ -14,6 +14,15 @@ const getTooltipContent = (valueY: number) => ({
   body: `${formatPercent(valueY, 0)} of all beds`,
   width: '125px',
 });
+
+const maxColumnValue = (data: Column[]) => {
+  return Math.max.apply(
+    null,
+    data.map(function (column) {
+      return column.y;
+    }),
+  );
+};
 
 const ChartRatioBedsWithCovidPatients = ({
   columnData,
@@ -25,7 +34,7 @@ const ChartRatioBedsWithCovidPatients = ({
   <ChartZones
     height={height}
     columnData={columnData}
-    capY={CAP_Y}
+    capY={maxColumnValue(columnData) + CAP_PADDING}
     zones={RATIO_BEDS_WITH_COVID_PATIENTS_LEVEL_INFO_MAP}
     getTooltipContent={getTooltipContent}
     getPointText={getPointText}
