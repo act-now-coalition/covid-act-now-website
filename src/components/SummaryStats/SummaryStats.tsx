508a3b57b24d30f5a5e083e21fed9b824ea791b4
Michael Lehenbauer <mikelehen@gmail.com>
2022-05-03 09:19:10 -0700

    Remove legacy dead code from SummaryStats component.
    SummaryStats used to be used for the LocationPage above-the-fold, but we rewrote it a long time ago so now it's only used by Embed and SocialLocationPreview. So it contained a lot of dead code.
    
    This also lets us get rid of the detail() method from LevelInfo, which cleans up some legacy metric code.
    
    I also deleted the unused SignalStatus component which used to be used by SummaryStats but its usage was removed a while ago.
    
    Remove dead SignalStatus component.

--- src/components/SummaryStats/SummaryStats.tsx
+++ src/components/SummaryStats/SummaryStats.tsx
@@ -9,69 +9,41 @@ import {
   SummaryStatsWrapper,
   SummaryStatWrapper,
   StatNameText,
-  StatDetailText,
   StatValueText,
   StatTextWrapper,
   StatValueWrapper,
 } from './SummaryStats.style';
 import * as urls from 'common/urls';
-import StatTag from 'components/SummaryStats/StatTag';
 import { CircleIcon } from 'components/NewLocationPage/Shared/Shared.style';
 
-//TODO (Chelsi) - remove dupication: extract 'LocationHeaderStats' and 'SummaryStats' into a single separate component
+//TODO (Chelsi) - remove dupication: extract 'SummaryStats' into a single separate component
 
 const SummaryStat: React.FC<{
   chartType: Metric;
   value: number;
-  onClick: () => void;
-  beta?: boolean;
-  condensed?: boolean;
-  flipSignalStatusOrder?: boolean;
-  isMobile?: boolean;
-  isHeader?: boolean;
   isEmbed?: boolean;
   embedOnClickBaseURL?: string;
-}> = ({
-  chartType,
-  value,
-  onClick,
-  beta,
-  condensed,
-  flipSignalStatusOrder,
-  isMobile,
-  isHeader,
-  isEmbed,
-  embedOnClickBaseURL,
-}) => {
+}> = ({ chartType, value, isEmbed, embedOnClickBaseURL }) => {
   const levelInfo = getLevelInfo(chartType, value);
   const embedOnClick = () => {
     const url = urls.addSharingId(`${embedOnClickBaseURL}/chart/${chartType}`);
     window.open(url, '_blank');
   };
-  const finalOnClick = isEmbed ? embedOnClick : onClick;
+  const finalOnClick = isEmbed ? embedOnClick : () => {};
 
   return (
-    <SummaryStatWrapper
-      onClick={finalOnClick}
-      $condensed={condensed}
-      $isEmbed={isEmbed}
-    >
+    <SummaryStatWrapper onClick={finalOnClick} $isEmbed={isEmbed}>
       <StatTextWrapper $isEmbed={isEmbed}>
-        <StatNameText $condensed={condensed} $isEmbed={isEmbed}>
+        <StatNameText $isEmbed={isEmbed}>
           {getMetricNameForCompare(chartType)}{' '}
-          {!condensed && beta && isMobile && (
-            <StatTag beta isHeader={isHeader} />
-          )}
         </StatNameText>
-        {!condensed && <StatDetailText>{levelInfo.detail()}</StatDetailText>}
       </StatTextWrapper>
-      <StatValueWrapper $condensed={condensed}>
+      <StatValueWrapper>
         {value == null ? null : (
           <>
             <CircleIcon $iconColor={levelInfo.color} />
-            <StatValueText $condensed={condensed} $isEmbed={isEmbed}>
+            <StatValueText $isEmbed={isEmbed}>
               {formatValue(chartType, value, /*nullValueCopy=*/ '-')}
-              {!condensed && beta && !isMobile && <StatTag beta />}
             </StatValueText>
           </>
         )}
@@ -80,27 +52,15 @@ const SummaryStat: React.FC<{
   );
 };
 
-const noop = () => {};
-
 const SummaryStats = (props: {
   stats: { [key: string]: number | null };
-  condensed?: boolean;
   isEmbed?: boolean;
-  onRtRangeClick?: () => void;
-  onTestPositiveClick?: () => void;
-  onIcuUtilizationClick?: () => void;
-  onVaccinationsClick?: () => void;
-  isMobile?: boolean;
-  isHeader?: boolean;
   embedOnClickBaseURL?: string;
 }) => {
   const hasStats = !!Object.values(props.stats).filter(
     (val: number | null) => val !== null,
   ).length;
   const sharedStatProps = {
-    isMobile: props.isMobile,
-    condensed: props.condensed,
-    isHeader: props.isHeader,
     embedOnClickBaseURL: props.embedOnClickBaseURL,
     isEmbed: props.isEmbed,
   };
@@ -108,21 +68,18 @@ const SummaryStats = (props: {
   return (
     <>
       {hasStats && (
-        <SummaryStatsWrapper $condensed={props.condensed}>
+        <SummaryStatsWrapper>
           <SummaryStat
-            onClick={props.onRtRangeClick || noop}
             chartType={Metric.WEEKLY_CASES_PER_100K}
             value={props.stats[Metric.WEEKLY_CASES_PER_100K] as number}
             {...sharedStatProps}
           />
           <SummaryStat
-            onClick={props.onRtRangeClick || noop}
             chartType={Metric.ADMISSIONS_PER_100K}
             value={props.stats[Metric.ADMISSIONS_PER_100K] as number}
             {...sharedStatProps}
           />
           <SummaryStat
-            onClick={props.onRtRangeClick || noop}
             chartType={Metric.RATIO_BEDS_WITH_COVID}
             value={props.stats[Metric.RATIO_BEDS_WITH_COVID] as number}
             {...sharedStatProps}
