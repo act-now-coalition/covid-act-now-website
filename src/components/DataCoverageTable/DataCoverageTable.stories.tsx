665e79d943e55133554f5149bfe46abc0f403daa
Pablo Navarro Castillo <pnavarrc@gmail.com>
2022-05-10 16:28:09 -0700

    reorder data availability component

--- src/components/DataCoverageTable/DataCoverageTable.stories.tsx
+++ src/components/DataCoverageTable/DataCoverageTable.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import CoverageTable, { COVERAGE_SUMMARY } from 'components/DataCoverageTable';
 
 export default {
-  title: 'Data API/Data Availability Table',
+  title: 'Components/Data Availability Table',
   component: CoverageTable,
 };
 
