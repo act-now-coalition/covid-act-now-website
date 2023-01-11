a2073d7cf3a9a7afa0a0a54f1eded9b2ea86e881
Michael Lehenbauer <mikelehen@gmail.com>
2023-01-11 14:34:12 -0800

    Add disclaimer per CAN Copy Audit '23 Q1 doc.
    Added to: FAQ, Glossary, Research Rundown, COVID Explained Homepage, COVID Explained articles.

---
+++ src/components/LearnDisclaimer/LearnDisclaimer.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import LearnDisclaimer from './LearnDisclaimer';
+
+export default {
+  title: 'Components/LearnDisclaimer',
+  component: LearnDisclaimer,
+};
+
+export const Example = (args: any) => <LearnDisclaimer />;
