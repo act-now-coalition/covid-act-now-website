c38442e598fcbc90066b062ef5dbd987b211cad1
sean mcclure <55333380+smcclure17@users.noreply.github.com>
2023-04-02 12:59:31 -0400

    Add new CDC source for cases data (#7383)
    * Add CDC source for cases data
    
    * add source work daily new cases, too
    
    * and for Rt()

--- src/common/utils/provenance.tsx
+++ src/common/utils/provenance.tsx
@@ -39,17 +39,29 @@ const metricToSourceMap: RegionSourceMap = {
         sourceName: 'The New York Times',
         url: 'https://github.com/nytimes/covid-19-data',
       },
+      {
+        sourceName: 'CDC COVID Data Tracker',
+        url: 'https://covid.cdc.gov/covid-data-tracker/#county-view',
+      },
     ],
     metro: [
       {
         sourceName: 'The New York Times',
         url: 'https://github.com/nytimes/covid-19-data',
       },
+      {
+        sourceName: 'CDC COVID Data Tracker',
+        url: 'https://covid.cdc.gov/covid-data-tracker/#county-view',
+      },
     ],
     county: [
       {
         sourceName: 'The New York Times',
         url: 'https://github.com/nytimes/covid-19-data',
+      },
+      {
+        sourceName: 'CDC COVID Data Tracker',
+        url: 'https://covid.cdc.gov/covid-data-tracker/#county-view',
       },
     ],
   },
@@ -59,17 +71,29 @@ const metricToSourceMap: RegionSourceMap = {
         sourceName: 'The New York Times',
         url: 'https://github.com/nytimes/covid-19-data',
       },
+      {
+        sourceName: 'CDC COVID Data Tracker',
+        url: 'https://covid.cdc.gov/covid-data-tracker/#county-view',
+      },
     ],
     metro: [
       {
         sourceName: 'The New York Times',
         url: 'https://github.com/nytimes/covid-19-data',
       },
+      {
+        sourceName: 'CDC COVID Data Tracker',
+        url: 'https://covid.cdc.gov/covid-data-tracker/#county-view',
+      },
     ],
     county: [
       {
         sourceName: 'The New York Times',
         url: 'https://github.com/nytimes/covid-19-data',
+      },
+      {
+        sourceName: 'CDC COVID Data Tracker',
+        url: 'https://covid.cdc.gov/covid-data-tracker/#county-view',
       },
     ],
   },
@@ -144,17 +168,29 @@ const metricToSourceMap: RegionSourceMap = {
         sourceName: 'The New York Times',
         url: 'https://github.com/nytimes/covid-19-data',
       },
+      {
+        sourceName: 'CDC COVID Data Tracker',
+        url: 'https://covid.cdc.gov/covid-data-tracker/#county-view',
+      },
     ],
     metro: [
       {
         sourceName: 'The New York Times',
         url: 'https://github.com/nytimes/covid-19-data',
       },
+      {
+        sourceName: 'CDC COVID Data Tracker',
+        url: 'https://covid.cdc.gov/covid-data-tracker/#county-view',
+      },
     ],
     county: [
       {
         sourceName: 'The New York Times',
         url: 'https://github.com/nytimes/covid-19-data',
+      },
+      {
+        sourceName: 'CDC COVID Data Tracker',
+        url: 'https://covid.cdc.gov/covid-data-tracker/#county-view',
       },
     ],
   },
